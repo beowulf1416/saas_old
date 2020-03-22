83fd614340f4f57c80ddb44df2bd1af570153415
ferd <beowulf1416@gmail.com>
2020-03-22 10:06:46 +0800

    initial commit

---
+++ src/test/java/com/tomale/saas/SaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomale.saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
