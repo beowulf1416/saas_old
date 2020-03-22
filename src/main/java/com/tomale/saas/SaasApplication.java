4956a43cdd969a6127bfef06c6257406ba9b960a
ferdinand <beowulf1416@gmail.com>
2020-03-22 15:36:58 +0800

    customize jtwig usage

--- src/main/java/com/tomale/saas/SaasApplication.java
+++ src/main/java/com/tomale/saas/SaasApplication.java
@@ -5,30 +5,13 @@ import org.apache.logging.log4j.LogManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-
-import org.jtwig.spring.boot.JtwigAutoConfiguration;
-import org.jtwig.spring.JtwigViewResolver;
 
 
-@SpringBootApplication(
-	exclude = {
-		JtwigAutoConfiguration.class
-	}
-)
+@SpringBootApplication
 public class SaasApplication {
 	public final static Logger log = LogManager.getLogger(SaasApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaasApplication.class, args);
-	}
-
-	@Bean
-	public ViewResolver viewResolver() {
-		JtwigViewResolver resolver = new JtwigViewResolver();
-		resolver.setPrefix("classpath:/templates/");
-		resolver.setSuffix(".html");
-		return resolver;
 	}
 }
