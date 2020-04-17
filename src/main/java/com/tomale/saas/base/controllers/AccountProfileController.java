aa18853d66b537316441e2e44bb198531608032b
ferdinand <beowulf1416@gmail.com>
2020-04-17 11:33:51 +0800

    adding account profile controller

---
+++ src/main/java/com/tomale/saas/base/controllers/AccountProfileController.java
@@ -0,0 +1,17 @@
+package com.tomale.saas.base.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/account")
+public class AccountProfileController {
+
+    @PreAuthorize("hasPermission(#user, 'security.default')")
+    public ModelAndView viewDefault() {
+        ModelAndView mv = new ModelAndView("profile/default");
+        return mv;
+    }
+}
