06e2080ecd9311cffd518939680f776ac094e59c
ferdinand <beowulf1416@gmail.com>
2020-04-18 20:07:47 +0800

    rename controller

--- src/main/java/com/tomale/saas/modules/admin/clients/controllers/ClientController.java
+++ src/main/java/com/tomale/saas/modules/admin/clients/controllers/ClientController.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
     produces = MediaType.TEXT_HTML_VALUE,
     method = RequestMethod.GET
 )
-public class DefaultController {
+public class ClientController {
 
     @GetMapping("")
     @PreAuthorize("hasPermission(#user, 'admin.clients')")
