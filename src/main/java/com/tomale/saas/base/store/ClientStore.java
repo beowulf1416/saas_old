5284fe88a93190e3bdec405fdcf339a3435ad107
ferdinand <beowulf1416@gmail.com>
2020-04-19 12:14:47 +0800

    adding client store

--- src/main/java/com/tomale/saas/base/store/ClientStore.java
+++ src/main/java/com/tomale/saas/base/store/ClientStore.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import com.tomale.saas.base.models.Client;
 
@@ -19,7 +20,8 @@ public class ClientStore {
 
     private static final Logger log = LogManager.getLogger(ClientStore.class);
 
-    private static final String SQL_CLIENTS_ALL = "{? = call clients.clients_all()}";
+    private static final String SQL_CLIENTS_ALL = "{call clients.clients_all()}";
+    private static final String SQL_CLIENTS_GET = "{call clients.clients_get(?)}";
 
     private final JdbcTemplate jdbc;
 
@@ -37,6 +39,34 @@ public class ClientStore {
 
             ResultSet rs = stmt.getResultSet();
             ArrayList<Client> results = new ArrayList<Client>();
+            while(rs.next()) {
+                UUID id = UUID.fromString(rs.getString(0));
+                boolean active = rs.getBoolean(1);
+                String name = rs.getString(2);
+                String address = rs.getString(3);
+            }
+            return results;
+        } catch(SQLException e) {
+            log.error(e);
+            throw new Exception("An error occured while trying to add user");
+        }
+    }
+
+    public List<Client> get() throws Exception {
+        try {
+            CallableStatement stmt = jdbc.getDataSource()
+                .getConnection()
+                .prepareCall(SQL_CLIENTS_GET);
+            stmt.execute();
+
+            ResultSet rs = stmt.getResultSet();
+            ArrayList<Client> results = new ArrayList<Client>();
+            while(rs.next()) {
+                UUID id = UUID.fromString(rs.getString(0));
+                boolean active = rs.getBoolean(1);
+                String name = rs.getString(2);
+                String address = rs.getString(3);
+            }
             return results;
         } catch(SQLException e) {
             log.error(e);
