b45db1894182b9822b0b4c28641c71595a9293fd
ferdinand <beowulf1416@gmail.com>
2020-04-19 17:00:38 +0800

    modified app user grants

--- data/sql/postgres/create_app_user.sql
+++ data/sql/postgres/create_app_user.sql
@@ -7,6 +7,12 @@ create role saas_app_usr login password 'saas_app_pw';
 grant select, insert, update, delete on all tables in schema iam to saas_app_usr;
 grant all privileges on database saas to saas_app_usr;
 
+/** clients **/
+grant all privileges on all tables in schema clients to saas_app_usr;
+grant usage on schema clients to saas_app_usr;
+grant execute on all functions in schema clients to saas_app_usr;
+
+/** iam **/
 grant all privileges on all tables in schema iam to saas_app_usr;
 grant usage on schema iam to saas_app_usr;
 grant execute on all functions in schema iam to saas_app_usr;
