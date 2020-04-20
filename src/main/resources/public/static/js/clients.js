f3dc753dbf3f6595e4711ddebc76f051687df88b
ferdinand <beowulf1416@gmail.com>
2020-04-20 21:30:34 +0800

    adding client selection web component - incomplete

--- src/main/resources/public/static/js/clients.js
+++ src/main/resources/public/static/js/clients.js
@@ -6,11 +6,18 @@
         constructor() {
             super();
 
+            const elemDL = this.for;
+            console.log(elemDL);
+
             const container = document.createElement('div');
             container.classList.add('client-selection');
 
             const shadow = this.attachShadow({ mode: 'open' });
             shadow.appendChild(container);
+        }
+
+        get for() {
+            return this.getAttribute('for') || '';
         }
     }
 
