ad160ead425401a6838681577be98d2e73a45663
ferdinand <beowulf1416@gmail.com>
2020-04-21 11:13:04 +0800

    modified client selector web component

--- src/main/resources/public/static/js/clients.js
+++ src/main/resources/public/static/js/clients.js
@@ -1,25 +1,66 @@
 'use strict';
 
 (function() {
-    class ClientSelection extends HTMLElement {
+    class ClientSelectorList extends HTMLElement {
 
         constructor() {
-            super();
+            self = super();
+
+            const ul = document.createElement("ul");
+            ul.classList.add("client-list")
 
             const elemDL = this.for;
-            console.log(elemDL);
+            const elem = document.getElementById(elemDL);
+            if (elem != null && elem instanceof HTMLDataListElement) {
+                console.log(elem.options);
+                for(let o of elem.options) {
+                    const t = o.getAttribute("label");
+
+                    const a = document.createElement("a");
+                    a.classList.add("nav-link");
+                    a.classList.add("client-item-link");
+                    a.setAttribute("title", t);
+                    a.innerText = t;
+                    a.setAttribute("href", "#");
+
+                    const li = document.createElement("li");
+                    li.classList.add("client-list-item");
+                    li.appendChild(a);
+
+                    ul.appendChild(li);
+                }
+            }
 
             const container = document.createElement('div');
-            container.classList.add('client-selection');
+            container.classList.add('client-selector-list');
+            container.appendChild(ul);
 
             const shadow = this.attachShadow({ mode: 'open' });
             shadow.appendChild(container);
+
+            this.clientSelect = this.clientSelect.bind(this);
         }
 
         get for() {
             return this.getAttribute('for') || '';
         }
+
+        clientSelect(event, elem) {
+            console.log("clientSelect");
+            console.log(event);
+            console.log(elem);
+        }
+
+        connectedCallback() {
+            console.log("here");
+            const clientLinks = this.shadowRoot.querySelectorAll(".client-item-link");
+            for(let l of clientLinks) {
+                l.addEventListener("click", (function(f, arg1) { 
+                    return function(e) { f(e, arg1) }; 
+                })(this.clientSelect, l), false);
+            }
+        }
     }
 
-    customElements.define('client-selection', ClientSelection);
+    customElements.define('client-selector-list', ClientSelectorList);
 })();
