c3cab621b3932845156c39db4b6f878e40d76979
ferdinand <beowulf1416@gmail.com>
2020-04-21 11:35:43 +0800

    adding client selection handler - incomplete

--- src/main/java/com/tomale/saas/base/controllers/ClientController.java
+++ src/main/java/com/tomale/saas/base/controllers/ClientController.java
@@ -4,25 +4,32 @@ import com.tomale.saas.base.models.User;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import com.tomale.saas.base.models.Client;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@Controller
-@RequestMapping("/client")
+@Controller 
+@RequestMapping(
+    path = "/client",
+    produces = MediaType.TEXT_PLAIN_VALUE
+)
 public class ClientController {
 
-    @GetMapping("/select")
+    @GetMapping("")
     @PreAuthorize("hasPermission(#user, 'user.authenticated')")
-    public ModelAndView viewSelect() {
-        ModelAndView mv = new ModelAndView("client/select");
+    public ModelAndView viewDefault() {
+        ModelAndView mv = new ModelAndView("client/default");
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Object o = auth.getPrincipal();
@@ -32,6 +39,22 @@ public class ClientController {
             mv.addObject("clients", clients);
         }
 
+        return mv;
+    }
+
+    @GetMapping("/select")
+    @PreAuthorize("hasPermission(#user, 'user.authenticated')")
+    public ModelAndView viewSelect(
+        HttpServletResponse response,
+        @RequestParam String clientId
+    ) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth instanceof User) {
+            User user = (User) auth;
+
+        }
+
+        ModelAndView mv = new ModelAndView("client/select");
         return mv;
     }
 }
