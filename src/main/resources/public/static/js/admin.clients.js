d773ea16f6f8fee930302db20609e61604668701
ferdinand <beowulf1416@gmail.com>
2020-04-22 15:13:55 +0800

    modified admin clients component

--- src/main/resources/public/static/js/admin.clients.js
+++ src/main/resources/public/static/js/admin.clients.js
@@ -5,34 +5,20 @@ class ClientList extends HTMLElement {
     constructor() {
         self = super();
 
-        const containerActions = document.createElement('div');
-        containerActions.classList.add('actions-container');
-
-        const bAdd = document.createElement('button');
-        bAdd.classList.add('client-add');
-        bAdd.setAttribute('title', 'Add Client');
-        bAdd.innerHTML = '&oplus;';
-
-        self.addClientEvent = new CustomEvent('addClient', {
-            bubbles: true,
-            cancelable: true
-        });
-        bAdd.addEventListener('click', function(e){
-            self.dispatchEvent(self.addClientEvent);
-        });
-
-        containerActions.appendChild(bAdd);
-
-        const ul = document.createElement('ul');
-        ul.classList.add('client-list');
-
         const container = document.createElement('div');
         container.classList.add('client-list-container');
-        container.appendChild(ul);
-        container.appendChild(containerActions);
+
+        self.initList(container);
+        self.initActions(container);
+        self.initFormAddClient(container);
 
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.appendChild(container);
+    }
+
+    initList(container) {
+        const ul = document.createElement('ul');
+        ul.classList.add('client-list');
 
         fetch('/api/admin/clients/all', {
             method: 'POST',
@@ -57,9 +43,63 @@ class ClientList extends HTMLElement {
                 });
             }
         });
+
+        container.appendChild(ul);
+    }
+
+    initActions(container) {
+        const containerActions = document.createElement('div');
+        containerActions.classList.add('actions-container');
+
+        const bAdd = document.createElement('button');
+        bAdd.classList.add('client-add');
+        bAdd.setAttribute('title', 'Add Client');
+        bAdd.innerHTML = '&oplus;';
+
+        self.addClientEvent = new CustomEvent('addClient', {
+            bubbles: true,
+            cancelable: true
+        });
+        bAdd.addEventListener('click', function(e){
+            self.dispatchEvent(self.addClientEvent);
+        });
+
+        containerActions.appendChild(bAdd);
+        container.appendChild(containerActions);
+    }
+
+    initFormAddClient(container) {
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('client-form');
+        formContainer.innerHTML = `
+            <form class="client-form">
+                <fieldset>
+                    <legend>Please enter required information</legend>
+                    <div class="form-input">
+                        <label for="clientName">Name</label>
+                        <input type="text" id="clientName" name="clientName" class="client-name" />
+                    </div><!-- .form-input -->
+                    <div class="form-input">
+                        <label for="clientAddress">Address</label>
+                        <input type="text" id="clientAddress" name="clientAddress" class="client-addr" />
+                    </div><!-- .form-input -->
+                </fieldset>
+                <div class="actions">
+                    <button type="button" id="btnSave" class="btnSave">Save</button>
+                </div><!-- .actions -->
+            </form>
+        `;
+
+        container.appendChild(formContainer);
     }
 
     connectedCallback() {
+        if (this.isConnected) {
+            const btnSave = this.shadowRoot.getElementById('btnSave');
+            btnSave.addEventListener('click', function(e) {
+                console.log('btnSave.clicked');
+            });
+        }
     }
 }
 
