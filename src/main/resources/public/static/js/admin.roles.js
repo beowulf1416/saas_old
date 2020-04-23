787903e736ff13833fb7eaaa2e22530ae4edb175
ferdinand <beowulf1416@gmail.com>
2020-04-23 19:31:50 +0800

    adding admin roles component - incomplete

---
+++ src/main/resources/public/static/js/admin.roles.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class RolesList extends HTMLElement {
+
+    constructor() {
+        self = super();
+    }
+
+    connectedCallback() {
+        
+    }
+}
+
+customElements.define('roles-list', RoleList);
+export { RolesList };
