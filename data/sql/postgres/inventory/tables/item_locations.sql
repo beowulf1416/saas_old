2eb6435a21c6787dfe73c8e162f8f1cabfab3921
ferdinand <beowulf1416@gmail.com>
2020-04-28 18:29:21 +0800

    modified item locations table

--- data/sql/postgres/inventory/tables/item_locations.sql
+++ data/sql/postgres/inventory/tables/item_locations.sql
@@ -4,5 +4,14 @@
 create table if not exists item_locations (
     client_id uuid not null,
     location_id uuid not null,
-    qty numeric(12, 4) not null
+    qty numeric(12, 4) not null,
+    uom_id uuid not null,
+    constraint pk_item_locations primary key (client_id, location_id),
+    constraint fk_item_locations_1 foreign key (client_id)
+        references clients.clients (id) on delete restrict on update restrict,
+    constraint fk_item_locations_2 foreign key (location_id)
+        references locations (id) on delete restrict on update restrict,
+    constraint fk_item_locations_3 foreign key (uom_id)
+        references item_uom (id) on delete restrict on update restrict,
+    constraint chk_item_locations_1 check(qty > 0)
 );
