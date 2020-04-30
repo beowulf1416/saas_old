2a83c20868e0cd5711792e0b6e5200257a53dd25
ferdinand <beowulf1416@gmail.com>
2020-04-30 23:11:29 +0800

    modified item editor

--- src/main/resources/public/static/js/inventory/item.edit.js
+++ src/main/resources/public/static/js/inventory/item.edit.js
@@ -23,8 +23,40 @@ class InventoryItemEditor extends HTMLElement {
         fieldset.innerHTML = `
             <legend>General</legend>
             <div class="form-group">
+                <label for="id">Id</label>
+                <input type="text" id="id" name="id" title="Item Id" />
+            </div><!-- .form-group -->
+            <div class="form-group">
                 <label for="name">Name</label>
                 <input type="text" id="name" name="name" title="Name" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input type="text" id="description" name="description" title="Item description" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="make">Make</label>
+                <input type="text" id="make" name="make" title="Item Make" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="brand">Brand</label>
+                <input type="text" id="brand" name="brand" title="Item brand" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="model">Model</label>
+                <input type="text" id="model" name="model" title="Item Model" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="version">Version</label>
+                <input type="text" id="version" name="version" title="Item Version" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="sku">SKU</label>
+                <input type="text" id="sku" name="sku" title="Item Stock Keeping Unit" />
+            </div><!-- .form-group -->
+            <div class="form-group">
+                <label for="UPC">UPC</label>
+                <input type="text" id="upc" name="uoc" title="Universal Product Code" />
             </div><!-- .form-group -->
         `;
 
