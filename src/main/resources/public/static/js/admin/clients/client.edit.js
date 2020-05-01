e8fc41acdef00a9bb13f958e44f981676f1a62a1
ferdinand <beowulf1416@gmail.com>
2020-05-01 19:40:48 +0800

    adding client editor component

---
+++ src/main/resources/public/static/js/admin/clients/client.edit.js
@@ -0,0 +1,58 @@
+'use strict';
+
+class ClientEditor extends HTMLElement {
+
+    constructor() {
+        self = super();
+
+        const div = document.createElement('div');
+        div.classList.add('container');
+
+        this.initForm(self, div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(div);
+    }
+
+    initForm(component, container) {
+        const form = document.createElement('form');
+        form.classList.add('form-client');
+        form.innerHTML = `
+            <fieldset class="required">
+                <legend>Required</legend>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" title="Name" class="form-input-text" />
+                </div><!-- .form-group -->
+                <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" id="address" name="address" title="address" class="form-input-text" />
+                </div><!-- .form-group" -->
+            </fieldset>
+            <fieldset class="actions">
+                <button type="button" class="bSave">Save</button>
+                <button type="button" class="bCancel">Cancel</button>
+            </fieldset>
+        `;
+
+        container.appendChild(form);
+    }
+
+    connectedCallback() {
+        if (this.isConnected) {
+            const shadow = this.shadowRoot;
+            const bSave = shadow.querySelector('#bSave');
+            bSave.addEventListener('click', function(e) {
+                console.log('client editor button save clicked');
+            });
+
+            const bCancel = shadow.querySelector('#bCancel');
+            bCancel.addEventListener('click', function(e) {
+                console.log('client editor button cancel clicked');
+            });
+        }
+    }
+}
+
+customElements.define('client-editor', ClientEditor);
+export { ClientEditor };
