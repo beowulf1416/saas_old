5b20cbb23b28719a25cc6e15114f2d5a637ae5fd
ferdinand <beowulf1416@gmail.com>
2020-05-04 15:29:17 +0800

    adding permissions to inventory page

--- src/main/java/com/tomale/saas/modules/inventory/controllers/InventoryController.java
+++ src/main/java/com/tomale/saas/modules/inventory/controllers/InventoryController.java
@@ -2,7 +2,7 @@ package com.tomale.saas.modules.inventory.controllers;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +15,14 @@ public class InventoryController {
     public final static Logger log = LogManager.getLogger(InventoryController.class);
 
     @GetMapping("")
+    @PreAuthorize("hasPermission(#user, 'inventory.dashboard')")
     public String viewDefault() {
         log.debug("VIEW: inventory.default");
         return "inventory/default";
     }
 
     @GetMapping("/dashboard")
+    @PreAuthorize("hasPermission(#user, 'inventory.dashboard')")
     public String viewDashboard() {
         log.debug("VIEW: inventory.dashboard");
         return "inventory/default";
