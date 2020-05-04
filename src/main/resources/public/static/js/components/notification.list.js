78aa1645e558a5d8979b78cf368751fbf7d673a4
ferdinand <beowulf1416@gmail.com>
2020-05-04 11:37:42 +0800

    added notification list component

---
+++ src/main/resources/public/static/js/components/notification.list.js
@@ -0,0 +1,41 @@
+'use strict';
+
+class NotificationList extends HTMLElement {
+
+    constructor() {
+        self = super();
+
+        const ul = document.createElement('ul');
+        ul.classList.add('notification-list');
+
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.appendChild(ul);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(div);
+
+        this.add = this.add.bind(this);
+    }
+
+    add(status, msg, timeout) {
+        const self = this;
+        const shadow = this.shadowRoot;
+        const ul = shadow.querySelector('ul.notification-list');
+        const id = 'msg' + Math.random().toString(16).slice(10);
+        const li = document.createElement('li');
+        li.id = id;
+        li.classList.add('list-item');
+        li.classList.add('message');
+        li.classList.add('message-' + status);
+        li.textContent = msg;
+        if (timemout && Number.isInteger(timeout)) {
+            setTimeout(() => {
+                ul.removeChild(li);
+            }, timeout);
+        }
+    }
+}
+
+customElements.define('notification-list', NotificationList);
+export { NotificationList };
