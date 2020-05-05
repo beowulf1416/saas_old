57c8329327365dbf66b9e435626d06b08c392e92
ferdinand <beowulf1416@gmail.com>
2020-05-05 17:18:21 +0800

    refactor client users table

--- src/main/resources/public/static/js/components/admin/clients/client.users.table.js
+++ src/main/resources/public/static/js/components/admin/clients/client.users.table.js
@@ -49,76 +49,107 @@ class AdminClientUsersTable extends HTMLElement {
         container.append(div);
     }
 
-    setClient(clientId) {
+    setUsers(users) {
         const self = this;
-        self.dataset.clientId = clientId;
-        this.refresh();
+        if (Array.isArray(users)) {
+            const tbl = self.shadowRoot.querySelector("table.users tbody");
+            users.forEach(u => {
+                const tr = document.createElement('tr');
+                tr.classList.add('user-row');
+                tr.innerHTML = `
+                    <td>
+                        <input type="radio" name="selectedUser" class="user-select" title="Select" value="${u.id}" />
+                    </td>
+                    <td>${u.active}</td>
+                    <td>${u.name}</td>
+                    <td>
+                        <a class="nav-link nav-email" href="mailto: ${u.email}" title="Send email to ${u.email}">${u.email}</a>
+                    </td>
+                `;
+                tbl.appendChild(tr);
+
+                const userSelect = tr.querySelector('input.user-select');
+                userSelect.addEventListener('change', function(e) {
+                    self.dispatchEvent(new CustomEvent('onselectuser', {
+                        bubbles: true,
+                        cancelable: true,
+                        detail: userSelect.value
+                    }));
+                });
+            });
+        } else {
+            self.dispatchEvent(new CustomEvent('onerror', {
+                bubbles: true,
+                cancelable: true,
+                detail: "Expecting and array of users"
+            }));
+        }
     }
 
-    refresh() {
-        const self = this;
-        const clientId = self.dataset.clientId;
-        AdminClients.users(clientId, function(e) {
-            if (e.status == 'success') {
-                if (e.json) {
-                    const data = JSON.parse(e.json);
-                    if (Array.isArray(data)) {
-                        const tbl = self.shadowRoot.querySelector("table.users tbody");
-                        while(tbl.firstChild) {
-                            tbl.removeChild(tbl.lastChild);
-                        }
-                        data.forEach(d => {
-                            const tr = document.createElement('tr');
-                            tr.classList.add('user-row');
-                            tr.innerHTML = `
-                                <td>
-                                    <input type="radio" name="selectedUser" class="user-select" title="Select" value="${d.id}" />
-                                </td>
-                                <td>${d.active}</td>
-                                <td>${d.name}</td>
-                                <td>
-                                    <a class="nav-link nav-email" href="mailto: ${d.email}" title="Send email to ${d.email}">${d.email}</a>
-                                </td>
-                            `;
-                            tbl.appendChild(tr);
+    // refresh() {
+    //     const self = this;
+    //     const clientId = self.dataset.clientId;
+    //     AdminClients.users(clientId, function(e) {
+    //         if (e.status == 'success') {
+    //             if (e.json) {
+    //                 const data = JSON.parse(e.json);
+    //                 if (Array.isArray(data)) {
+    //                     const tbl = self.shadowRoot.querySelector("table.users tbody");
+    //                     while(tbl.firstChild) {
+    //                         tbl.removeChild(tbl.lastChild);
+    //                     }
+    //                     data.forEach(d => {
+    //                         const tr = document.createElement('tr');
+    //                         tr.classList.add('user-row');
+    //                         tr.innerHTML = `
+    //                             <td>
+    //                                 <input type="radio" name="selectedUser" class="user-select" title="Select" value="${d.id}" />
+    //                             </td>
+    //                             <td>${d.active}</td>
+    //                             <td>${d.name}</td>
+    //                             <td>
+    //                                 <a class="nav-link nav-email" href="mailto: ${d.email}" title="Send email to ${d.email}">${d.email}</a>
+    //                             </td>
+    //                         `;
+    //                         tbl.appendChild(tr);
 
-                            const userSelect = tr.querySelector('input.user-select');
-                            userSelect.addEventListener('change', function(e) {
-                                self.dispatchEvent(new CustomEvent('onselectuser', {
-                                    bubbles: true,
-                                    cancelable: true,
-                                    detail: userSelect.value
-                                }));
-                            });
-                        });
+    //                         const userSelect = tr.querySelector('input.user-select');
+    //                         userSelect.addEventListener('change', function(e) {
+    //                             self.dispatchEvent(new CustomEvent('onselectuser', {
+    //                                 bubbles: true,
+    //                                 cancelable: true,
+    //                                 detail: userSelect.value
+    //                             }));
+    //                         });
+    //                     });
 
-                        const tr = document.createElement('tr');
-                        tr.classList.add('user-add');
-                        tr.innerHTML = `
-                            <td>
-                                <a id="userAdd" class="nav-link user-row-add" title="Add User" href="#">Add User</a>
-                            </td>
-                        `;
-                        tbl.appendChild(tr);
+    //                     const tr = document.createElement('tr');
+    //                     tr.classList.add('user-add');
+    //                     tr.innerHTML = `
+    //                         <td>
+    //                             <a id="userAdd" class="nav-link user-row-add" title="Add User" href="#">Add User</a>
+    //                         </td>
+    //                     `;
+    //                     tbl.appendChild(tr);
 
-                        const lAdd = tbl.querySelector("a#userAdd");
-                        lAdd.addEventListener('click', function(e) {
-                            self.dispatchEvent(new CustomEvent('onadduser', {
-                                bubbles: true,
-                                cancelable: true
-                            }));
-                        });
-                    } else {
+    //                     const lAdd = tbl.querySelector("a#userAdd");
+    //                     lAdd.addEventListener('click', function(e) {
+    //                         self.dispatchEvent(new CustomEvent('onadduser', {
+    //                             bubbles: true,
+    //                             cancelable: true
+    //                         }));
+    //                     });
+    //                 } else {
 
-                    }
-                } else {
-                    console.error(e.json);
-                }
-            } else {
-                console.error(e.message);
-            }
-        });
-    }
+    //                 }
+    //             } else {
+    //                 console.error(e.json);
+    //             }
+    //         } else {
+    //             console.error(e.message);
+    //         }
+    //     });
+    // }
 }
 
 customElements.define('client-users-table', AdminClientUsersTable);
