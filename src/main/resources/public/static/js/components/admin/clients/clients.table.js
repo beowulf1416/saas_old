3ca335ff510d8578702a12c4fa60ab19a484f223
ferdinand <beowulf1416@gmail.com>
2020-05-07 09:59:08 +0800

    removing client table refresh function

--- src/main/resources/public/static/js/components/admin/clients/clients.table.js
+++ src/main/resources/public/static/js/components/admin/clients/clients.table.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { AdminClients } from '/static/js/api/admin.clients.js';
+// import { AdminClients } from '/static/js/api/admin.clients.js';
 
 
 class AdminClientsTable extends HTMLElement {
@@ -13,11 +13,13 @@ class AdminClientsTable extends HTMLElement {
 
         this.initTable(self, container);
 
-        this.refresh = this.refresh.bind(this);
-        this.refresh();
+        // this.refresh = this.refresh.bind(this);
+        // this.refresh();
 
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.appendChild(container);
+
+        this.setClients = this.setClients.bind(this);
     }
     
     connectedCallback() {
@@ -36,6 +38,7 @@ class AdminClientsTable extends HTMLElement {
                 <thead>
                     <tr>
                         <th></th>
+                        <th>Active</th>
                         <th>Name</th>
                     </tr>
                 </thead>
@@ -47,74 +50,151 @@ class AdminClientsTable extends HTMLElement {
         container.append(div);
     }
 
-    refresh() {
+    setClients(clients, options) {
         const self = this;
-        AdminClients.all(function(e) {
-            if (e && e.status == 'success') {
-                const data = JSON.parse(e.json);
-                if (Array.isArray(data)) {
-                    const shadow = self.shadowRoot;
-                    const tbl = shadow.querySelector('table.clients tbody');
-                    while(tbl.firstChild) {
-                        tbl.removeChild(tbl.lastChild);
-                    }
+        if (Array.isArray(clients)) {
+            const shadow = self.shadowRoot;
+            const tbl = shadow.querySelector('table.clients tbody');
+            while(tbl.firstChild) {
+                tbl.removeChild(tbl.lastChild);
+            }
 
-                    const clients = data;
-                    clients.forEach(client => {
-                        const tr = document.createElement('tr');
-                        tr.classList.add('client-row');
-                        tr.innerHTML = `
-                            <td></td>
-                            <td>
-                                <a class="nav-link client-row-link" title="${client.name}" data-id="${client.id}" href="#">
-                                    ${client.name}
-                                </a>
-                            </td>
-                        `;
+            clients.forEach(client => {
+                const tr = document.createElement('tr');
+                tr.classList.add('client-row');
+                tr.innerHTML = `
+                    <td></td>
+                    <td>
+                        <a title="Toggle Active" class="nav-link client-link-active" href="#" data-id="${client.id}" data-active="${client.active}">${client.active}</a>
+                    </td>
+                    <td>
+                        <a class="nav-link client-row-link" title="${client.name}" data-id="${client.id}" href="#">
+                            ${client.name}
+                        </a>
+                    </td>
+                `;
 
-                        tbl.appendChild(tr);
-                    });
+                tbl.appendChild(tr);
 
-                    const lClients = tbl.querySelectorAll('tbody .client-row-link');
-                    lClients.forEach(l => {
-                        l.addEventListener('click', function(e) {
-                            self.dispatchEvent(new CustomEvent('onselectclient', {
-                                bubbles: true,
-                                cancelable: true,
-                                detail: {
-                                    clientId: l.dataset.id
-                                }
-                            }));
-                        });
-                    });
+                const lClient = tr.querySelector('a.client-row-link');
+                lClient.addEventListener('click', function(e) {
+                    self.dispatchEvent(new CustomEvent('onselectclient', {
+                        bubbles: true,
+                        cancelable: true,
+                        detail: {
+                            clientId: lClient.dataset.id
+                        }
+                    }));
+                });
+
+                const lActive = tr.querySelector('a.client-link-active');
+                lActive.addEventListener('click', function(e) {
+                    self.dispatchEvent(new CustomEvent('onupdateclientactive', {
+                        bubbles: true,
+                        cancelable: true,
+                        detail: {
+                            clientId: lActive.dataset.id,
+                            active: lActive.dataset.active
+                        }
+                    }));
+                });
+            });
+
+            if (options && options.allowAdd == true) {
+                const tr = document.createElement('tr');
+                tr.classList.add('client-add');
+                tr.innerHTML = `
+                    <td>
+                        <a  id="lAdd" class="nav-link client-row-add" title="Add Client" href="#">Add</a>
+                    </td>
+                `;
+
+                const lAdd = tr.querySelector('a#lAdd');
+                lAdd.addEventListener('click', function(e) {
+                    self.dispatchEvent(new CustomEvent('onaddclient', {
+                        bubbles: true,
+                        cancelable: true
+                    }));
+                });
+
+                tbl.appendChild(tr);
+            }
+        } else {
+            self.dispatchEvent(new CustomEvent('onerror', {
+                bubbles: true,
+                cancelable: true
+            }));
+        }
+    }
+
+    // refresh() {
+    //     const self = this;
+    //     AdminClients.all(function(e) {
+    //         if (e && e.status == 'success') {
+    //             const data = JSON.parse(e.json);
+    //             if (Array.isArray(data)) {
+    //                 const shadow = self.shadowRoot;
+    //                 const tbl = shadow.querySelector('table.clients tbody');
+    //                 while(tbl.firstChild) {
+    //                     tbl.removeChild(tbl.lastChild);
+    //                 }
+
+    //                 const clients = data;
+    //                 clients.forEach(client => {
+    //                     const tr = document.createElement('tr');
+    //                     tr.classList.add('client-row');
+    //                     tr.innerHTML = `
+    //                         <td></td>
+    //                         <td>
+    //                             <a class="nav-link client-row-link" title="${client.name}" data-id="${client.id}" href="#">
+    //                                 ${client.name}
+    //                             </a>
+    //                         </td>
+    //                     `;
+
+    //                     tbl.appendChild(tr);
+    //                 });
+
+    //                 const lClients = tbl.querySelectorAll('tbody .client-row-link');
+    //                 lClients.forEach(l => {
+    //                     l.addEventListener('click', function(e) {
+    //                         self.dispatchEvent(new CustomEvent('onselectclient', {
+    //                             bubbles: true,
+    //                             cancelable: true,
+    //                             detail: {
+    //                                 clientId: l.dataset.id
+    //                             }
+    //                         }));
+    //                     });
+    //                 });
                     
 
-                    const tr = document.createElement('tr');
-                    tr.classList.add('client-add');
-                    tr.innerHTML = `
-                        <td>
-                            <a  id="lAdd" class="nav-link client-row-add" title="Add Client" href="#">Add</a>
-                        </td>
-                    `;
+    //                 const tr = document.createElement('tr');
+    //                 tr.classList.add('client-add');
+    //                 tr.innerHTML = `
+    //                     <td>
+    //                         <a  id="lAdd" class="nav-link client-row-add" title="Add Client" href="#">Add</a>
+    //                     </td>
+    //                 `;
 
-                    const lAdd = tr.querySelector('a#lAdd');
-                    lAdd.addEventListener('click', function(e) {
-                        self.dispatchEvent(new CustomEvent('onaddclient', {
-                            bubbles: true,
-                            cancelable: true
-                        }));
-                    });
+    //                 const lAdd = tr.querySelector('a#lAdd');
+    //                 lAdd.addEventListener('click', function(e) {
+    //                     self.dispatchEvent(new CustomEvent('onaddclient', {
+    //                         bubbles: true,
+    //                         cancelable: true
+    //                     }));
+    //                 });
 
-                    tbl.appendChild(tr);
+    //                 tbl.appendChild(tr);
 
-                } else {
-                    console.error('unexpected data');
-                }
-            } else {
-                console.error(e);
-            }
-        });
-    }
+    //             } else {
+    //                 console.error('unexpected data');
+    //             }
+    //         } else {
+    //             console.error(e);
+    //         }
+    //     });
+    // }
 }
 
 customElements.define('clients-table', AdminClientsTable);
