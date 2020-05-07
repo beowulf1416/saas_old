27f057d40ca883e5e9cd99cff3e38dadf6ab957c
ferdinand <beowulf1416@gmail.com>
2020-05-07 09:57:31 +0800

    modified sp to retrieve use clients

--- data/sql/postgres/iam/procs/users/user_clients_all.sql
+++ data/sql/postgres/iam/procs/users/user_clients_all.sql
@@ -6,14 +6,18 @@ create or replace function user_clients_all (
 )
 returns table (
     client_id clients.clients.id%type,
-    client_name clients.clients.name%type
+    active clients.clients.active%type,
+    name clients.clients.name%type,
+    address clients.clients.address%type
 )
 as $$
 begin
     return query
     select
         distinct c.id,
-        c.name
+        c.active,
+        c.name,
+        c.address
     from clients.clients c
         inner join iam.user_clients uc on c.id = uc.client_id
     where uc.user_id = p_user_id;
