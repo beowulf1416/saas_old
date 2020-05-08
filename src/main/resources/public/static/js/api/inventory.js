49472140d4fe257210af0fc5d6c12baeca954fd4
ferdinand <beowulf1416@gmail.com>
2020-05-08 20:45:48 +0800

    adding inventory api

---
+++ src/main/resources/public/static/js/api/inventory.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Inventory {
+
+    static items(clientId, filter) {
+        fetch('/api/inventory/items', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                clientId: clientId,
+                filter: filter
+            })
+        })
+        .then((response) => response.json());
+    }
+
+    static criticalItems(clientId, filter, func) {
+        fetch('/api/inventory/items', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                clientId: clientId,
+                filter: filter,
+                critical: true
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => func ? func(data) : console.data(data));
+    }
+}
