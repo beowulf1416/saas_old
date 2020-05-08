01d47727ea485072fb537ef70c863bea00a19228
ferdinand <beowulf1416@gmail.com>
2020-05-08 15:23:53 +0800

    adding inventory items page

--- src/main/java/com/tomale/saas/modules/inventory/controllers/InventoryController.java
+++ src/main/java/com/tomale/saas/modules/inventory/controllers/InventoryController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -27,5 +28,11 @@ public class InventoryController {
     public String viewDashboard() {
         log.debug("VIEW: inventory.dashboard");
         return "inventory/default";
+    }
+
+    @GetMapping("/items")
+    @PreAuthorize("hasAuthority('inventory.items')")
+    public ModelAndView viewItems() {
+        return new ModelAndView("inventory/items");
     }
 }
