33a407984ecedf8b35e639f4f128d361e813092a
ferdinand <beowulf1416@gmail.com>
2020-05-08 15:24:16 +0800

    adding inventory items custom element

--- src/main/resources/public/static/js/components/inventory/items.table.js
+++ src/main/resources/public/static/js/components/inventory/items.table.js
@@ -15,11 +15,42 @@ class ItemsTable extends HTMLElement {
     }
 
     initTable(component, container) {
+        const div = document.createElement('div');
+        div.classList.add('tbl-wrapper');
+        div.innerHTML = `
+            <form class="form-items">
+                <table class="tbl-items">
+                    <thead>
+                        <tr>
+                            <th class="col-select"></th>
+                            <th class="col-name"></th>
+                        </tr>
+                        <tr class="row-filter">
+                            <th class="col-select">
+                            </th>
+                            <th class="col-name">
+                                <input type="search" id="nameFilter" name="nameFilter" class="form-input-search form-name" title="Filter Item Name" />
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+            </form>
+        `;
 
+        conatainer.appendChild(div);
     }
 
     setItems(items, options) {
+        const self = this;
+        if (Array.isArray(items)) {
 
+        } else {
+            self.dispatchEvent(new CustomEvent('error', {
+                bubbles: true,
+                cancelable: true,
+                detail: "Expecting an array of items";
+            }));
+        }
     }
 }
 customElements.define('items-table', ItemsTable);
