277b1e9f07320155a2560a5a2528354b34d6f334
ferdinand <beowulf1416@gmail.com>
2020-05-09 09:44:12 +0800

    modified inventory schema

--- data/sql/postgres/inventory/create.sql
+++ data/sql/postgres/inventory/create.sql
@@ -20,12 +20,16 @@ set schema 'inventory';
 \ir tables/item_locations.sql
 
 
+
 /** functions **/
 \ir procs/categories/category_add.sql
 
 \ir procs/items/item_add.sql
 \ir procs/items/item_update.sql
+
 \ir procs/items/items_all.sql
+\ir procs/items/items_by_name.sql
+
 
 
 set schema 'public';
