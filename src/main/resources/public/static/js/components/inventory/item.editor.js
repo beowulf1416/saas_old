fb8d031b23ed64d1301ada445e04f178063fc663
ferdinand <beowulf1416@gmail.com>
2020-05-09 15:42:37 +0800

    modified inventory items pag

--- src/main/resources/public/static/js/components/inventory/item.editor.js
+++ src/main/resources/public/static/js/components/inventory/item.editor.js
@@ -19,10 +19,35 @@ class ItemEditor extends HTMLElement {
         div.classList.add('form-wrapper', 'form-item-wrapper');
         div.innerHTML = `
             <form class="form-item">
+                <fieldset>
+                    <legend>General</legend>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" placeholder="Name" id="name" name="name" class="form-input-text form-name" />
+                    </div><!-- .form-group -->
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <input type="text" placeholder="Description" id="description" name="description" class="form-input-text form-desc" />
+                    </div><!-- .form-group -->
+                </fieldset>
+                <button type="button" id="btnSave" class="btn btn-save btn-default">Save</button>
+                <button type="button" id="btnCancel" class="btn btn-cancel">Cancel</button>
             </form>
         `;
 
-        container.appendChild(form);
+        container.appendChild(div);
+
+        const save = div.querySelector('button#btnSave');
+        save.addEventListener('click', function(e) {
+            const form = div.querySelector('form.form-item');
+            const data = new FormData(form);
+
+            self.dispatchEvent(new CustomEvent('onsaveitem', {
+                bubbles: true,
+                cancelable: true,
+                detail: data
+            }));
+        });
     }
 }
 customElements.define('item-editor', ItemEditor);
--- src/main/resources/public/static/js/components/inventory/items.table.js
+++ src/main/resources/public/static/js/components/inventory/items.table.js
@@ -13,6 +13,7 @@ class ItemsTable extends HTMLElement {
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.appendChild(div);
 
+        this.setOptions = this.setOptions.bind(this);
         this.setItems = this.setItems.bind(this);
     }
 
@@ -29,13 +30,14 @@ class ItemsTable extends HTMLElement {
                             <th class="col-name">Name</th>
                         </tr>
                         <tr class="row-filter">
-                            <th class="col-select">
-                            </th>
+                            <th class="col-select"></th>
                             <th class="col-name">
                                 <input type="search" id="nameFilter" name="nameFilter" class="form-input-search form-name" title="Filter Item Name" placeholder="Item Name"/>
                             </th>
                         </tr>
                     </thead>
+                    <tbody>
+                    </tbody>
                 </table>
             </form>
         `;
@@ -52,8 +54,36 @@ class ItemsTable extends HTMLElement {
         });
     }
 
-    setItems(items, options) {
+    setOptions(options) {
         const self = this;
+        const shadow = this.shadowRoot;
+        this.options = options;
+
+        const tbody = shadow.querySelector('table.tbl-items tbody');
+        let aAdd = tbody.querySelector("a#itemAdd");
+        if (aAdd == null) {
+            const tr = document.createElement('tr');
+            tr.classList.add('item-add');
+            tr.innerHTML = `
+                <td colspan="2">
+                    <a id="itemAdd" class="nav-link item-add-link" title="Add Item" href="#">Add</a>
+                </td>
+            `;
+            tbody.appendChild(tr);
+
+            aAdd = tbody.querySelector('a#itemAdd');
+            aAdd.addEventListener('click', function(e) {
+                self.dispatchEvent(new CustomEvent('onadditem', {
+                    bubbles: true,
+                    cancelable: true
+                }));
+            });
+        }
+    }
+
+    setItems(items) {
+        const self = this;
+        const options = this.options;
         const shadow = this.shadowRoot;
         if (Array.isArray(items)) {
             const tbl = shadow.querySelector('table.tbl-items');
