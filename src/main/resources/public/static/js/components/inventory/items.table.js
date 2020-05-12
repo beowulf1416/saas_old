c48a81c2eef1f8543a1a096e53bc0b94b4cc456c
ferdinand <beowulf1416@gmail.com>
2020-05-12 16:17:30 +0800

    switch from using options to attribs

--- src/main/resources/public/static/js/components/inventory/items.table.js
+++ src/main/resources/public/static/js/components/inventory/items.table.js
@@ -23,6 +23,11 @@ class ItemsTable extends HTMLElement {
     }
 
     initTable(component, container) {
+        const classSKU = component.hasAttribute('hide-sku') ? 'col-sku col-hidden' : 'col-sku';
+        const classUPC = component.hasAttribute('hide-upc') ? 'col-upc col-hidden' : 'col-upc';
+        const classQTY = component.hasAttribute('hide-qty') ? 'col-qty col-hidden' : 'col-qty';
+        const classOUM = component.hasAttribute('hide-uom') ? 'col-uom col-hidden' : 'col-uom';
+
         const div = document.createElement('div');
         div.classList.add('tbl-wrapper');
         div.innerHTML = `
@@ -34,6 +39,10 @@ class ItemsTable extends HTMLElement {
                             <th class="col-select"></th>
                             <th class="col-name">Name</th>
                             <th class="col-description">Description</th>
+                            <th class="${classSKU}">SKU</th>
+                            <th class="${classUPC}">UPC</th>
+                            <th class="${classQTY}">Quantity</th>
+                            <th class="${classOUM}">Unit</th>
                         </tr>
                         <tr class="row-filter">
                             <th class="col-select"></th>
@@ -97,6 +106,13 @@ class ItemsTable extends HTMLElement {
         const options = this.options;
         const shadow = this.shadowRoot;
         if (Array.isArray(items)) {
+            const showAdd = this.hasAttribute('show-add');
+            const multiselect = this.hasAttribute('multiselect');
+            const classSKU = this.hasAttribute('hide-sku') ? 'col-sku col-hidden' : 'col-sku';
+            const classUPC = this.hasAttribute('hide-sku') ? 'col-upc col-hidden' : 'col-upc';
+            const classQTY = this.hasAttribute('hide-qty') ? 'col-qty col-hidden' : 'col-qty';
+            const classUOM = this.hasAttribute('hide-uom') ? 'col-uom col-hidden' : 'col-uom';
+
             const tbl = shadow.querySelector('table.tbl-items');
             const tbody = tbl.querySelector('tbody');
             while(tbody.firstChild) {
@@ -115,7 +131,7 @@ class ItemsTable extends HTMLElement {
 
                 let tds = [];
 
-                if (options && options.multiselect == true) {
+                if (multiselect) {
                     tds.push(`<td class="col-select"><input type="checkbox" name="selectItem" title="Select Item" class="form-input-check form-item" value="${item.id}" /></td>`);
                 } else {
                     tds.push(`<td class="col-select"><input type="radio" name="selectItem" title="Select Item" class="form-input-check form-item" value="${item.id}" /></td>`);
@@ -123,16 +139,17 @@ class ItemsTable extends HTMLElement {
 
                 tds.push(`<td class="col-name">${item_name}</td>`);
                 tds.push(`<td class="col-description">${item.description}</td>`);
-                tds.push(`<td class="col-sku">${item.sku}</td>`);
-                tds.push(`<td class="col-upc">${item.upc}</td>`);
-                tds.push(`<td class="col-qty"></td>`);
+                tds.push(`<td class="${classSKU}">${item.sku}</td>`);
+                tds.push(`<td class="${classUPC}">${item.upc}</td>`);
+                tds.push(`<td class="${classQTY}"></td>`);
+                tds.push(`<td class="${classUOM}"></td>`);
 
                 tr.innerHTML = tds.join('');
                 
                 tbody.appendChild(tr);
             });
 
-            if (options && options.allowAdd == true) {
+            if (showAdd) {
                 const tr = document.createElement('tr');
                 tr.classList.add('item-add');
                 tr.innerHTML = `
