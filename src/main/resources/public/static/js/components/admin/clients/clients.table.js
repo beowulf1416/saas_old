188ad29acc4e3545bca16ff9f30af130e673aa1f
ferdinand <beowulf1416@gmail.com>
2020-05-13 15:30:00 +0800

    clean up

--- src/main/resources/public/static/js/components/admin/clients/clients.table.js
+++ src/main/resources/public/static/js/components/admin/clients/clients.table.js
@@ -146,9 +146,6 @@ class AdminClientsTable extends HTMLElement {
                         }
                     }));
                 });
-
-                
-                
             });
 
             if (showAdd) {
