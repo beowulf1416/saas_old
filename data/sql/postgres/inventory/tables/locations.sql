7b5859b3cf0f5c748d3f5dc6e44f83a390a94bcd
ferdinand <beowulf1416@gmail.com>
2020-05-14 21:23:35 +0800

    added rack_id column

--- data/sql/postgres/inventory/tables/locations.sql
+++ data/sql/postgres/inventory/tables/locations.sql
@@ -9,10 +9,11 @@ create table if not exists locations (
     warehouse_id uuid not null,
     floor_id varchar(100),
     shelf_id varchar(100),
+    rack_id varchar(100),
     level_id varchar(100),
     bin_id varchar(100),
     constraint pk_locations primary key (id),
-    constraint u_locations_1 unique (client_id, warehouse_id, floor_id, shelf_id, level_id, bin_id),
+    constraint u_locations_1 unique (client_id, warehouse_id, floor_id, shelf_id, rack_id, level_id, bin_id),
     constraint fk_locations_1 foreign key (client_id)
         references clients.clients (id) on delete restrict on update restrict,
     constraint fk_locations_2 foreign key (warehouse_id)
