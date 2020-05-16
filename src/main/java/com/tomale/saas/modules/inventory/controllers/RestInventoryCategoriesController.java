c19fa36d106c07f22d96b21baaf543c65842c1b3
ferdinand <beowulf1416@gmail.com>
2020-05-16 09:38:48 +0800

    modified permissions

--- src/main/java/com/tomale/saas/modules/inventory/controllers/RestInventoryCategoriesController.java
+++ src/main/java/com/tomale/saas/modules/inventory/controllers/RestInventoryCategoriesController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestInventoryCategoriesController {
     
     @PostMapping("/all")
-    @PreAuthorize("hasAuthority('inventory.categories')")
+    @PreAuthorize("hasAuthority('inventory.items')")
     public ApiResult getAll(@RequestBody Map<String, Object> params, HttpServletResponse response) {
         return new ApiResult(
             "success",
