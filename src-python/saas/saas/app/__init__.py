efcddaf7a0038fc0adc53eff14af8602e604d0de
ferdinand <beowulf1416@gmail.com>
2020-05-17 13:32:36 +0800

    adding session handling using cookies

--- src-python/saas/saas/app/__init__.py
+++ src-python/saas/saas/app/__init__.py
@@ -1,2 +1,15 @@
+import logging
+log = logging.getLogger(__name__)
+
+from pyramid.session import SignedCookieSessionFactory
+
+
 def includeme(config):
     config.include('saas.app.core')
+
+    settings = config.get_settings()
+    secret = settings['cookie.secret']
+    log.info(secret)
+
+    config.set_session_factory(SignedCookieSessionFactory(secret))
+    
