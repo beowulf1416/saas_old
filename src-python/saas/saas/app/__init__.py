a17465e4c16fa631e3b9a5cbe47bff52a43c75b2
ferdinand <beowulf1416@gmail.com>
2020-05-17 21:49:35 +0800

    adding database configuration

--- src-python/saas/saas/app/__init__.py
+++ src-python/saas/saas/app/__init__.py
@@ -1,6 +1,8 @@
 import logging
 log = logging.getLogger(__name__)
 
+import json
+
 from saas.app.core.session.jwt.session_factory import SessionFactory
 from saas.app.core.policies.authentication import AuthenticationPolicy
 from saas.app.core.policies.authorization import AuthorizationPolicy
@@ -10,6 +12,16 @@ def includeme(config):
     config.include('saas.app.core')
 
     settings = config.get_settings()
+
+    config_location = settings['app.config']
+    with open('{0}/databases.json'.format(config_location), 'r') as f:
+        data = json.loads(f.read())
+        databases = data['databases']
+        for db in databases:
+            dbtype = db['type']
+
+
+
     cookie_secret = settings['cookie.secret']
     cookie_max_age = settings['cookie.max_age']
     cookie_timeout = settings['cookie.timeout']
