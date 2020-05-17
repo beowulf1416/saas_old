6d853b834a11ca111a5413b12c301fd65932e6db
ferdinand <beowulf1416@gmail.com>
2020-05-17 21:32:07 +0800

    modified start up

--- src-python/saas/saas/app/__init__.py
+++ src-python/saas/saas/app/__init__.py
@@ -1,7 +1,7 @@
 import logging
 log = logging.getLogger(__name__)
 
-from pyramid.session import SignedCookieSessionFactory
+from saas.app.core.session.jwt.session_factory import SessionFactory
 from saas.app.core.policies.authentication import AuthenticationPolicy
 from saas.app.core.policies.authorization import AuthorizationPolicy
 
@@ -10,8 +10,14 @@ def includeme(config):
     config.include('saas.app.core')
 
     settings = config.get_settings()
-    secret = settings['cookie.secret']
+    cookie_secret = settings['cookie.secret']
+    cookie_max_age = settings['cookie.max_age']
+    cookie_timeout = settings['cookie.timeout']
 
-    config.set_session_factory(SignedCookieSessionFactory(secret))
+    config.set_session_factory(SessionFactory(
+        cookie_secret, 
+        cookie_max_age,
+        cookie_timeout
+    ))
     config.set_authentication_policy(AuthenticationPolicy())
     config.set_authorization_policy(AuthorizationPolicy())
