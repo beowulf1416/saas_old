79f2cb0b95bf8943055d408ec7df86fdfa3da84a
ferdinand <beowulf1416@gmail.com>
2020-05-18 18:29:52 +0800

    adding exception pages

--- src-python/saas/saas/app/core/views/__init__.py
+++ src-python/saas/saas/app/core/views/__init__.py
@@ -1,8 +1,18 @@
 import logging
-
 log = logging.getLogger(__name__)
 
+
 def includeme(config):
+    config.add_notfound_view(
+        view_not_found,
+        renderer = 'saas.app:templates/errors/404.html'
+    )
+
+    config.add_forbidden_view(
+        view_forbidden,
+        renderer = 'saas.app:templates/errors/403.html'
+    )
+
     config.add_route(
         'security.supported.signins',
         '/security/signin'
@@ -17,3 +27,12 @@ def includeme(config):
         'user.dashboard',
         '/dashboard'
     )
+
+
+def view_not_found(request):
+    log.debug("view not found")
+    return {}
+
+def view_forbidden(request):
+    log.debug("view forbidden")
+    return {}
