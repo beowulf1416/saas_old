2fffc680706eec331ae5d4e4986bbee877ee3b14
ferdinand <beowulf1416@gmail.com>
2020-05-18 10:01:22 +0800

    fix test

--- src-python/saas/saas/tests/test_user_store.py
+++ src-python/saas/saas/tests/test_user_store.py
@@ -21,9 +21,13 @@ class TestUserStore(unittest.TestCase):
 
 
     def test_email_exists(self):
-        [(result, ), ] = self.userStore.emailExists('beowulf1416@gmail.com')
+        result = self.userStore.emailExists('beowulf1416@gmail.com')
         self.assertEqual(True, result, '{0}'.format(result))
 
     def test_user_add(self):
         result = self.userStore.userAdd('test2@test2.com', 'test2')
         self.assertNotEqual(None, result)
+
+    def test_user_by_email(self):
+        result = self.userStore.userByEmail('test1@test1.com')
+        self.assertEqual(1, len(result), '{0}'.format(result))
