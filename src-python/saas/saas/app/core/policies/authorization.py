090d48f7c49fb5e405d8341502ee27780f67e767
ferdinand <beowulf1416@gmail.com>
2020-05-18 20:49:24 +0800

    modified authorization - incomplete

--- src-python/saas/saas/app/core/policies/authorization.py
+++ src-python/saas/saas/app/core/policies/authorization.py
@@ -3,12 +3,23 @@ log = logging.getLogger(__name__)
 
 from zope.interface import implementer
 from pyramid.interfaces import IAuthorizationPolicy
+from pyramid.security import Everyone, Authenticated
 
 
 @implementer(IAuthorizationPolicy)
 class AuthorizationPolicy(object):
+    def __init__(self, userStore):
+        self.userStore = userStore
+
     def permits(self, context, principals, permission):
         log.debug('//todo AuthorizationPolicy::permits()')
+        log.debug(context)
+        log.debug(principals)
+        log.debug(permission)
+
+        effective_principals = [x for x in principals if x not in [Everyone, Authenticated]]
+        log.debug(effective_principals)
+
         return None
 
     def principals_allowed_by_permission(self, context, permission):
