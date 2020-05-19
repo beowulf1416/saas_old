71b6c7a8a315a17f36d5de1a006f396c54d768c7
ferdinand <beowulf1416@gmail.com>
2020-05-19 20:50:25 +0800

    requiring include of admin views and api

--- src-python/saas/saas/app/modules/admin/__init__.py
+++ src-python/saas/saas/app/modules/admin/__init__.py
@@ -5,4 +5,6 @@ log = logging.getLogger(__name__)
 def includeme(config):
     log.info('including: saas.app.modules.admin')
 
-    config.include('saas.app.core.stores')
+    config.include('saas.app.modules.admin.stores')
+    config.include('saas.app.modules.admin.views')
+    config.include('saas.app.modules.admin.views.api')
