a22848811138f0270b3bf73c36f1f2b74a40169e
ferdinand <beowulf1416@gmail.com>
2020-05-19 15:21:02 +0800

    modified client selection pages

--- src-python/saas/saas/app/core/views/client.py
+++ src-python/saas/saas/app/core/views/client.py
@@ -2,6 +2,7 @@ import logging
 log = logging.getLogger(__name__)
 
 from pyramid.view import view_config
+import pyramid.httpexceptions as exception
 
 
 @view_config(
@@ -10,8 +11,14 @@ from pyramid.view import view_config
     request_method='GET'
 )
 def view_client_list(request):
+    destination = request.params['destination'] if 'destination' in request.params else ''
+
     session = request.session
-    email = session['email']
+    email = session['email'] if 'email' in session else ''
+
+    if email == '':
+        # user is not logged in, redirect to sign in page
+        raise exception.HTTPFound(request.route_url('security.signin'))
 
     services = request.services()
     userStore = services['store.user']
@@ -20,13 +27,22 @@ def view_client_list(request):
     clients = userStore.userClients(user_id)
 
     return {
+        'destination': destination,
         'clients': clients
     }
 
 @view_config(
     route_name='client.select',
-    renderer='saas.app:templates/client/select.html',
-    request_method='GET'
+    # renderer='saas.app:templates/client/select.html',
+    request_method='GET',
+    # match_param='client_id'
 )
 def view_client_select(request):
+    destination = request.params['destination'] if 'destination' in request.params else request.route_url('user.dashboard')
+    client_id = request.matchdict['client_id'] if 'client_id' in request.matchdict else ''
+
+    session = request.session
+    session['client'] = client_id
+
+    raise exception.HTTPFound(destination)
     return {}
