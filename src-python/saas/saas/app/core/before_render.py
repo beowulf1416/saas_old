1fe94baec6f811f290c7bfca6168a3d02abc1c75
ferdinand <beowulf1416@gmail.com>
2020-05-19 14:33:59 +0800

    adding client to global renderer variables

--- src-python/saas/saas/app/core/before_render.py
+++ src-python/saas/saas/app/core/before_render.py
@@ -7,7 +7,14 @@ from pyramid.events import BeforeRender
 @subscriber(BeforeRender)
 def add_renderer_globals(event):
     request = event['request']
+    services = request.services()
     session = request.session
 
     if 'email' in session:
         event['email'] = session['email']
+
+    if 'client' in session:
+        client_id = session['client']
+        clientStore = services['store.client']
+        client = clientStore.getClient(client_id)
+        event['client_name'] = client[2]
