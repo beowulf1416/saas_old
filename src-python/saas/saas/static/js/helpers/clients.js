aae084b8b909d7ed0192713804f7d6815d7f5138
ferdinand <beowulf1416@gmail.com>
2020-05-20 10:50:05 +0800

    modified helpers and custom elements

--- src-python/saas/saas/static/js/helpers/clients.js
+++ src-python/saas/saas/static/js/helpers/clients.js
@@ -11,5 +11,20 @@ class Clients {
         })
         .then((r) => r.json());
     }
+
+    static setActive(clientId, active) {
+        return fetch('/api/clients/active', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                clientId: clientId,
+                active: active
+            })
+        })
+        .then((r) => r.json());
+    }
 }
 export { Clients };
