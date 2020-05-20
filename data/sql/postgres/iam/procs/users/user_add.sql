faec8cabe25ee1b8f5b1a60fae69aab68a744289
ferdinand <beowulf1416@gmail.com>
2020-05-20 18:21:35 +0800

    modified user_app sp.

--- data/sql/postgres/iam/procs/users/user_add.sql
+++ data/sql/postgres/iam/procs/users/user_add.sql
@@ -1,3 +1,6 @@
+/**
+ * add user account
+ */
 create or replace function user_add (
     p_email iam.users.email%type,
     p_name iam.users.name%type
@@ -5,7 +8,9 @@ create or replace function user_add (
 returns iam.users.id%type
 as $$
 declare
-    tmpId iam.users.id%type;
+    t_user_id iam.users.id%type;
+    default_client_id clients.clients.id%type;
+    default_role_id iam.roles.id%type;
 begin
     insert into iam.users (
         email,
@@ -16,9 +21,44 @@ begin
         p_name,
         true
     ) 
-    returning id into tmpId;
+    returning id into t_user_id;
 
-    return tmpId;
+    -- retrieve default client id
+    select
+        a.id into default_client_id
+    from clients.clients a
+    where a.name = 'default';
+
+    -- add user to default client
+    insert into iam.user_clients (
+        user_id,
+        client_id
+    ) values (
+        t_user_id,
+        default_client_id
+    )
+    on conflict do nothing;
+
+    -- retrieve 'everyone' role
+    select
+        a.id into default_role_id
+    from iam.roles a
+    where a.client_id = default_client_id
+        and a.name = 'everyone';
+
+    -- assign role to user
+    insert into iam.role_users (
+        client_id,
+        role_id,
+        user_id
+    ) values (
+        default_client_id,
+        default_role_id,
+        t_user_id
+    )
+    on conflict do nothing;
+
+    return t_user_id;
 end
 $$
 language plpgsql;
