8bec81a4b91c2d8cf802f85a589e87d154b3abcf
ferd <beowulf1416@gmail.com>
2020-05-23 06:53:33 +0800

    modified countries table

--- data/sql/postgres/common/create.sql
+++ data/sql/postgres/common/create.sql
@@ -8,6 +8,14 @@ set schema 'common';
 \ir tables/currencies.sql
 \ir tables/uom.sql
 
+-- https://www.postgresql.org/docs/12/app-psql.html
+\copy common.countries (@exclude, @exclude, name, code) \
+from '../csv/countries.csv' 
+with option \
+format csv \
+header true \
+quote '"' \
+
 
 
 set schema 'public';
--- data/sql/postgres/common/tables/countries.sql
+++ data/sql/postgres/common/tables/countries.sql
@@ -1,19 +1,14 @@
 create table if not exists countries (
     id int not null,
     name varchar(100) not null,
-    code varchar(5) not null,
+    iso_3166_1_alpha_2 varchar(5) not null,
+    iso_3166_1_alpha_3 varchar(5) not null,
+    iso_3166_1_numeric int not null,
+    iso_4217_currency_alpha varchar(5) not null,
+    iso_4217_currency_country_name varchar(100) not null,
+    iso_4217_currency_minor_unit varchar(100) not null,
+    iso_4217_currency_name varchar(100) not null,
+    iso_4217_currency_numeric int not null,
     constraint pk_countries primary key (id),
     constraint u_countries unique (name)
 );
-
-do $$
-begin
-    -- https://www.postgresql.org/docs/9.2/sql-copy.html
-    copy countries 
-    from '../csv/countries.csv'
-    option 
-        format csv
-        header true;
-end
-$$
-language plpgsql;
