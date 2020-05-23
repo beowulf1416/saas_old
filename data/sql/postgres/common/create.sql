e40bfb24441b090eeff7b0971b06179e25dbced7
ferdinand <beowulf1416@gmail.com>
2020-05-23 17:12:12 +0800

    trim data from countries csv. modified import.

--- data/sql/postgres/common/create.sql
+++ data/sql/postgres/common/create.sql
@@ -9,13 +9,7 @@ set schema 'common';
 \ir tables/uom.sql
 
 -- https://www.postgresql.org/docs/12/app-psql.html
-\copy common.countries (id, name) \
-from '../csv/countries.csv' 
-with option \
-format csv \
-header true \
-quote '"'
-
+\copy common.countries (name,iso_3166_1_alpha_2,iso_3166_1_alpha_3,iso_3166_1_numeric,iso_4217_currency_alpha,iso_4217_currency_country_name,iso_4217_currency_minor_unit,iso_4217_currency_name,iso_4217_currency_numeric) from '../../csv/countries.csv' with delimiter ',' csv header quote '"'
 
 \copy common.currencies (id, name) \
 from '../csv/currencies.csv' \
