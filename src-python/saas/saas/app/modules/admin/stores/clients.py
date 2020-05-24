adb8a2007890a1db16029afd76722cc80b932599
ferdinand <beowulf1416@gmail.com>
2020-05-24 16:37:28 +0800

    modified clients store. modified test.

--- src-python/saas/saas/app/modules/admin/stores/clients.py
+++ src-python/saas/saas/app/modules/admin/stores/clients.py
@@ -2,78 +2,51 @@ import logging
 log = logging.getLogger(__name__)
 
 from saas.app.core.services.connection import ConnectionManager
+from saas.app.core.stores.base import BaseStore
 
 
-class ClientsStore(object):
+class ClientsStore(BaseStore):
 
     def __init__(self, manager: ConnectionManager, name: str):
-        self._mgr = manager
-        self._name = name
+        super(ClientsStore, self).__init__(manager, name)
+        # self._mgr = manager
+        # self._name = name
 
     def getAll(self):
-        cn = self._mgr.getConnection(self._name)
         try:
-            c = cn.cursor()
-            c.callproc('clients.clients_all')
-            result = c.fetchall()
+            result = super(ClientsStore, self).runProc('clients.clients_all', [])
             return result
         except Exception as e:
             log.error(e)
             raise Exception('An error occured while retrieving all clients')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
 
     def add(self, name: str, address: str, url: str):
-        cn = self._mgr.getConnection(self._name)
         try:
-            c = cn.cursor()
-            c.callproc('clients.client_add', [name, address, url])
-            cn.commit()
-            [(client_id, )] = c.fetchall()
+            [(client_id, )] = super(ClientsStore, self).runProcTransactional('clients.client_add', [name, address, url])
             return client_id
         except Exception as e:
-            cn.rollback()
-            log.error(e)
             raise Exception('An error occured while adding a client')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
 
     def get(self, client_id: str):
-        cn = self._mgr.getConnection(self._name)
         try:
-            c = cn.cursor()
-            c.callproc('clients.clients_get', [client_id, ])
-            (client, ) = c.fetchall()
+            (client, ) = super(ClientsStore, self).runProc('clients.clients_get', [client_id, ])
             return client
         except Exception as e:
             log.error(e)
             raise Exception('An error occured while retrieving client')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
 
     def getByUrlName(self, url_name: str):
-        cn = self._mgr.getConnection(self._name)
         try:
-            c = cn.cursor()
-            c.callproc('clients.client_by_url_name', [url_name, ])
-            [client, ] = c.fetchall()
+            (client, ) = super(ClientsStore, self).runProc('clients.client_by_url_name', [url_name, ])
             return client
         except Exception as e:
             log.error(e)
             raise Exception('An error occured while retrieving client by url name')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
 
     def setActive(self, clientId: str, active: bool):
-        cn = self._mgr.getConnection(self._name)
         try:
-            c = cn.cursor()
-            c.callproc('clients.client_set_active', [clientId, active])
-            cn.commit()
+            super(ClientsStore, self).runProcTransactional('clients.client_set_active', [clientId, active])
         except Exception as e:
-            cn.rollback()
             log.error(e)
             raise Exception('An error occured while setting client active status')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
         
