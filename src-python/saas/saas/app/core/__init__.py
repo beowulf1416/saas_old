aff804e4727873b6911794e39ab66f2623b6c27d
ferdinand <beowulf1416@gmail.com>
2020-05-25 20:20:22 +0800

    added datetime json renderer

--- src-python/saas/saas/app/core/__init__.py
+++ src-python/saas/saas/app/core/__init__.py
@@ -2,8 +2,19 @@ import logging
 
 log = logging.getLogger(__name__)
 
+import json
+from pyramid.renderers import JSON
+import datetime
+
+
 def includeme(config):
     log.info('including: saas.app.core')
+
+    json_renderer = JSON()
+    def datetime_adapter(obj, request):
+        return obj.isoformat()
+    json_renderer.add_adapter(datetime.datetime, datetime_adapter)
+    config.add_renderer('json', json_renderer)
 
     config.include('saas.app.core.services')
     config.include('saas.app.core.stores')
