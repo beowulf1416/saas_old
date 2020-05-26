d05f9d6b462a4be96a6c67ee08f153d034cb7d36
ferdinand <beowulf1416@gmail.com>
2020-05-27 06:51:54 +0800

    modified accounting schema

--- data/sql/postgres/accounting/procs/accounts/account_assign_parent.sql
+++ data/sql/postgres/accounting/procs/accounts/account_assign_parent.sql
@@ -5,16 +5,35 @@ create or replace function account_assign_parent (
 )
 returns void
 as $$
+declare
+    acct_type_id accounting.accounts.type_id%type;
+    parent_acct_type_id accounting.accounts.type_id%type;
 begin
-    insert into accounting.account_tree (
-        client_id,
-        acct_id,
-        parent_acct_id
-    ) values (
-        p_client_id,
-        p_acct_id,
-        p_parent_acct_id
-    );
+    select
+        a.type_id into acct_type_id
+    from accounting.accounts a
+    where a.client_id = p_client_id
+        and a.id = p_acct_id;
+
+    select
+        a.type_id into parent_acct_type_id
+    from accounting.accounts a
+    where a.client_id = p_client_id
+        and a.id = p_parent_acct_id;
+    
+    if acct_type_id == parent_acct_type_id then
+        insert into accounting.account_tree (
+            client_id,
+            acct_id,
+            parent_acct_id
+        ) values (
+            p_client_id,
+            p_acct_id,
+            p_parent_acct_id
+        );
+    else
+        raise exception 'cannot assign parent account of type %s to account of type %s', parent_acct_type_id, acct_type_id;
+    endif;
 end
 $$
 language plpgsql;
--- data/sql/postgres/accounting/tables/account_tree.sql
+++ data/sql/postgres/accounting/tables/account_tree.sql
@@ -10,6 +10,7 @@ create table if not exists account_tree (
     constraint fk_account_tree_2 foreign key (acct_id)
         references accounts (id) on delete restrict on update restrict,
     constraint fk_account_tree_3 foreign key (parent_acct_id)
-        references accounts (id) on delete restrict on update restrict
+        references accounts (id) on delete restrict on update restrict,
+    constraint chk_account_tree_1 check (acct_id != parent_acct_id)
 );
 create index idx_account_tree_1 on account_tree using gist (path);
