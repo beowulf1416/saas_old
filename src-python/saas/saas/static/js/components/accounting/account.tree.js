f2b4e193a4107e4c83d293ff0c9c49342f15e04f
ferdinand <beowulf1416@gmail.com>
2020-05-27 21:24:13 +0800

    modified account list page

--- src-python/saas/saas/static/js/components/accounting/account.tree.js
+++ src-python/saas/saas/static/js/components/accounting/account.tree.js
@@ -21,7 +21,7 @@ class AccountTree extends HTMLElement {
         shadow.appendChild(div);
 
         this.addAccounts = this.addAccounts.bind(this);
-        this.setAccountTypes = this.setAccountTypes.bind(this);
+        // this.setAccountTypes = this.setAccountTypes.bind(this);
     }
 
     init(component, container) {
@@ -31,11 +31,25 @@ class AccountTree extends HTMLElement {
         if (showAdd) {
             add = '<a class="nav-link link-add-account" title="Add Account" href="#">&plus;</a>';
         }
-
+        
         const ths = [];
         ths.push(`<th scope="col" class="column col-name">Name</th>`);
         ths.push(`<th scope="col" class="column col-description">Description</th>`);
         const thall = ths.join('');
+        const col_count = ths.length;
+
+        const account_types = ['asset', 'liability', 'equity', 'income', 'expense'];
+        const tbodies = [];
+        account_types.forEach(t => {
+            tbodies.push(`
+                <tbody id="${t}">
+                    <tr class="tbody-header">
+                        <td colspan="${col_count}" role="row" aria-level="1">${t}</td>
+                    </tr>
+                </tbody>
+            `);
+        });
+        const tbodyall = tbodies.join('');
 
         const div = document.createElement('div');
         div.classList.add('table-wrapper');
@@ -51,8 +65,7 @@ class AccountTree extends HTMLElement {
                         ${thall}
                     </tr>
                 </thead>
-                <tbody>
-                </tbody>
+                ${tbodyall}
                 <tfoot>
                     <tr>
                         <td>${add}</td>
@@ -73,63 +86,68 @@ class AccountTree extends HTMLElement {
         }
     }
 
-    setAccountTypes(types = []) {
-        const self = this;
-        const shadow = this.shadowRoot;
-        const tbody = shadow.querySelector('table.tbl-accounts tbody');
-        types.forEach(t => {
-            const tds = [];
-            tds.push(`<td role="gridcell" class="col col-name"><a class="link-nav link-select-type" href="#" title="${t.name}" data-typeid="${t.id}">${t.name}</a></td>`);
-            tds.push(`<td role="gridcell" class="col col-description"></td>`);
-            const tdall = tds.join('');
-
-            const tr = document.createElement('tr');
-            tr.classList.add('row-account-type');
-            tr.setAttribute('role', 'row');
-            tr.setAttribute('aria-level', 1);
-            tr.setAttribute('aria-posinset', 1);
-            tr.setAttribute('aria-setsize', 1);
-            tr.setAttribute('aria-expanded', true);
-            tr.innerHTML = `${tdall}`;
-
-            tbody.appendChild(tr);
-
-            const ltype = tr.querySelector('a.link-select-type');
-            ltype.addEventListener('click', function(e) {
-                self.dispatchEvent(new CustomEvent('onselectaccounttype', {
-                    bubbles: true,
-                    cancelable: true,
-                    detail: {
-                        accountTypeId: ltype.dataset.typeid
-                    }
-                }));
-            });
-        });
-    }
-
     addAccounts(accounts = [], parent = null) {
         const self = this;
         const shadow = this.shadowRoot;
-        const tbody = shadow.querySelector('table.tbl-accounts tbody');
         if (parent == null) {
             accounts.forEach(a => {
-                const tds = [];
-                tds.push(`<td role="gridcell" class="col col-name">${a.name}</td>`);
-                tds.push(`<td role="gridcell" class="col col-description">${a.description}</td>`);
-                const tdall = tds.join('');
+                let tbody = null;
+                const type_id = a.type_id;
+                console.log(type_id);
+                switch(type_id) {
+                    case 1: { // asset
+                        tbody = shadow.querySelector('table.tbl-accounts tbody#asset');
+                        break;
+                    }
+                    case 2: { // liabilities
+                        tbody = shadow.querySelector('table.tbl-accounts tbody#liability');
+                        break;
+                    }
+                    case 3: { // equity
+                        tbody = shadow.querySelector('table.tbl-accounts tbody#equity');
+                        break;
+                    }
+                    case 4: { // income
+                        tbody = shadow.querySelector('table.tbl-accounts tbody#income');
+                        break;
+                    }
+                    case 5: { // expense
+                        tbody = shadow.querySelector('table.tbl-accounts tbody#expense');
+                        break;
+                    }
+                    default: {
+                        self.dispatchEvent(new CustomEvent('onerror', {
+                            bubbles: true,
+                            cancelable: true,
+                            detail: {
+                                message: `unknown account type: ${type_id}`
+                            }
+                        }));
+                        break;
+                    }
+                }
 
-                const tr = document.createElement('tr');
-                tr.classList.add('row-account');
-                tr.setAttribute('role', 'row');
-                tr.setAttribute('aria-level', 1);
-                tr.setAttribute('aria-posinset', 1);
-                tr.setAttribute('aria-setsize', 1);
-                tr.setAttribute('aria-expanded', true);
-                tr.innerHTML = `
-                    ${tdall}
-                `;
+                console.log(tbody);
+                if (tbody != null) {
+                    const tds = [];
+                    tds.push(`<td role="gridcell" class="col col-name">${a.name}</td>`);
+                    tds.push(`<td role="gridcell" class="col col-description">${a.description}</td>`);
+                    const tdall = tds.join('');
 
-                tbody.appendChild(tr);
+                    const tr = document.createElement('tr');
+                    tr.classList.add('row-account');
+                    tr.setAttribute('role', 'row');
+                    tr.setAttribute('aria-level', a.level);
+                    tr.setAttribute('aria-posinset', 1);
+                    tr.setAttribute('aria-setsize', 1);
+                    tr.setAttribute('aria-expanded', true);
+                    tr.innerHTML = `
+                        ${tdall}
+                    `;
+
+                    tbody.appendChild(tr);
+                    console.log('here');
+                }
             });
         } else {
             self.dispatchEvent(new CustomEvent('onerror', {
