f45903b17bd9ab664b1754af6cf8dacf52ceefea
ferdinand <beowulf1416@gmail.com>
2020-05-29 19:24:09 +0800

    modified user store - incomplete

--- src-python/saas/saas/app/core/stores/user.py
+++ src-python/saas/saas/app/core/stores/user.py
@@ -2,41 +2,33 @@ import logging
 log = logging.getLogger(__name__)
 
 from saas.app.core.services.connection import ConnectionManager
+from saas.app.core.stores.base import BaseStore
 
 
-class UserStore(object):
+class UserStore(BaseStore):
 
     def __init__(self, manager: ConnectionManager, name: str):
-        self._mgr = manager
-        self._name = name
+        super(UserStore, self).__init__(manager, name)
 
     def emailExists(self, email: str):
-        cn = self._mgr.getConnection(self._name)
+        '''check if email is already registered
+        '''
         try:
-            c = cn.cursor()
-            c.callproc('iam.user_email_exists', [email, ])
-            [(result, ), ] = c.fetchall()
+            [(result, )] = super(UserStore, self).runProc('iam.user_email_exists', [email, ])
             return result
         except Exception as e:
             log.error(e)
-            raise Exception('An error occured while checking if email exists')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
+            raise Exception('Unable to check if email exists')
 
     def userAdd(self, email: str, name: str):
-        cn = self._mgr.getConnection(self._name)
+        ''' add user
+        '''
         try:
-            c = cn.cursor()
-            c.callproc('iam.user_add', [email, name])
-            cn.commit()
-            [(user_id, )] = c.fetchall()
+            [(user_id, )] = super(UserStore, self).runProcTransactional('iam.user_add', [email, name])
             return user_id
         except Exception as e:
-            cn.rollback()
             log.error(e)
-            raise Exception('An error occured while adding user')
-        finally:
-            self._mgr.returnConnection(self._name, cn)
+            raise Exception('Unable to add user')
         
 
     def userByEmail(self, email: str):
