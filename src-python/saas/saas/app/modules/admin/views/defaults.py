1d4a1574f31654c2e0372d48109c43db719a9e69
ferdinand <beowulf1416@gmail.com>
2020-06-01 15:47:32 +0800

    temporarily disable permission check

--- src-python/saas/saas/app/modules/admin/views/defaults.py
+++ src-python/saas/saas/app/modules/admin/views/defaults.py
@@ -7,7 +7,8 @@ from pyramid.view import view_config
 @view_config(
     route_name='admin.dashboard',
     request_method='GET',
-    renderer='saas.app.modules.admin:templates/default.html'
+    renderer='saas.app.modules.admin:templates/default.html',
+    # permission='admin.dashboard' # todo need to specify a permission
 )
 def view_dashboard(request):
     log.debug('view: view_dashboard')
