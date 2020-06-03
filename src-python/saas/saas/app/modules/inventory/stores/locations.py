66ae3c77826d36e705d5ee552d60e72f44ab4343
ferdinand <beowulf1416@gmail.com>
2020-06-03 21:15:57 +0800

    added inventory location store. added test.

--- src-python/saas/saas/app/modules/inventory/stores/locations.py
+++ src-python/saas/saas/app/modules/inventory/stores/locations.py
@@ -15,11 +15,11 @@ class LocationStore(BaseStore):
     def add(self, clientId: UUID, warehouseId: UUID, floor_id: str, shelf_id: str,
         rack_id: str, level_id: str, bin_id: str):
         try:
-            result = super(LocationStore, self).runProcTransactional(
-                'inventory.locations_add',
-                [clientId, ]
+            [(location_id, )] = super(LocationStore, self).runProcTransactional(
+                'inventory.location_add',
+                [clientId, warehouseId, floor_id, shelf_id, rack_id, level_id, bin_id]
             )
-            return result
+            return location_id
         except Exception as e:
             log.error(e)
             raise Exception('Unable to add inventory location')
---
+++ src-python/saas/tests/inventory/test_inventory_locations_store.py
@@ -0,0 +1,47 @@
+import unittest
+
+from pyramid import testing
+
+import string
+import random
+from uuid import UUID
+
+
+class TestInventoryItemsStore(unittest.TestCase):
+
+    def setUp(self):
+        self.config = testing.setUp()
+
+        from saas.app.core.services.connection import ConnectionManager
+        from saas.app.modules.inventory.stores.warehouses import WarehouseStore
+        from saas.app.modules.inventory.stores.locations import LocationStore
+        from saas.app.core.stores.client import ClientStore
+
+        self.mgr = ConnectionManager({
+            'app.config': '../../etc'
+        })
+        self.warehouseStore = WarehouseStore(self.mgr, 'default')
+        self.locationStore = LocationStore(self.mgr, 'default')
+        self.clientStore = ClientStore(self.mgr, 'default')
+
+    def generate_random_str(self, length: int):
+        allowed = string.ascii_lowercase + string.digits
+        return ''.join(random.choice(allowed) for i in range(length))
+
+    def test_location_add(self):
+        random_str = self.generate_random_str(10)
+        (client_id, active, name, address, url_name)  = self.clientStore.getDefaultClient()
+        try:
+            warehouse_id = self.warehouseStore.add(client_id, random_str, random_str)
+            location_id = self.locationStore.add(
+                client_id, 
+                warehouse_id,
+                random_str,
+                random_str,
+                random_str,
+                random_str,
+                random_str 
+            )
+            test = UUID(location_id)
+        except Exception as e:
+            self.fail(e)
