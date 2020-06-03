7d931718d52eecceeecf1ff6db75be529d104522
ferdinand <beowulf1416@gmail.com>
2020-06-03 08:56:48 +0800

    caching parsed schemas

--- src-python/saas/saas/app/core/services/validator.py
+++ src-python/saas/saas/app/core/services/validator.py
@@ -8,13 +8,19 @@ import json
 class SchemaValidator(object):
     def __init__(self, schema_path: str):
         self._schema_path = schema_path
+        self._schemas = {}
 
     def validate(self, schema_file: str, instance: object):
         try:
-            with open('{0}/{1}'.format(self._schema_path, schema_file)) as f:
-                data = f.read()
-                schema = json.loads(data)
-                jsonschema.validate(instance, schema)
+            schema = ''
+            if schema_file in self._schemas:
+                schema = self._schemas[schema_file]
+            else:
+                with open('{0}/{1}'.format(self._schema_path, schema_file)) as f:
+                    data = f.read()
+                    schema = json.loads(data)
+                    
+            jsonschema.validate(instance, schema)
         except ValidationError as e:
             log.error(e)
             raise Exception(e.message)
