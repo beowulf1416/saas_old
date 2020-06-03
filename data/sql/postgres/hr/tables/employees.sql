64abb6e1e16cd48957463dc6557dffbaf98f8db6
ferdinand <beowulf1416@gmail.com>
2020-06-03 10:29:25 +0800

    modified employees table

--- data/sql/postgres/hr/tables/employees.sql
+++ data/sql/postgres/hr/tables/employees.sql
@@ -6,13 +6,11 @@ create table if not exists employees (
     active boolean not null default false,
     created_ts timestamp with time zone not null default(now() at time zone 'utc'),
     client_id uuid not null,
-    first_name varchar(100) not null,
-    middle_name varchar(100) not null,
-    last_name varchar(100) not null,
-    prefix varchar(100),
-    suffix varchar(100),
+    people_id uuid not null,
     constraint pk_employees primary key (id),
-    constraint u_employees_1 unique (client_id, first_name, middle_name, last_name, prefix, suffix),
+    constraint u_employees_1 unique (client_id, people_id),
     constraint fk_employees_1 foreign key (client_id)
-        references clients.clients (id) on delete restrict on update restrict
+        references clients.clients (id) on delete restrict on update restrict,
+    constraint fk_employees_2 foreign key (people_id)
+        references crm.people (id) on delete restrict on update restrict
 );
