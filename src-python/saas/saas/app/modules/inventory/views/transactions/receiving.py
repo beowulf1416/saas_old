926ae02ca18ec7d08797cc89c1d98edab84e2b9d
ferdinand <beowulf1416@gmail.com>
2020-06-04 19:07:19 +0800

    modified receiving page

--- src-python/saas/saas/app/modules/inventory/views/transactions/receiving.py
+++ src-python/saas/saas/app/modules/inventory/views/transactions/receiving.py
@@ -6,7 +6,7 @@ from pyramid.view import view_config
 @view_config(
     route_name='inventory.transactions.receiving',
     request_method='GET',
-    renderer='saas.app.modules.inventory:templates/default.html'
+    renderer='saas.app.modules.inventory:templates/transactions/receiving.html'
 )
 def view_inv_transaactions_receiving(request):
     return {}
