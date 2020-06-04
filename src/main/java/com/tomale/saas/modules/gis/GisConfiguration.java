873065b8ea5b5963a33352600b32f7c129f9b63c
ferdinand <beowulf1416@gmail.com>
2020-06-04 19:23:31 +0800

    remove java project files

--- src/main/java/com/tomale/saas/modules/gis/GisConfiguration.java
+++
@@ -1,5 +0,0 @@
-package com.tomale.saas.modules.gis;
-
-public class GisConfiguration {
-    
-}
--- src/main/resources/public/static/js/util.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class Util {
-
-    static generateId() {
-        return Math.random().toString(16).slice(10);
-    }
-}
-export { Util };
