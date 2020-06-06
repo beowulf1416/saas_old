4e505a5a1cfff6b5b53cd2b7fb7255665122b0e8
ferdinand <beowulf1416@gmail.com>
2020-06-06 12:12:15 +0800

    adding clients package to navigators

--- src/saas/saas/app/modules/clients/__init__.py
+++ src/saas/saas/app/modules/clients/__init__.py
@@ -1,10 +1,22 @@
 import logging
 log = logging.getLogger(__name__)
 
+from saas.app.core.services import get_service
+
+
 def includeme(config):
     log.info('including: saas.app.modules.clients')
 
     config.include('saas.app.modules.clients.stores')
     config.include('saas.app.modules.clients.views')
     config.include('saas.app.modules.clients.views.api')
+
+    services = get_service(None)
     
+    navigators = services['navigators']
+    navigators['client'] = {
+        'title': 'Client Management',
+        'help': 'Client management',
+        'icon': '<span class="material-icons">business</span>',
+        'template': 'saas.app.modules.clients:templates/module.html'
+    }
