4fdb179a3bfe8ed2c8c494e253aa12f20d3ed34e
ferdinand <beowulf1416@gmail.com>
2020-06-06 12:04:29 +0800

    modified accounting

--- src/saas/saas/app/modules/accounting/__init__.py
+++ src/saas/saas/app/modules/accounting/__init__.py
@@ -1,9 +1,21 @@
 import logging
 log = logging.getLogger(__name__)
 
+from saas.app.core.services import get_service
+
 def includeme(config):
     log.info('including: saas.app.modules.accounting')
 
     config.include('saas.app.modules.accounting.stores')
     config.include('saas.app.modules.accounting.views')
     config.include('saas.app.modules.accounting.views.api')
+
+    services = get_service(None)
+
+    navigators = services['navigators']
+    navigators['accounting'] = {
+        'title': 'Accounting',
+        'help': 'Accounting',
+        'icon': '<span class="material-icons">account_balance</span>',
+        'template': 'saas.app.modules.accounting:templates/module.html'
+    }
