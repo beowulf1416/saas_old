e572dc44de1973becf93f270121dedd0e3b301f5
ferdinand <beowulf1416@gmail.com>
2020-06-07 21:30:15 +0800

    modified tab container custom element

--- src/saas/saas/static/custom.elements/tab.container/tab.container.js
+++ src/saas/saas/static/custom.elements/tab.container/tab.container.js
@@ -18,6 +18,7 @@ class TabContainer extends HTMLElement {
         shadow.appendChild(style);
         shadow.appendChild(div);
 
+        this.setActiveTab = this.setActiveTab.bind(this);
         this.addTab = this.addTab.bind(this);
     }
 
@@ -34,66 +35,102 @@ class TabContainer extends HTMLElement {
         container.appendChild(div);
     }
 
+    setActiveTab(id = '') {
+        if (id != '') {
+            const self = this;
+            const shadow = this.shadowRoot;
+
+            const atab = shadow.querySelector('a[role=tab][aria-selected=true]');
+            if (atab != null) {
+                atab.setAttribute('aria-selected', 'false');
+            }
+
+            const ptab = shadow.querySelector('div.tab-panel.active');
+            if (ptab != null) {
+                ptab.classList.remove('active');
+            }
+
+            const selectedTab = shadow.querySelector(`a#link-${id}`);
+            if (selectedTab != null) {
+                selectedTab.setAttribute('aria-selected', true);
+            }
+
+            const selectedPanel = shadow.getElementById(`tabpanel-${id}`);
+            if (selectedPanel != null) {
+                selectedPanel.classList.add('active');
+            }
+        }
+    }
+
     addTab(id = '', title = '', content = '') {
-        const self = this;
-        const shadow = this.shadowRoot;
+        if (id != '' && title != '' && content != "") {
+            const self = this;
+            const shadow = this.shadowRoot;
 
-        const tabs = shadow.querySelector('.tabs');
-        const ul = shadow.querySelector('[role=tablist]')
+            const tabs = shadow.querySelector('.tabs');
+            const ul = shadow.querySelector('[role=tablist]')
 
-        const li = document.createElement('li');
-        li.classList.add('list-item');
-        li.innerHTML = `
-            <a  id="link-${id}"
-                class="tab-link"
-                role="tab"
-                aria-selected="true"
-                aria-controls="tabpanel-${id}"
-                href="#">
-                ${title}
-            </a>
-        `;
+            // check if tab already exists
+            const existingTab = shadow.getElementById(`link-${id}`);
+            console.log(existingTab);
+            if (existingTab == null) {
+                const li = document.createElement('li');
+                li.classList.add('list-item');
+                li.innerHTML = `
+                    <a  id="link-${id}"
+                        class="tab-link"
+                        role="tab"
+                        aria-selected="true"
+                        aria-controls="tabpanel-${id}"
+                        href="#">
+                        ${title}
+                    </a>
+                `;
 
-        const div = document.createElement('div');
-        div.classList.add('tab-panel', 'active');
-        div.setAttribute('role', 'tabpanel');
-        div.setAttribute('id', `tabpanel-${id}`);
-        div.innerHTML = `
-            <div class="content-wrapper">
-                ${content}
-            </div><!-- .content-wrapper -->
-        `;
+                const div = document.createElement('div');
+                div.classList.add('tab-panel', 'active');
+                div.setAttribute('role', 'tabpanel');
+                div.setAttribute('id', `tabpanel-${id}`);
+                div.innerHTML = `
+                    <div class="content-wrapper">
+                        ${content}
+                    </div><!-- .content-wrapper -->
+                `;
 
-        const tab = shadow.querySelector('.tabs [role=tablist] .tab-link[aria-selected=true]');
-        if (tab != null) {
-            tab.setAttribute['aria-selected', 'false'];
-        }
+                const tab = shadow.querySelector('.tabs [role=tablist] .tab-link[aria-selected=true]');
+                if (tab != null) {
+                    tab.setAttribute['aria-selected', 'false'];
+                }
 
-        const tp = shadow.querySelector('.tabs [role=tabpanel].active');
-        if (tp != null) {
-            tp.classList.remove('active');
-        }
+                const tp = shadow.querySelector('.tabs [role=tabpanel].active');
+                if (tp != null) {
+                    tp.classList.remove('active');
+                }
 
-        ul.appendChild(li);
-        tabs.appendChild(div);
+                ul.appendChild(li);
+                tabs.appendChild(div);
 
-        const a = li.querySelector('a');
-        a.addEventListener('click', function(e) {
-            const tab = shadow.querySelector('.tabs [role=tablist] .tab-link[aria-selected=true]');
-            if (tab != null) {
-                tab.setAttribute('aria-selected', 'false');
+                const a = li.querySelector('a');
+                a.addEventListener('click', function(e) {
+                    const tab = shadow.querySelector('.tabs [role=tablist] .tab-link[aria-selected=true]');
+                    if (tab != null) {
+                        tab.setAttribute('aria-selected', 'false');
+                    }
+
+                    let tp = shadow.querySelector('.tabs [role=tabpanel].active');
+                    if (tp != null) {
+                        tp.classList.remove('active');
+                    }
+
+                    a.setAttribute('aria-selected', 'true');
+                    const aria_controls = a.getAttribute('aria-controls');
+                    tp = shadow.getElementById(aria_controls);
+                    tp.classList.add('active'); 
+                });
+            } else {
+                this.setActiveTab(id);
             }
-
-            let tp = shadow.querySelector('.tabs [role=tabpanel].active');
-            if (tp != null) {
-                tp.classList.remove('active');
-            }
-
-            a.setAttribute('aria-selected', 'true');
-            const aria_controls = a.getAttribute('aria-controls');
-            tp = shadow.getElementById(aria_controls);
-            tp.classList.add('active'); 
-        });
+        }
     }
 }
 customElements.define('tab-container', TabContainer);
