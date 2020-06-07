2cb75b681dcc0883dd3a6ea552427396a4fb7080
ferdinand <beowulf1416@gmail.com>
2020-06-08 06:41:43 +0800

    adding actions

--- src/saas/saas/static/js/modules/admin.js
+++ src/saas/saas/static/js/modules/admin.js
@@ -1,9 +1,17 @@
 'use strict';
+import { AdminClientsService } from '/static/js/services/admin.clients.js';
 (function(){
     const tab = document.querySelector('tab-container');
+
+    window.services = window.services ? window.services : {};
+    window.services['admin.clients'] = AdminClientsService;
 
     window.actions = window.actions ? window.actions : {};
     window.actions['admin.clients'] = function() {
         tab.addTab('admin.clients', 'Clients', '<clients-table></clients-table>');
+    };
+
+    window.actions['admin.roles'] = function() {
+        tab.addTab('admin.roles', 'Roles', '<client-roles></client-roles>');
     };
 })();
