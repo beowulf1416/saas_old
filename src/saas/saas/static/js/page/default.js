ba32c6b2c29efc3278b800598f353f0c3d16174f
ferdinand <beowulf1416@gmail.com>
2020-06-07 14:16:47 +0800

    toggle first items as active

--- src/saas/saas/static/js/page/default.js
+++ src/saas/saas/static/js/page/default.js
@@ -1,36 +1,36 @@
 'use strict';
 (function(){
     // navigators
-    const setActiveNavigatorTab = function(navigator_id) {
-        const a_nav = document.querySelector('a[aria-selected=true');
-        if (a_nav != null) {
-            a_nav.setAttribute('aria-selected','false');
-            a_nav.parentElement.classList.remove('active');
-        }
+    // const setActiveNavigatorTab = function(navigator_id) {
+    //     const a_nav = document.querySelector('a[aria-selected=true');
+    //     if (a_nav != null) {
+    //         a_nav.setAttribute('aria-selected','false');
+    //         a_nav.parentElement.classList.remove('active');
+    //     }
 
-        const content_old = document.querySelector('div.nav-content.active');
-        if (content_old != null) {
-            content_old.classList.remove('active');
-        }
+    //     const content_old = document.querySelector('div.nav-content.active');
+    //     if (content_old != null) {
+    //         content_old.classList.remove('active');
+    //     }
 
-        const a_new = document.querySelector(`a#nav-${navigator_id}`);
-        if (a_new != null) {
-            a_new.setAttribute('aria-selected', true);
-            a_new.parentElement.classList.add('active');
-        }
+    //     const a_new = document.querySelector(`a#nav-${navigator_id}`);
+    //     if (a_new != null) {
+    //         a_new.setAttribute('aria-selected', true);
+    //         a_new.parentElement.classList.add('active');
+    //     }
 
-        const content_new = document.querySelector(`div#nav-content-${navigator_id}.nav-content`);
-        if (content_new != null) {
-            content_new.classList.add('active');
-        }
-    };
+    //     const content_new = document.querySelector(`div#nav-content-${navigator_id}.nav-content`);
+    //     if (content_new != null) {
+    //         content_new.classList.add('active');
+    //     }
+    // };
 
-    document.querySelectorAll('.navigators .nav-links .nav').forEach(a => {
-        a.addEventListener('click', function(e) {
-            const nav_id = a.dataset.navid;
-            setActiveNavigatorTab(nav_id);
-        });
-    });
+    // document.querySelectorAll('.navigators .nav-links .nav').forEach(a => {
+    //     a.addEventListener('click', function(e) {
+    //         const nav_id = a.dataset.navid;
+    //         setActiveNavigatorTab(nav_id);
+    //     });
+    // });
 
-    setActiveNavigatorTab('admin');
+    // setActiveNavigatorTab('admin');
 })();
--- src/saas/saas/static/js/ui/tabs.js
+++ src/saas/saas/static/js/ui/tabs.js
@@ -1,20 +1,24 @@
 'use strict';
 
 function tabs(elem) {
-    elem.querySelectorAll('.tabs .tablist a[role=tab]').forEach(a => {
+    elem.querySelectorAll('[role=tabs] [role=tablist] a[role=tab]').forEach(a => {
         a.addEventListener('click', function(e) {
             const tp = a.getAttribute('aria-controls');
             if (tp != null) {
-                const current_tabpanel = elem.querySelector('.tabs [role=tabpanel].active');
+                const current_tabpanel = elem.querySelector('[role=tabs] [role=tabpanel].active');
                 if (current_tabpanel != null) {
                     current_tabpanel.classList.remove('active');
                 }
                 
-                elem.querySelector(`.tabs #${tp}[role=tabpanel]`).classList.add('active');
+                elem.querySelector(`[role=tabs] #${tp}[role=tabpanel]`).classList.add('active');
             }
             console.log('//TODO click');
         });
     });
+
+    // set the first tab as active
+    elem.querySelector('[role=tabs] [role=tablist] a[role=tab]').classList.add('active');
+    elem.querySelector('[role=tabs] [role=tabpanel]').classList.add('active');
 }
 
 export { tabs };
