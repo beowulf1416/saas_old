f27dfc1ff35e4d9b329bb8af0ae0b15f7d68dcf0
ferdinand <beowulf1416@gmail.com>
2020-06-07 20:39:48 +0800

    adding notification list custom element

---
+++ src/saas/saas/static/custom.elements/notification.list/notification.list.js
@@ -0,0 +1,89 @@
+'use strict';
+
+class NotificationList extends HTMLElement {
+
+    constructor() {
+        self = super();
+
+        const style = document.createElement('link');
+        style.rel = "stylesheet";
+        style.href = "/static/custom.elements/notification.list/notification.list.css";
+
+        const bulma = document.createElement('link');
+        bulma.rel = "stylesheet";
+        bulma.href = "https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css";
+
+        const ul = document.createElement('ul');
+        ul.classList.add('notification-list');
+
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.appendChild(ul);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(bulma);
+        shadow.appendChild(div);
+
+        this.add = this.add.bind(this);
+        this.error = this.error.bind(this);
+    }
+
+    add(status, msg, timeout) {
+        const self = this;
+        const shadow = this.shadowRoot;
+        const ul = shadow.querySelector('ul.notification-list');
+        const id = 'msg' + Math.random().toString(16).slice(10);
+        const li = document.createElement('li');
+        li.id = id;
+        li.classList.add('list-item');
+        li.classList.add('message');
+        li.classList.add('message-' + status);
+        // li.textContent = msg;
+
+        let c = '';
+        switch(status) {
+            case 'success': {
+                c = 'is-success';
+                break;
+            }
+            case 'error': {
+                c = 'is-danger';
+                break;
+            }
+            case 'info': {
+                c = 'is-info';
+                break;
+            }
+            default: {
+                console.log('unsupported notification status type: ' + status);
+                break;
+            }
+        }
+
+        li.innerHTML = `
+            <div class="notification ${c}">
+                <button type="button" class="delete" title="Delete"></button>
+                ${msg}
+            </div>
+        `;
+
+        ul.appendChild(li);
+        if (timeout && Number.isInteger(timeout)) {
+            setTimeout(() => {
+                ul.removeChild(li);
+            }, timeout);
+        }
+
+        li.querySelector('button.delete').addEventListener('click', function(e) {
+            ul.removeChild(li);
+        });
+    }
+
+    error(message = '', timeout = null) {
+        this.add('error', message, timeout);
+    }
+}
+
+customElements.define('notification-list', NotificationList);
+export { NotificationList };
