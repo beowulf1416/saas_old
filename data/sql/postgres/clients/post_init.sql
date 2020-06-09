d37a4748858f874970f4f547c211673dd324d517
ferdinand <beowulf1416@gmail.com>
2020-06-09 09:23:54 +0800

    remove client url_name field

--- data/sql/postgres/clients/post_init.sql
+++ data/sql/postgres/clients/post_init.sql
@@ -9,7 +9,7 @@ begin
     -- create default client
     select
         a into client_id
-    from clients.client_add('default', 'N/A', 'default') a;
+    from clients.client_add('default', 'N/A') a;
     
     perform * from clients.client_set_active(client_id, true);
 end
