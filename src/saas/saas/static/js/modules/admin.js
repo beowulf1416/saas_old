1aef2ba5ebd777cab6440578c490687785d16e7d
ferdinand <beowulf1416@gmail.com>
2020-06-09 15:46:27 +0800

    adding ability to edit clients

--- src/saas/saas/static/js/modules/admin.js
+++ src/saas/saas/static/js/modules/admin.js
@@ -8,7 +8,7 @@ import { AdminClientsService } from '/static/js/services/admin.clients.js';
 
     window.actions = window.actions ? window.actions : {};
     window.actions['admin.clients'] = function() {
-        tab.addTab('admin.clients', 'Clients', '<clients-table></clients-table>');
+        tab.addTab('admin.clients', 'Clients', '<clients-table show-new show-edit></clients-table>');
     };
 
     window.actions['admin.roles'] = function() {
