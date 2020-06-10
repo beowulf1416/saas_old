96adbdbf81050b28595be7729ae1ad978dd0e596
ferdinand <beowulf1416@gmail.com>
2020-06-10 10:32:45 +0800

    initial commit of user selector custom elements

---
+++ src/saas/saas/static/custom.elements/user.selector/user.selector.js
@@ -0,0 +1,139 @@
+'use strict';
+import { Users } from '/static/js/helpers/users.js';
+
+class UserSelector extends HTMLElement {
+    
+    constructor() {
+        const self = super();
+
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom.elements/user.selector/user.selector.css');
+
+        const google_web_fonts = document.createElement("link");
+        google_web_fonts.setAttribute('rel', 'stylesheet');
+        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(div);
+
+        this._attachEventHandlers = this._attachEventHandlers.bind(this);
+        this.setUsers = this.setUsers.bind(this);
+    }
+
+    _init(container) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="wrapper">
+                <div class="form-wrapper">
+                    <form class="form-user-selector">
+                        <label for="filter">user</label>
+                        <input type="search" id="filter" name="filter" class="form-input-search" />
+                        <button type="button" class="btn btn-filter">
+                            <span class="material-icons">search</span>
+                        </button>
+                    </form>
+                </div><!-- .form-wrapper -->
+                <table class="tbl-users">
+                    <caption>Users</caption>
+                    <colgroup>
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                    <tfooter>
+                        <tr>
+                            <td><a class="link-assign-user" title="Assign Users" href="#">Assign Users</a></td>
+                        </tr>
+                    </tfooter>
+                </table>
+            </div><!-- .wrapper -->
+        `;
+    }
+
+    _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const beginsearch = function(filter) {
+            Users.filter(filter).then((r) => {
+                if (r.status == 'success') {
+                    self.setUsers(r.json.users);
+                } else {
+                    notify(r.status, r.message);
+                }
+            });
+        };
+
+        const filter = shadow.getElementById('filter');
+        filter.addEventListener('keyup', function(e) {
+            if (e.keyCode == 13) {
+                beginsearch(filter.value);
+                e.preventDefault();
+            }
+        });
+
+        const btnfilter = shadow.querySelector('button.btn-filter');
+        btnfilter.addEventListener('click', function(e) {
+            beginsearch(filter.value);
+        });
+
+        const assign = shadow.querySelector('.link-assign-user');
+        assign.addEventListener('click', function(e) {
+            const userIds = [];
+            const selected = shadow.querySelectorAll('.form-input-selected:checked');
+            selected.forEach((u) => {
+                userIds.push(u.value);
+            });
+
+            self.dispatchEvent(new CustomEvent('assign', {
+                bubbles: true,
+                cancelable: true,
+                detail: {
+                    userIds: userIds
+                }
+            }));
+            e.preventDefault();
+        });
+    }
+
+    setUsers(users = []) {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const tbody = shadow.querySelector('table.tbl-users tbody');
+        while(tbody.firstChild) {
+            tbody.removeChild(tbody.lastChild);
+        }
+
+        users.forEach((u) => {
+            const tds = [];
+            tds.push(`<td><input type="checkbox" name="selected" class="form-input-selected" title="Select User" value="${u.id}" /></td>`);
+            tds.push(`<td>${u.name}</td>`);
+            tds.push(`<td><a class="link-email" title="Email ${u.email}" href="mailto: ${u.name} <${u.email}>">${u.email}</a></td>`);
+            const tdall = tds.join('');
+
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                ${tdall}
+            `;
+
+            tbody.appendChild(tr);
+        });
+    }
+}
+customElements.define('user-selector', UserSelector);
+export { UserSelector };
