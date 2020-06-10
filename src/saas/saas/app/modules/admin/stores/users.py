56ece4110dbaa46f5f9e181ce776589e2e0e1351
ferdinand <beowulf1416@gmail.com>
2020-06-10 14:55:44 +0800

    modified type hints. modified function to assign users to clients.

--- src/saas/saas/app/modules/admin/stores/users.py
+++ src/saas/saas/app/modules/admin/stores/users.py
@@ -4,13 +4,16 @@ log = logging.getLogger(__name__)
 from saas.app.core.services.connection import ConnectionManager
 from saas.app.core.stores.base import BaseStore
 
+from uuid import UUID
+from typing import List
+
 
 class UsersStore(BaseStore):
 
     def __init__(self, manager: ConnectionManager, name: str):
         super(UsersStore, self).__init__(manager, name)
 
-    def getAllClientUsers(self, clientId: str):
+    def getAllClientUsers(self, clientId: UUID):
         try:
             result = super(UsersStore, self).runProc('iam.client_users_all', [clientId, ])
             return result
@@ -18,30 +21,35 @@ class UsersStore(BaseStore):
             log.error(e)
             raise Exception('Unable to retrieve client users')
 
-    def addClientUser(self, clientId: str, userId: str):
+    def addClientUsers(self, clientId: UUID, userIds: List[UUID]):
+        cn = super(UsersStore, self).begin()
         try:
-            super(UsersStore, self).runProcTransactional('iam.client_user_add', [clientId, userId])
+            for userId in userIds:
+                c = cn.cursor()
+                c.callproc('iam.client_user_add', [clientId, userId])
+            super(UsersStore, self).commit(cn)
         except Exception as e:
             log.error(e)
-            raise Exception('Unable to add client user')
+            super(UsersStore, self).rollback(cn)
+            raise Exception('Unable to assign user to client')
 
-    def removeClientUser(self, clientId: str, userId: str):
+    def removeClientUser(self, clientId: UUID, userId: UUID):
         try:
             super(UsersStore, self).runProcTransactional('iam.client_user_remove', [clientId, userId])
         except Exception as e:
             log.error(e)
             raise Exception('Unable to remove client user')
 
-    def clientRoles(self, clientId: str, userId: str):
+    def clientRoles(self, clientId: UUID, userId: UUID):
         try:
             result = super(UsersStore, self).runProc('iam.client_user_roles', [clientId, userId])
         except Exception as e:
             log.error(e)
             raise Exception('Unable to retrieve client roles')
 
-    def addClientUserRoles(self, clientId: str, userId: str, roleIds: list):
+    def addClientUserRoles(self, clientId: UUID, userId: UUID, roleIds: list):
+        cn = self(UsersStore, self).begin()
         try:
-            cn = self(UsersStore, self).begin()
             for roleId in roleIds:
                 c = cn.cursor()
                 c.callproc('iam.role_assign_user', [clientId, roleId, userId])
@@ -51,7 +59,7 @@ class UsersStore(BaseStore):
             super(UsersStore, self).rollback(cn)
             raise Exception('Unable to assign roles to user')
 
-    def removeClientUserRole(self, clientId: str, userId: str, roleId: str):
+    def removeClientUserRole(self, clientId: UUID, userId: UUID, roleId: UUID):
         try:
             super(UsersStore, self).runProcTransactional('iam.role_remove_user', [clientId, roleId, userId])
         except Exception as e:
