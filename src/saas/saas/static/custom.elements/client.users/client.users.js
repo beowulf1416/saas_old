748ecdf9c620fffc2f0490017616559fd8343953
ferdinand <beowulf1416@gmail.com>
2020-06-10 09:58:25 +0800

    modified client user custom element

--- src/saas/saas/static/custom.elements/client.users/client.users.js
+++ src/saas/saas/static/custom.elements/client.users/client.users.js
@@ -1,4 +1,6 @@
 'use strict';
+import { showInTab, showInView, notify } from '/static/js/ui/ui.js';
+import { Users } from '/static/js/helpers/users.js';
 
 class ClientUsers extends HTMLElement {
 
@@ -22,6 +24,15 @@ class ClientUsers extends HTMLElement {
         shadow.appendChild(style);
         shadow.appendChild(google_web_fonts);
         shadow.appendChild(div);
+
+        this._attachEventHandlers = this._attachEventHandlers.bind(this);
+        this.setClientId = this.setClientId.bind(this);
+        this._getClientId = this._getClientId.bind(this);
+        this._refreshUsers = this._refreshUsers.bind(this);
+        this.setUsers = this.setUsers.bind(this);
+        this._refreshRoles = this._refreshRoles.bind(this);
+
+        this._attachEventHandlers();
     }
 
     _init(container) {
@@ -32,7 +43,7 @@ class ClientUsers extends HTMLElement {
                 <button type="button" class="btn btn-refresh" title="Refresh">
                     <span class="material-icons">refresh</span>
                 </button>
-                <button type="button" class="btn btn-user-add" title="Create a new role">
+                <button type="button" class="btn btn-user-add" title="Add a User">
                     <span class="material-icons">group_add</span>
                 </button>
             </div><!-- .toolbar -->
@@ -53,8 +64,10 @@ class ClientUsers extends HTMLElement {
                     </colgroup>
                     <thead>
                         <th></th>
+                        <th></th>
                         <th>Active</th>
                         <th>Name</th>
+                        <th>Email</th>
                     </thead>
                     <tbody>
                     </tbody>
@@ -86,6 +99,171 @@ class ClientUsers extends HTMLElement {
         `;
 
         container.appendChild(div);
+    }
+
+    _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const btnclient = shadow.querySelector('button.btn-client');
+        btnclient.addEventListener('click', function(e) {
+            const selector = showInView('<client-selector></client-selector>');
+            selector.addEventListener('selected', function(e) {
+                if (e.detail.client) {
+                    const client_id = e.detail.client;
+                    self.setClientId(client_id);
+                }
+                e.preventDefault();
+            });
+            e.preventDefault();
+        });
+
+        const btnadduser = shadow.querySelector('button.btn-user-add');
+        btnadduser.addEventListener('click', function(e) {
+            const client_id = self._getClientId();
+            const selector = showInView(`<user-selector client-id="${client_id}"></user-selector>`);
+            selector.addEventListener('assign', function(e) {
+                e.preventDefault();
+            });
+            e.preventDefault();
+        });
+
+        const addrole = shadow.querySelector('.link-role-add');
+        addrole.addEventListener('click', function(e) {
+            const client_id = self._getClientId();
+            const selector = showInView('<role-selector></role-selector>');
+            selector.addEventListener('selected', function(e) {
+                console.log('selected');
+                e.preventDefault();
+            });
+            e.preventDefault();
+        });
+    }
+
+    setClientId(client_id = '') {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const client = shadow.getElementById('client_id');
+        client.value = client_id;
+
+        self._refreshUsers(client_id);
+    }
+
+    _getClientId() {
+        const shadow = this.shadowRoot;
+
+        const client = shadow.getElementById('client_id');
+        return client.value;
+    }
+
+    _refreshUsers(client_id) {
+        const self = this;
+        Users.all(client_id).then((r) => {
+            if (r.status == 'success') {
+                const users = r.json.users;
+                self.setUsers(users);
+            }
+        })
+    }
+
+    setUsers(users = []) {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const tbody = shadow.querySelector('table.tbl-users tbody');
+        while(tbody.firstChild) {
+            tbody.removeChild(tbody.lastChild);
+        }
+
+        users.forEach(u => {
+            const tds = [];
+            tds.push(`<td><input type="radio" name="selected" class="form-input-radio form-input-selected" title="Select User" value="${u.id}" /></td>`);
+            tds.push(`<td><a class="link-edit" title="Edit User" href="#"><span class="material-icons">edit</span></a></td>`);
+            tds.push(`<td><a class="link-active-user" title="Toggle Active State" href="#">${u.active}</a></td>`);
+            tds.push(`<td><a class="link-email" title="Email ${u.email}" href="mailto: ${u.name} <${u.email}>">${u.email}</a></td>`);
+            tds.push(`<td>${u.name}</td>`);
+            const tdall = tds.join('');
+
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                ${tdall}
+            `;
+
+            tbody.appendChild(tr);
+
+            // event handlers
+            const edit = tr.querySelector('.link-edit');
+            edit.addEventListener('click', function(e) {
+                showInTab('admin.users', 'Edit User', `<user-editor user-id="${u.id}"></user-editor>`);
+                e.preventDefault();
+            });
+
+            const active = tr.querySelector('.link-active-user');
+            active.addEventListener('click', function(e) {
+                console.log('toggle active');
+                e.preventDefault();
+            });
+
+            const selected = tr.querySelector('.form-input-selected');
+            selected.addEventListener('change', function(e) {
+                const client_id = self._getClientId();
+                const user_id = selected.value;
+                self._refreshRoles(client_id, user_id);
+            });
+        });
+    }
+
+    _refreshRoles(client_id, user_id) {
+        const self = this;
+        Users.getRoles(client_id, user_id).then((r) => {
+            if (r.status == 'success') {
+                const roles = r.json.roles;
+                self.setRoles(roles);
+            } else {
+                notify(r.status, r.message);
+            }
+        });
+    }
+
+    setRoles(roles = []) {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const tbody = shadow.querySelector('table.tbl-roles tbody');
+        while(tbody.firstChild) {
+            tbody.removeChild(tbody.lastChild);
+        }
+
+        roles.forEach((r) => {
+            const tds = [];
+            tds.push(`<td><a class="link-remove-role" title="Remove Role" href="#" data-roleid="${r.id}">&minus;</a></td>`);
+            tds.push(`<td>${r.name}</td>`);
+            const tdall = tds.join('');
+
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                ${tdall}
+            `;
+
+            tbody.appendChild(tr);
+
+            // event handlers
+            const remove = tr.querySelector('.link-remove-role');
+            remove.addEventListener('click', function(e) {
+                const client_id = self._getClientId();
+                const role_id = remove.dataset.roleid;
+
+                Users.removeRole(client_id, user_id, role_id).then((r) => {
+                    if (r.status == 'success') {
+                        self._refreshRoles(client_id, user_id);
+                    } else {
+                        notify(r.status, r.message);
+                    }
+                });
+                e.preventDefault();
+            });
+        });
     }
 }
 customElements.define('client-users', ClientUsers);
