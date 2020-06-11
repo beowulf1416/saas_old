55e8933599ac645151fc9293607614c8848f498c
ferdinand <beowulf1416@gmail.com>
2020-06-11 21:01:57 +0800

    modify function names and test

--- src/saas/saas/app/modules/common/stores/uom.py
+++ src/saas/saas/app/modules/common/stores/uom.py
@@ -11,9 +11,37 @@ class UOMStore(BaseStore):
         super(UOMStore, self).__init__(manager, name)
 
 
-    def all(self):
+    def length(self):
         try:
-            return super(UOMStore, self).runProc('common.uom_all', [])
+            return super(UOMStore, self).runProc('common.uom_length_all', [])
         except Exception as e:
             log.error(e)
-            raise Exception('Unable to retrieve all units of measure')
+            raise Exception('Unable to retrieve units of measure for length')
+
+    def area(self):
+        try:
+            return super(UOMStore, self).runProc('common.uom_area_all', [])
+        except Exception as e:
+            log.error(e)
+            raise Exception('Unable to retrieve units of measure for width')
+
+    def volume(self):
+        try:
+            return super(UOMStore, self).runProc('common.uom_volume_all', [])
+        except Exception as e:
+            log.error(e)
+            raise Exception('Unable to retrieve units of measure for height')
+
+    def weight(self):
+        try:
+            return super(UOMStore, self).runProc('common.uom_weight_all', [])
+        except Exception as e:
+            log.error(e)
+            raise Exception('Unable to retrieve units of measure for weight')
+
+    def quantity(self):
+        try:
+            return super(UOMStore, self).runProc('common.uom_quantity_all', [])
+        except Exception as e:
+            log.error(e)
+            raise Exception('Unable to retrieve units of measure for quantity')
--- src/saas/tests/common/test_common_uom_store.py
+++ src/saas/tests/common/test_common_uom_store.py
@@ -16,6 +16,22 @@ class TestUOMStore(unittest.TestCase):
         })
         self.uomStore = UOMStore(self.mgr, 'default')
 
-    def test_uom_all(self):
-        result = self.uomStore.all()
+    def test_uom_length(self):
+        result = self.uomStore.length()
+        self.assertGreater(len(result), 0)
+
+    def test_uom_area(self):
+        result = self.uomStore.area()
+        self.assertGreater(len(result), 0)
+
+    def test_uom_volume(self):
+        result = self.uomStore.volume()
+        self.assertGreater(len(result), 0)
+
+    def test_uom_weight(self):
+        result = self.uomStore.weight()
+        self.assertGreater(len(result), 0)
+    
+    def test_uom_quantity(self):
+        result = self.uomStore.quantity()
         self.assertGreater(len(result), 0)
