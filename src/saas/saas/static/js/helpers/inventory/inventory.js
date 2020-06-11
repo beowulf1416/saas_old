70ec2f9a1fbc38169e5c1e6f15674b4abe153b2f
ferdinand <beowulf1416@gmail.com>
2020-06-11 21:23:04 +0800

    modified helper function to fetch units

--- src/saas/saas/static/js/helpers/inventory/inventory.js
+++ src/saas/saas/static/js/helpers/inventory/inventory.js
@@ -2,14 +2,17 @@
 
 class Inventory {
 
-    static uoms() {
+    static uoms(dimension = '') {
         return fetch('/api/inventory/uom/all', {
             method: 'POST',
             credentials: 'same-origin',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify({
+                dimension: dimension
+            })
         })
         .then((r) => r.json());
     }
