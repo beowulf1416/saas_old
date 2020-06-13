be1a0938f40b26a2cb7e35480e4aa9d7d14b3a8f
ferdinand <beowulf1416@gmail.com>
2020-06-13 17:13:31 +0800

    fix inventory item substitutes function

--- data/sql/postgres/inventory/procs/items/item_substitutes.sql
+++ data/sql/postgres/inventory/procs/items/item_substitutes.sql
@@ -5,7 +5,9 @@ create or replace function item_substitutes (
 returns table (
     id inventory.items.id%type,
     active inventory.items.active%type,
-    name inventory.items.name%type
+    name inventory.items.name%type,
+    sku inventory.items.sku%type,
+    upc inventory.items.upc%type
 )
 as $$
 begin
@@ -13,10 +15,13 @@ begin
     select
         a.id,
         a.active,
-        a.name
+        a.name,
+        a.sku,
+        a.upc
     from inventory.items a
+        inner join inventory.item_substitutes b on a.id = b.substitute_item_id
     where a.client_id = p_client_id
-        and a.id = p_item_id;
+        and b.item_id = p_item_id;
 end
 $$
 language plpgsql
