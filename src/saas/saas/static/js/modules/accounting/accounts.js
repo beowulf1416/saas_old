e94753ef3bee4af019074e903968b4b88b33cfe0
ferdinand <beowulf1416@gmail.com>
2020-06-13 20:51:12 +0800

    adding accounts helper

---
+++ src/saas/saas/static/js/modules/accounting/accounts.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Accounts {
+    static getAccountTypes() {
+        return fetch('/api/accounting/accounts/types', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((r) => r.json());
+    }
+
+    static add(clientId, typeId, name, description, parentAccountId) {
+        return fetch('/api/accounting/accounts/add', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                clientId: clientId,
+                typeId: typeId,
+                name: name,
+                description: description,
+                parentAccountId: parentAccountId
+            })
+        })
+        .then((r) => r.json());
+    }
+}
+export { Accounts };
