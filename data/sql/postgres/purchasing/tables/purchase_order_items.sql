c7b13c30174f4527d1a93171285ef69f97db9a3a
ferdinand <beowulf1416@gmail.com>
2020-06-14 17:09:35 +0800

    fix incorrect sql

--- data/sql/postgres/purchasing/tables/purchase_order_items.sql
+++ data/sql/postgres/purchasing/tables/purchase_order_items.sql
@@ -1,5 +1,5 @@
 create table if not exists purchase_order_items (
-    id uuid not null public.gen_random_uuid(),
+    id uuid not null default public.gen_random_uuid(),
     active boolean not null default false,
     created_ts timestamp without time zone not null default(now() at time zone 'utc'),
     client_id uuid not null,
@@ -8,7 +8,7 @@ create table if not exists purchase_order_items (
     unit_id int not null,
     constraint pk_purchase_order_items primary key (id),
     constraint fk_purchase_order_items_1 foreign key (client_id)
-        references clients.clients (client_id) on delete restrict on update restrict,
+        references clients.clients (id) on delete restrict on update restrict,
     constraint fk_purchase_order_items_2 foreign key (unit_id)
         references common.uom (id) on delete restrict on update restrict
 );
