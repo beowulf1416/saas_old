f0a22c6129d65f3710399ba6267d540321a97ddd
ferdinand <beowulf1416@gmail.com>
2020-06-14 17:56:24 +0800

    adding hr actions

---
+++ src/saas/saas/static/js/modules/hr/actions.js
@@ -0,0 +1,11 @@
+'use strict';
+import { showInTab } from '/static/js/ui/ui.js';
+(function(){
+    console.log('hr actions');
+
+    window.actions = window.actions ? window.actions : {};
+    window.actions['hr.dashboard'] = function() {
+        // const client_id = window.clientId;
+        // showInTab('purchasing-dashboard', 'Dashboard', `<items-explorer client-id="${client_id}"></items-explorer>`);
+        console.log('hr.dashboard');
+})();
