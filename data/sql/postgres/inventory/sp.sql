6ff56299b38ced2314329940f43307bf815139ea
ferdinand <beowulf1416@gmail.com>
2020-06-15 16:12:06 +0800

    added sp to filter warehouses

--- data/sql/postgres/inventory/sp.sql
+++ data/sql/postgres/inventory/sp.sql
@@ -23,6 +23,7 @@ set schema 'inventory';
 
 \ir procs/warehouses/warehouse_add.sql
 \ir procs/warehouses/warehouses_all.sql
+\ir procs/warehouses/warehouses_filter.sql
 
 \ir procs/locations/location_add.sql
 
