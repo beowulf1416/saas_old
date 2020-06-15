9aab9d2c31e9bda79bac9978d15a05a71b9daa1c
ferdinand <beowulf1416@gmail.com>
2020-06-15 21:45:31 +0800

    modified purchase order custom element. prefetching uoms.

--- src/saas/saas/static/custom.elements/purchasing/purchase.order/purchase.order.js
+++ src/saas/saas/static/custom.elements/purchasing/purchase.order/purchase.order.js
@@ -1,5 +1,5 @@
 'use strict';
-
+import { Inventory } from '/static/js/modules/inventory/inventory.js';
 class PurchaseOrder extends HTMLElement {
 
     constructor() {
@@ -24,11 +24,17 @@ class PurchaseOrder extends HTMLElement {
         shadow.appendChild(div);
 
         this._getClientId = this._getClientId.bind(this);
+        this._getPOId = this._getPOId.bind(this);
         this._attachEventHandlers = this._attachEventHandlers.bind(this);
+        this._prefetch = this._prefetch.bind(this);
+
+        this._attachEventHandlers();
+        this._prefetch();
     }
 
     _init(container) {
         const client_id = this.getAttribute('client-id');
+        const po_id = this.hasAttribute('po-id') ? this.getAttribute('po-id') : '';
 
         const div = document.createElement('div');
         div.classList.add('wrapper');
@@ -41,6 +47,7 @@ class PurchaseOrder extends HTMLElement {
             <div class="form-wrapper">
                 <form class="form-purchase-order">
                     <input type="hidden" id="client-id" name="client_id" value="${client_id}" />
+                    <input type="hidden" id="po-id" name="po_id" value="${po_id}" />
 
                     <!-- date -->
                     <label for="date">Date</label>
@@ -58,14 +65,17 @@ class PurchaseOrder extends HTMLElement {
                         <table class="tbl-po-items">
                             <caption>Purchase Order Items</caption>
                             <colgroup>
+                                <col class="col-remove">
+                                <col class="col-description">
+                                <col class="col-qty">
+                                <col class="col-uom">
                             </colgroup>
                             <thead>
                                 <tr>
-                                    <th></th>
-                                    <th></th>
-                                    <th>Description</th>
-                                    <th>Quantity</th>
-                                    <th>UOM</th>
+                                    <th scope="col"></th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">UOM</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -78,7 +88,7 @@ class PurchaseOrder extends HTMLElement {
                                 </tr>
                             </tfooter>
                         </table>
-                    </div>,!-- .table-wrapper -->
+                    </div><!-- .table-wrapper -->
                 </form>
             </div><!-- .form-wrapper -->
         `;
@@ -86,10 +96,28 @@ class PurchaseOrder extends HTMLElement {
         container.appendChild(div);
     }
 
+    _prefetch() {
+        const self = this;
+
+        Inventory.uoms().then((r) => {
+            if (r.status == 'success') {
+                self._uoms = r.json.uoms;
+            } else {
+                notify(r.status, r.message);
+            }
+        });
+    }
+
     _getClientId() {
         const shadow = this.shadowRoot;
         const client = shadow.getElementById('client-id');
         return client.value;
+    }
+
+    _getPOId() {
+        const shadow = this.shadowRoot;
+        const po = shadow.getElementById('po-id');
+        return po.value;
     }
 
     _attachEventHandlers() {
@@ -99,11 +127,46 @@ class PurchaseOrder extends HTMLElement {
         const save = shadow.getElementById('btn-save');
         save.addEventListener('click', function(e) {
             console.log('// TODO');
+            const po_id = self._getPOId();
+            if (po_id == '') {
+
+            } else {
+
+            }
         });
 
         const add = shadow.getElementById('link-add-item');
         add.addEventListener('click', function(e) {
             console.log('// TODO');
+
+            const uoms = self._uoms;
+            const options = [];
+            uoms.forEach((u) => {
+                if (u.symbol) {
+                    options.push(`<option value="${u.id}">${u.name} (${u.symbol})</option>`);
+                } else {
+                    options.push(`<option value="${u.id}">${u.name} </option>`);
+                }
+                
+            });
+            const optionsall = options.join('');
+            
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td><a class="link-remove-item" title="Remove" href="#">&minus;</a></td>
+                <td><input type="text" name="description" class="form-input-description" title="Description" placeholder="Description" /></td>
+                <td><input type="number" name="quantity" class="form-input-qty" title="Quantity" /></td>
+                <td>
+                    <select name="uom" class="form-class-uom" title="Unit of Measure">
+                        ${optionsall}
+                    </select>
+                </td>
+            `;
+
+            const tbody = shadow.querySelector('table.tbl-po-items tbody');
+            tbody.appendChild(tr);
+
+            e.preventDefault();
         });
     }
 }
