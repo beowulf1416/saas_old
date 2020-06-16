db2320689ea13bad480132157a7c236984f6c599
ferdinand <beowulf1416@gmail.com>
2020-06-16 16:05:47 +0800

    adding warehouse editor custom element

---
+++ src/saas/saas/static/custom.elements/inventory/warehouse.editor/warehouse.editor.js
@@ -0,0 +1,94 @@
+'use strict';
+import { InventoryWarehouse } from '/static/js/modules/inventory/warehouses.js';
+import { notify } from '/static/js/ui/ui.js';
+class WarehouseEditor extends HTMLElement {
+
+    constructor() {
+        const self = super();
+
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom.elements/inventory/warehouse.editor/warehouse.editor.css');
+
+        const google_web_fonts = document.createElement("link");
+        google_web_fonts.setAttribute('rel', 'stylesheet');
+        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(div);
+
+        this._getClientId = this._getClientId.bind(this);
+        this._getWarehouseId = this._getWarehouseId.bind(this);
+        this._attachEventHandlers = this._attachEventHandlers.bind(this);
+
+        this._attachEventHandlers();
+    }
+
+    _init(container) {
+        const client_id = this.getAttribute('client-id');
+        const warehouse_id = this.getAttribute('warehouse-id');
+
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.innerHTML = `
+            <div class="toolbar" role="toolbar">
+                <button id="btn-save" type="button" class="btn btn-save" title="Save">
+                    <span class="material-icons">save</span>
+                </button>
+            </div><!-- .toolbar -->
+            <div class="form-wrapper">
+                <form class="form-warehouse">
+                    <input type="hidden" id="client-id" name="client_id" value="${client_id}" />
+                    <input type="hidden" id="warehouse-id" name="warehouse_id" value="${warehouse_id}" />
+
+                    <!-- name -->
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" class="form-input-name" title="Warehouse Name" placeholder="Name" />
+
+                    <!-- address -->
+                    <label for="address">Address</label>
+                    <textarea id="address" name="address" class="form-input-address" title="Warehouse Address"></textarea>
+                </form>
+            </div><!-- .form-wrapper -->
+        `;
+
+        container.appendChild(div);
+    }
+
+    _getClientId() {
+        const shadow = this.shadowRoot;
+        const client = shadow.getElementById('client-id');
+        return client.value;
+    }
+
+    _getWarehouseId() {
+        const shadow = this.shadowRoot;
+        const warehouse = shadow.getElementById('warehouse-id');
+        return warehouse.value;
+    }
+
+    _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const btnsave = shadow.getElementById('btn-save');
+        btnsave.addEventListener('click', function(e) {
+            const client_id = self._getClientId();
+            const warehouse_id = self._getWarehouseId();
+
+            const name = shadow.getElementById('name');
+            const address = shadow.getElementById('address');
+            InventoryWarehouse.add(client_id, name.value, address.value).then((r) => {
+                notify(r.status, r.message);
+            });
+        });
+    }
+}
+customElements.define('warehouse-editor', WarehouseEditor);
