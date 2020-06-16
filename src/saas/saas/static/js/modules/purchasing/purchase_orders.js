b051025fa21789c6bdf0fe511f349bb1360e3572
ferdinand <beowulf1416@gmail.com>
2020-06-16 15:41:15 +0800

    adding helper

---
+++ src/saas/saas/static/js/modules/purchasing/purchase_orders.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class PurchaseOrders {
+
+    static save(order = {}) {
+        return fetch('/api/purchasing/purchase/orders/save', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: order
+            })
+        })
+        .then((r) => r.json());
+    }
+}
+export { PurchaseOrders };
