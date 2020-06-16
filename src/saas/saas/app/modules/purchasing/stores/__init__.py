36f0b000647bf147476aa1cbe36e46a34ca2f7fe
ferdinand <beowulf1416@gmail.com>
2020-06-16 21:05:40 +0800

    adding purchase order store

--- src/saas/saas/app/modules/purchasing/stores/__init__.py
+++ src/saas/saas/app/modules/purchasing/stores/__init__.py
@@ -1,6 +1,16 @@
 import logging
 log = logging.getLogger(__name__)
 
+from saas.app.core.services import get_service
+
+from saas.app.modules.purchasing.stores.purchase_order import PurchaseOrderStore
+
 
 def includeme(config):
     log.info('including: saas.app.modules.purchasing.stores')
+
+    services = get_service(None)
+    mgr = services['connection.manager']
+
+    poStore = PurchaseOrderStore(mgr, 'default')
+    services['store.purchasing.po'] = poStore
