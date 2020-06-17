84e1be3a2d4a611a1b2e4e58a4bec5d8c38a7cdc
ferdinand <beowulf1416@gmail.com>
2020-06-17 15:32:06 +0800

    fix incorrect delete statement

--- data/sql/postgres/purchasing/procs/purchase_order_items_remove.sql
+++ data/sql/postgres/purchasing/procs/purchase_order_items_remove.sql
@@ -1,11 +1,11 @@
 create or replace function purchase_order_items_remove (
     p_client_id clients.clients.id%type,
-    p_po_id purchasing.purchase_orders.po_id%type
+    p_po_id purchasing.purchase_orders.id%type
 )
 returns void
 as $$
 begin
-    delete from purchasing.purchase_orders
+    delete from purchasing.purchase_order_items
     where client_id = p_client_id
         and po_id = p_po_id;
 end
