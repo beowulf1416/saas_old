db880bec4910937904af08418faf567fdbdcd868
ferdinand <beowulf1416@gmail.com>
2020-06-17 17:03:45 +0800

    add helper function to filter purchase orders

--- src/saas/saas/static/js/modules/purchasing/purchase_orders.js
+++ src/saas/saas/static/js/modules/purchasing/purchase_orders.js
@@ -14,5 +14,21 @@ class PurchaseOrders {
         })
         .then((r) => r.json());
     }
+
+    static filter(clientId = '', filter = '') {
+        return fetch('/api/purchasing/purchase/orders/filter', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                clientId: clientId,
+                filter: filter
+            })
+        })
+        .then((r) => r.json());
+    }
 }
 export { PurchaseOrders };
