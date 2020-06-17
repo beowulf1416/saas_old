33814969c272e73e5dd197a9616277a4a47cfc44
ferdinand <beowulf1416@gmail.com>
2020-06-17 15:32:59 +0800

    added update function for purhcase orders

--- data/sql/postgres/purchasing/sp.sql
+++ data/sql/postgres/purchasing/sp.sql
@@ -1,6 +1,7 @@
 set schema 'purchasing';
 
 \ir procs/purchase_order_add.sql
+\ir procs/purchase_order_update.sql
 
 \ir procs/purchase_order_items_add.sql
 \ir procs/purchase_order_items_remove.sql
