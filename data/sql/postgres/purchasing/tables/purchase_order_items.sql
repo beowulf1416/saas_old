7344d05a0618da59f9bdeff0a6e75fe68bf867b4
ferdinand <beowulf1416@gmail.com>
2020-06-17 14:45:38 +0800

    adding warehouse id field. removed extra fields

--- data/sql/postgres/purchasing/tables/purchase_order_items.sql
+++ data/sql/postgres/purchasing/tables/purchase_order_items.sql
@@ -1,15 +1,15 @@
 create table if not exists purchase_order_items (
-    id uuid not null default public.gen_random_uuid(),
-    active boolean not null default false,
     created_ts timestamp without time zone not null default(now() at time zone 'utc'),
     client_id uuid not null,
+    po_id uuid not null,
     description text not null,
     quantity decimal(12,6) not null,
     unit_id int not null,
-    constraint pk_purchase_order_items primary key (id),
     constraint fk_purchase_order_items_1 foreign key (client_id)
         references clients.clients (id) on delete restrict on update restrict,
-    constraint fk_purchase_order_items_2 foreign key (unit_id)
+    constraint fk_purchase_order_items_2 foreign key (po_id)
+        references puchasing.purchase_orders (id) on delete restrict on update restrict,
+    constraint fk_purchase_order_items_3 foreign key (unit_id)
         references common.uom (id) on delete restrict on update restrict,
     constraint chk_purchase_order_items_1 check (quantity > 0)
 );
--- data/sql/postgres/purchasing/tables/purchase_orders.sql
+++ data/sql/postgres/purchasing/tables/purchase_orders.sql
@@ -4,6 +4,7 @@ create table if not exists purchase_orders (
     created_ts timestamp without time zone not null default(now() at time zone 'utc'),
     client_id uuid not null,
     description text not null,
+    warehouse_id uuid not null,
     constraint pk_purchase_orders primary key (id),
     constraint fk_purchase_orders_1 foreign key (client_id)
         references clients.clients (id) on delete restrict on update restrict 
