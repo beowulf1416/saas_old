d78dfe5555e252b28369d4c39ad1ae0b70d10f9d
ferdinand <beowulf1416@gmail.com>
2020-06-18 10:42:58 +0800

    modified hr actions

--- src/saas/saas/static/js/modules/hr/actions.js
+++ src/saas/saas/static/js/modules/hr/actions.js
@@ -9,4 +9,9 @@ import { showInTab } from '/static/js/ui/ui.js';
         // showInTab('purchasing-dashboard', 'Dashboard', `<items-explorer client-id="${client_id}"></items-explorer>`);
         console.log('hr.dashboard');
     }
+
+    window.actions['hr.team.explorer'] = function() {
+        const client_id = window.clientId;
+        showInTab('team-explorer', 'Team Explorer', `<team-explorer client-id="${client_id}"></team-explorer>`);
+    };
 })();
