00e7ace0795d22e9d835a35fd6d2853d229611f0
ferdinand <beowulf1416@gmail.com>
2020-06-18 16:48:55 +0800

    adding feature to add/update organization

--- src/saas/saas/static/custom.elements/clients/organization.editor/organization.editor.js
+++ src/saas/saas/static/custom.elements/clients/organization.editor/organization.editor.js
@@ -1,5 +1,6 @@
 'use strict';
-
+import { notify } from '/static/js/ui/ui.js';
+import { ClientOrganizations } from '/static/js/modules/clients/organizations.js';
 class OrganizationEditor extends HTMLElement {
 
     constructor() {
@@ -28,6 +29,7 @@ class OrganizationEditor extends HTMLElement {
 
     _init(container) {
         const client_id = this.getAttribute('client-id');
+        const org_id = this.hasAttribute('organization-id') ? this.getAttribute('organization-id') : '';
 
         const div = document.createElement('div');
         div.classList.add('wrapper');
@@ -40,10 +42,15 @@ class OrganizationEditor extends HTMLElement {
             <div class="form-wrapper">
                 <form class="form-organization">
                     <input type="hidden" id="client-id" name="client_id" value="${client_id}" />
+                    <input type="hidden" id="organization-id" name="organization_id" value="${org_id}" />
 
                     <!-- name -->
                     <label for="name">Name</label>
                     <input type="text" id="name" name="name" class="form-input-name" title="Organization Name" placeholder="Name" />
+
+                    <!-- description -->
+                    <label for="description">Description</label>
+                    <textarea id="description" name="description" class="form-input-description" title="Description" placeholder="Description"></textarea>
                 </form>
             </div><!-- .form-wrapper -->
         `;
@@ -52,7 +59,27 @@ class OrganizationEditor extends HTMLElement {
     }
 
     _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
 
+        const client_id = this.getAttribute('client-id');
+        const org_id = this.hasAttribute('organization-id') ? this.getAttribute('organization-id') : '';
+
+        const btnsave = shadow.getElementById('btn-save');
+        btnsave.addEventListener('click', function(e) {
+            const name = shadow.getElementById('name');
+            const description = shadow.getElementById('description');
+
+            if (org_id == '') {
+                ClientOrganizations.add(client_id, name.nodeValue, description.value).then((r) => {
+                    notify(r.status, r.message);
+                });
+            } else {
+                ClientOrganizations.update(client_id, org_id, name.value, description.value).then((r) => {
+                    notify(r.status, r.message);
+                });
+            }
+        });
     }
 }
 customElements.define('organization-editor', OrganizationEditor);
