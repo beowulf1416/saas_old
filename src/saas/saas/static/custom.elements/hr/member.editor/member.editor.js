12674bc28b900a86ca9b44488f7d954d4fabd122
ferdinand <beowulf1416@gmail.com>
2020-06-18 10:42:24 +0800

    adding member editor element

---
+++ src/saas/saas/static/custom.elements/hr/member.editor/member.editor.js
@@ -0,0 +1,48 @@
+'use strict';
+
+class MemberEditor extends HTMLElement {
+
+    constructor() {
+        const self = super();
+
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom.elements/hr/member.editor/member.editor.css');
+
+        const google_web_fonts = document.createElement("link");
+        google_web_fonts.setAttribute('rel', 'stylesheet');
+        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(div);
+    }
+
+    _init(container) {
+        const client_id = this.getAttribute('client-id');
+
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.innerHTML = `
+            <div class="toolbar" role="toolbar">
+                <button id="btn-save" type="button" class="btn btn-save" title="Save">
+                    <span class="material-icons">save</span>
+                </button>
+            </div><!-- .toolbar -->
+            <div class="form-wrapper">
+                <form class="form-member">
+                    <input type="hidden" id="client-id" name="client_id" value="${client_id}" />
+                </form>
+            </div><!-- .form-wrapper -->
+        `;
+
+        container.appendChild(div);
+    }
+}
+customElements.define('member-editor', MemberEditor);
