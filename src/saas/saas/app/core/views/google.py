aec2d4de1349424f402f0d14fe053e2392fa5ed1
ferdinand <beowulf1416@gmail.com>
2020-06-19 20:56:59 +0800

    fix for client country field

--- src/saas/saas/app/core/views/google.py
+++ src/saas/saas/app/core/views/google.py
@@ -95,7 +95,7 @@ def view_google_oauth_redirect(request):
                 userStore.userAdd(email, name)
 
             user = userStore.userByEmail(email)
-            (client_id, active, name, address) = clientStore.getDefaultClient()
+            (client_id, active, name, address, country_id) = clientStore.getDefaultClient()
             remember(request, email, client=client_id)
 
             raise exception.HTTPFound(request.route_url('home'))
