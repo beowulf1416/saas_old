1e501e4b236ef044514f1ea2ee9f221e8d11eadb
ferdinand <beowulf1416@gmail.com>
2020-06-19 20:35:57 +0800

    adding constraints to countries table

--- data/sql/postgres/common/tables/countries.sql
+++ data/sql/postgres/common/tables/countries.sql
@@ -9,5 +9,7 @@ create table if not exists countries (
     iso_4217_currency_name varchar(100),
     iso_4217_currency_numeric int,
     constraint pk_countries primary key (iso_3166_1_numeric),
-    constraint u_countries unique (name)
+    constraint u_countries_1 unique (name),
+    constraint u_countries_2 unique (iso_3166_1_alpha_2),
+    constraint u_countries_3 unique (iso_3166_1_alpha_3)
 );
