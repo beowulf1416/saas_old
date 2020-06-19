d1e9e238a7c226c58c3fc9b07fbf224c83ab268e
ferdinand <beowulf1416@gmail.com>
2020-06-19 17:09:06 +0800

    added fields to member editor

--- src/saas/saas/static/custom.elements/hr/member.editor/member.editor.js
+++ src/saas/saas/static/custom.elements/hr/member.editor/member.editor.js
@@ -39,27 +39,48 @@ class MemberEditor extends HTMLElement {
                 <form class="form-member">
                     <input type="hidden" id="client-id" name="client_id" value="${client_id}" />
 
-                    <div class="form-group form-group-name">
-                        <!-- first name -->
-                        <label for="first-name">First Name</label>
-                        <input type="text" id="first-name" name="first_name" class="form-input-first" title="First Name" placeholder="First Name" />
+                    <fieldset>
+                        <legend>Name</legend>
+                        <div class="form-group form-group-name">
+                            <!-- first name -->
+                            <label for="first-name">First Name</label>
+                            <input type="text" id="first-name" name="first_name" class="form-input-first" title="First Name" placeholder="First Name" />
 
-                        <!-- middle name -->
-                        <label for="middle-name">Middle Name</label>
-                        <input type="text" id="middle-name" name="middle_name" class="form-input-middle" title="Middle Name" placeholder="Middle Name" />
+                            <!-- middle name -->
+                            <label for="middle-name">Middle Name</label>
+                            <input type="text" id="middle-name" name="middle_name" class="form-input-middle" title="Middle Name" placeholder="Middle Name" />
 
-                        <!-- last name -->
-                        <label for="last-name">Last Name</label>
-                        <input type="text" id="last-name" name="last_name" class="form-input-last" title="Last Name" placeholder="Last Name" />
-                    </div><!-- .form-group -->
+                            <!-- last name -->
+                            <label for="last-name">Last Name</label>
+                            <input type="text" id="last-name" name="last_name" class="form-input-last" title="Last Name" placeholder="Last Name" />
+                        </div><!-- .form-group -->
 
-                    <!-- prefix -->
-                    <label for="prefix">Prefix</label>
-                    <input type="text" id="prefix" name="prefix" class="form-input-prefix" title="Prefix" placeholder="Prefix" />
+                        <!-- prefix -->
+                        <label for="prefix">Prefix</label>
+                        <input type="text" id="prefix" name="prefix" class="form-input-prefix" title="Prefix" placeholder="Prefix" />
 
-                    <!-- suffix -->
-                    <label for="suffix">Suffix</label>
-                    <input type="suffix" id="suffix" name="suffix" class="form-input-suffix" title="Suffix" placeholder="Suffix" />
+                        <!-- suffix -->
+                        <label for="suffix">Suffix</label>
+                        <input type="suffix" id="suffix" name="suffix" class="form-input-suffix" title="Suffix" placeholder="Suffix" />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Identifiers</legend>
+                        <label for="sss">SSS</label>
+                        <input type="text" id="sss" name="sss" class="form-input-sss" title="SSS" placeholder="SSS" />
+
+                        <label for="tin">TIN</label>
+                        <input type="text" id="tin" name="tin" class="form-input-tin" title="TIN" placeholder="TIN" />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Dates</legend>
+
+                        <div class="form-group form-group-hire-date">
+                            <!-- hire date -->
+                            <label for="hire-date">Hire Date</label>
+                            <input type="date" id="hire-date" name="hire_date" class="form-input-date-hire" title="Hire Date" />
+                        </div><!-- .form-group -->
+                    </fieldset>
+
                 </form>
             </div><!-- .form-wrapper -->
         `;
