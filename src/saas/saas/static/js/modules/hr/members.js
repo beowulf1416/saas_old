e09ab791f78d8f1e3132320c7005b457b68bdcac
ferdinand <beowulf1416@gmail.com>
2020-06-22 21:14:22 +0800

    adding filter function

--- src/saas/saas/static/js/modules/hr/members.js
+++ src/saas/saas/static/js/modules/hr/members.js
@@ -5,5 +5,12 @@ class Members {
     static save(member = {}) {
         return Util.fetch('/api/hr/members/save', member);
     }
+
+    static filter(clientId = '', filter = '') {
+        return Util.fetch('/api/hr/members/filter', {
+            clientId: clientId,
+            filter: filter
+        });
+    }
 }
 export { Members };
