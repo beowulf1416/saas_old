63588eee7e2762da505610f41cb144e17ff76028
ferdinand <beowulf1416@gmail.com>
2020-06-22 11:59:21 +0800

    initial commit of inventory material receiving custom element - incomplete

---
+++ src/saas/saas/static/custom.elements/inventory/receiving.editor/receiving.editor.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class ReceivingEditor extends HTMLElement {
+
+    constructor() {
+
+        const self = super();
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom.elements/inventory/warehouse.explorer/warehouse.explorer.css');
+
+        const google_web_fonts = document.createElement("link");
+        google_web_fonts.setAttribute('rel', 'stylesheet');
+        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(div);
+    }
+
+    _init(container) {
+        const client_id = this.getAttribute('client-id');
+
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.innerHTML = `
+            <div class="form-wrapper">
+                <form class="form-receiving">
+                    <input type="hidden" id="client-id" name="client_id" value="${client_id}" />
+
+                    <!--  -->
+                </form>
+            </div><!-- .form-wrapper -->
+        `;
+
+        container.appendChild(div);
+    }
+}
+customElements.define('receiving-editor', ReceivingEditor);
+export { ReceivingEditor };
