3286cc008c7054a257a922c02201b464f12f6e4c
ferdinand <beowulf1416@gmail.com>
2020-07-01 16:17:32 +0800

    rename sp

--- data/sql/postgres/purchasing/sp.sql
+++ data/sql/postgres/purchasing/sp.sql
@@ -4,7 +4,7 @@ set schema 'purchasing';
 \ir procs/purchase_order_update.sql
 \ir procs/purchase_orders_filter.sql
 
-\ir procs/purchase_order_items_add.sql
+\ir procs/purchase_order_item_add.sql
 \ir procs/purchase_order_items_remove.sql
 
 
