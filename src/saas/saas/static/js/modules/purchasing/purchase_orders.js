570bd223a85da291bef4bb65323eba5b1ddee646
ferdinand <beowulf1416@gmail.com>
2020-07-01 16:08:35 +0800

    modified purchasing module to use util

--- src/saas/saas/static/js/modules/purchasing/purchase_orders.js
+++ src/saas/saas/static/js/modules/purchasing/purchase_orders.js
@@ -1,34 +1,39 @@
 'use strict';
-
+import { Util } from '/static/js/util.js';
 class PurchaseOrders {
 
     static save(order = {}) {
-        return fetch('/api/purchasing/purchase/orders/save', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(order)
-        })
-        .then((r) => r.json());
+        // return fetch('/api/purchasing/purchase/orders/save', {
+        //     method: 'POST',
+        //     credentials: 'same-origin',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(order)
+        // })
+        // .then((r) => r.json());
+        return Util.fetch('/api/purchasing/purchase/orders/save', order);
     }
 
     static filter(clientId = '', filter = '') {
-        return fetch('/api/purchasing/purchase/orders/filter', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                clientId: clientId,
-                filter: filter
-            })
+        // return fetch('/api/purchasing/purchase/orders/filter', {
+        //     method: 'POST',
+        //     credentials: 'same-origin',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         clientId: clientId,
+        //         filter: filter
+        //     })
+        // })
+        // .then((r) => r.json());
+        return Util.fetch('/api/purchasing/purchase/orders/filter', {
+            clientId: clientId,
+            filter: filter
         })
-        .then((r) => r.json());
     }
 }
 export { PurchaseOrders };
