f5e31887a0b0da5f8283dd8c081b78448f6a7404
ferdinand <beowulf1416@gmail.com>
2020-07-01 21:52:17 +0800

    simplify functions for saving/updating purchase orders

--- data/sql/postgres/purchasing/procs/purchase_order_save.sql
+++ data/sql/postgres/purchasing/procs/purchase_order_save.sql
@@ -1,25 +1,24 @@
-create or replace function purchase_order_add (
+create or replace function purchase_order_save (
     p_client_id clients.clients.id%type,
+    p_po_id purchasing.purchase_orders.id%type,
     p_description purchasing.purchase_orders.description%type,
     p_warehouse_id purchasing.purchase_orders.warehouse_id%type
 )
-returns purchasing.purchase_orders.id%type
+returns void
 as $$
-declare
-    t_po_id purchasing.purchase_orders.id%type;
 begin
     insert into purchasing.purchase_orders (
+        id,
         client_id,
         description,
         warehouse_id
     ) values  (
+        p_po_id,
         p_client_id,
         p_description,
         p_warehouse_id
     )
-    returning id into t_po_id;
-
-    return t_po_id;
+    on conflict (id) do nothing;
 end
 $$
 language plpgsql;
