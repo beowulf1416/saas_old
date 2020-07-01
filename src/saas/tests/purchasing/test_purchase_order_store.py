2d0d57fd66ebede22f615a2d974d2c8c43e86206
ferdinand <beowulf1416@gmail.com>
2020-07-01 16:25:11 +0800

    modified test

--- src/saas/tests/purchasing/test_purchase_order_store.py
+++ src/saas/tests/purchasing/test_purchase_order_store.py
@@ -27,10 +27,10 @@ class TestPurchaseOrderStore(unittest.TestCase):
         allowed = string.ascii_lowercase + string.digits
         return ''.join(random.choice(allowed) for i in range(length))
 
-    def save_po(self):
+    def test_save_po(self):
         random_str = self.generate_random_str(10)
         (client_id, active, name, address, country_id)  = self.clientStore.getDefaultClient()
-        [(warehouse_id, ), ] = self.warehouseStore.add(client_id, random_str, random_str)
+        warehouse_id = self.warehouseStore.add(client_id, random_str, random_str)
         order = {
             'clientId': client_id,
             'description': random_str,
@@ -46,7 +46,27 @@ class TestPurchaseOrderStore(unittest.TestCase):
         po_id = self.poStore.save(order)
         self.assertFalse(po_id is None)
 
-    def filter_po(self):
-        (client_id, active, name, address, country_id)  = self.clientStore.getDefaultClient()
-        result = self.poStore.filter(client_id, '')
-        self.assertGreater(len(result), 0, '{0}'.format(result))
+    def test_filter_po(self):
+        try:
+            (client_id, active, name, address, country_id)  = self.clientStore.getDefaultClient()
+
+            random_str = self.generate_random_str(10)
+            warehouse_id = self.warehouseStore.add(client_id, random_str, random_str)
+            order = {
+                'clientId': client_id,
+                'description': random_str,
+                'warehouseId': warehouse_id,
+                'items': [
+                    {
+                        'description': random_str,
+                        'quantity': 1,
+                        'uom': 1
+                    }
+                ]
+            }
+            po_id = self.poStore.save(order)
+
+            result = self.poStore.filter(client_id, '')
+            self.assertGreater(len(result), 0, '{0}'.format(result))
+        except Exception as e:
+            self.fail(e)
