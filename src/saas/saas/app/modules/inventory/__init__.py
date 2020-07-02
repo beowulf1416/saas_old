7a18967f19cc21a4ea58e841dfdc6e12b5e0f4a7
ferdinand <beowulf1416@gmail.com>
2020-07-02 18:40:51 +0800

    loading item selector view module

--- src/saas/saas/app/modules/inventory/__init__.py
+++ src/saas/saas/app/modules/inventory/__init__.py
@@ -58,6 +58,10 @@ def includeme(config):
             },
             {
                 'type': 'module',
+                'script': '/static/custom.elements/inventory/item.selector/item-selector-view.js' 
+            },
+            {
+                'type': 'module',
                 'script': '/static/custom.elements/inventory/item.editor/item.editor.js'
             },
             {
