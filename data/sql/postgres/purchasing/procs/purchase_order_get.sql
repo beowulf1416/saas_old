5dc953857830e93deb21d50685410d45eb7dfbe3
ferdinand <beowulf1416@gmail.com>
2020-07-02 09:41:53 +0800

    added sp to retrieve purchase order

---
+++ data/sql/postgres/purchasing/procs/purchase_order_get.sql
@@ -0,0 +1,25 @@
+create or replace function purchase_order_get (
+    p_client_id clients.clients.id%type,
+    p_order_id purchasing.purchase_orders.id%type
+)
+returns table (
+    id purchasing.purchase_orders.id%type,
+    created purchasing.purchase_orders.created_ts%type,
+    description purchasing.purchase_orders.description%type,
+    warehouse_id purchasing.purchase_orders.warehouse_id%type
+)
+as $$
+begin
+    return query
+    select
+        a.id,
+        a.created_ts,
+        a.description,
+        a.warehouse_id
+    from purchasing.purchase_orders a
+    where a.client_id = p_client_id
+        and a.id = p_order_id;
+end
+$$
+language plpgsql
+stable;
--- data/sql/postgres/purchasing/sp.sql
+++ data/sql/postgres/purchasing/sp.sql
@@ -1,8 +1,8 @@
 set schema 'purchasing';
 
-\ir procs/purchase_order_add.sql
-\ir procs/purchase_order_update.sql
+\ir procs/purchase_order_save.sql
 \ir procs/purchase_orders_filter.sql
+\ir procs/purchase_order_get.sql
 
 \ir procs/purchase_order_item_add.sql
 \ir procs/purchase_order_items_remove.sql
