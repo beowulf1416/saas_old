c21819f47eb5fdcb815b1f82881d66deb2f4a2dd
ferdinand <beowulf1416@gmail.com>
2020-07-03 09:23:15 +0800

    returning name of uom in sp

--- data/sql/postgres/purchasing/procs/purchase_order_items_get.sql
+++ data/sql/postgres/purchasing/procs/purchase_order_items_get.sql
@@ -5,7 +5,8 @@ create or replace function purchase_order_items_get (
 returns table (
     description purchasing.purchase_order_items.description%type,
     quantity purchasing.purchase_order_items.quantity%type,
-    unit_id purchasing.purchase_order_items.unit_id%type
+    unit_id purchasing.purchase_order_items.unit_id%type,
+    unit text
 )
 as $$
 begin
@@ -13,8 +14,10 @@ begin
     select
         a.description,
         a.quantity,
-        a.unit_id
+        a.unit_id,
+        case when b.symbol is null then b.name else b.name || ' (' || b.symbol || ') ' end unit
     from purchasing.purchase_order_items a
+        inner join common.uom b on a.unit_id = b.id
     where a.client_id = p_client_id
         and a.po_id = p_order_id;
 end
