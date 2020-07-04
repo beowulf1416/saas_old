869dccd11d46be41b070a1d0e5de76046703d44a
ferdinand <beowulf1416@gmail.com>
2020-07-04 22:05:44 +0800

    added organization editor custom element

---
+++ src/saas/saas/static/custom.elements/crm/organization-editor/organization-editor.js
@@ -0,0 +1,57 @@
+'use strict';
+class OrganizationEditor extends HTMLElement {
+
+    constructor() {
+        const self = super();
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom.elements/crm/organization-editor/organization-editor.css');
+
+        const google_web_fonts = document.createElement("link");
+        google_web_fonts.setAttribute('rel', 'stylesheet');
+        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(div);
+
+        this._attachEventHandlers = this._attachEventHandlers.bind(this);
+
+        this._attachEventHandlers();
+    }
+
+    _init(container) {
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.innerHTML = `
+            <div class="toolbar" role="toolbar">
+                <button type="button" id="btn-save" class="btn btn-save" title="Save">
+                    <span class="material-icons">save</span>
+                </button>
+            </div><!-- .toolbar -->
+            <div class="form-wrapper">
+                <form id="form-organization">
+                </form>
+            </div><!-- .form-wrapper -->
+        `;
+
+        container.appendChild(div);
+    }
+
+    _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const btnsave = shadow.getElementById('btn-save');
+        btnsave.addEventListener('click', function(e) {
+            console.log('//TODO');
+        });
+    }
+}
+customElements.define('crm-organization-editor', OrganizationEditor);
