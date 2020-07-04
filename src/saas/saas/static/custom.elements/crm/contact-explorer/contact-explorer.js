bec0a4ef359dc4786ee16751e50ecb7e9098ce57
ferdinand <beowulf1416@gmail.com>
2020-07-04 22:18:14 +0800

    adding contact explorer custom element

---
+++ src/saas/saas/static/custom.elements/crm/contact-explorer/contact-explorer.js
@@ -0,0 +1,44 @@
+'use strict';
+class ContactExplorer extends HTMLElement {
+
+    constructor() {
+        const self = super();
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom.elements/crm/contact-explorer/contact-explorer.css');
+        
+        const common = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/css/default.css');
+
+        const google_web_fonts = document.createElement("link");
+        google_web_fonts.setAttribute('rel', 'stylesheet');
+        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(common);
+        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(div);
+    }
+
+    _init(container) {
+        const div = document.createElement('div');
+        div.classList.add('wrapper');
+        div.innerHTML = `
+            <div class="toolbar" role="toolbar">
+                <button type="button" id="btn-new" class="btn btn-new" title="New Contact">
+                    <span class="material-icons">person_add</span>
+                </button>
+            </div><!-- .toolbar -->
+        `;
+
+        container.appendChild(div);
+    }
+}
+customElements.define('contact-explorer', ContactExplorer);
