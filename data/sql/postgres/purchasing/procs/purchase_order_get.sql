57f12febdfb39c13beb12975a8b5c98caba24af0
ferdinand <beowulf1416@gmail.com>
2020-07-07 11:03:15 +0800

    adding vendor id to sp result

--- data/sql/postgres/purchasing/procs/purchase_order_get.sql
+++ data/sql/postgres/purchasing/procs/purchase_order_get.sql
@@ -7,6 +7,7 @@ returns table (
     created purchasing.purchase_orders.created_ts%type,
     description purchasing.purchase_orders.description%type,
     warehouse_id purchasing.purchase_orders.warehouse_id%type,
+    vendor_id purchasing.purchase_orders.vendor_id%type,
     instructions purchasing.purchase_orders.instructions%type
 )
 as $$
@@ -17,6 +18,7 @@ begin
         a.created_ts,
         a.description,
         a.warehouse_id,
+        a.vendor_id,
         a.instructions
     from purchasing.purchase_orders a
     where a.client_id = p_client_id
