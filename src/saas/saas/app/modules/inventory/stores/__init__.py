f3428f4978464bec93a7825b4017f375a2f75d72
ferdinand <beowulf1416@gmail.com>
2020-07-07 16:01:50 +0800

    moved vendors store from inventory to purchasing

--- src/saas/saas/app/modules/inventory/stores/__init__.py
+++ src/saas/saas/app/modules/inventory/stores/__init__.py
@@ -6,7 +6,6 @@ from saas.app.core.services import get_service
 from saas.app.modules.inventory.stores.items import ItemsStore
 from saas.app.modules.inventory.stores.warehouses import WarehouseStore
 from saas.app.modules.inventory.stores.locations import LocationStore
-from saas.app.modules.inventory.stores.vendors import VendorStore
 
 
 def includeme(config):
@@ -15,14 +14,6 @@ def includeme(config):
     services = get_service(None)
     mgr = services['connection.manager']
 
-    itemsStore = ItemsStore(mgr, 'default')
-    services['store.inventory.items'] = itemsStore
-
-    warehouseStore = WarehouseStore(mgr, 'default')
-    services['store.inventory.warehouses'] = warehouseStore
-
-    locationStore = LocationStore(mgr, 'default')
-    services['store.inventory.locations'] = locationStore
-
-    vendorStore = VendorStore(mgr, 'default')
-    services['store.inventory.vendors'] = vendorStore
+    services['store.inventory.items'] = ItemsStore(mgr, 'default')
+    services['store.inventory.warehouses'] = WarehouseStore(mgr, 'default')
+    services['store.inventory.locations'] = LocationStore(mgr, 'default')
--- src/saas/saas/app/modules/purchasing/stores/__init__.py
+++ src/saas/saas/app/modules/purchasing/stores/__init__.py
@@ -4,6 +4,7 @@ log = logging.getLogger(__name__)
 from saas.app.core.services import get_service
 
 from saas.app.modules.purchasing.stores.purchase_order import PurchaseOrderStore
+from saas.app.modules.purchasing.stores.vendors import VendorStore
 
 
 def includeme(config):
@@ -12,5 +13,5 @@ def includeme(config):
     services = get_service(None)
     mgr = services['connection.manager']
 
-    poStore = PurchaseOrderStore(mgr, 'default')
-    services['store.purchasing.po'] = poStore
+    services['store.purchasing.po'] = PurchaseOrderStore(mgr, 'default')
+    services['store.purchasing.vendors'] = VendorStore(mgr, 'default')
