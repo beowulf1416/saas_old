f4200454f2cd06c5f7a48f0f5fa928557f0aef9a
ferdinand <beowulf1416@gmail.com>
2020-07-07 11:12:44 +0800

    modified organization selector element

--- src/saas/saas/static/custom.elements/crm/organization-selector/organization-selector.js
+++ src/saas/saas/static/custom.elements/crm/organization-selector/organization-selector.js
@@ -55,28 +55,14 @@ class OrganizationSelector extends HTMLElement {
 
         const client_id = this.getAttribute('client-id');
 
-        const display = shadow.getElementById('display');
-
         const select = shadow.getElementById('btn-select');
         select.addEventListener('click', function(e) {
             const selector = showInView('Select Organization', `<crm-organization-selector-view client-id="${client_id}"></crm-organization-selector-view>`);
             selector.addEventListener('change', function(e) {
                 const org = e.detail.organization;
 
-                display.value = org.name;
                 self._org_id = org.id;
-
-                Organizations.get(client_id, org.id).then((r) => {
-                    if (r.status == 'success') {
-                        const input_address = shadow.getElementById('address');
-                        if (input_address) {
-                            const org_full = r.json.organization;
-                            input_address.value = org_full.address;
-                        }
-                    } else {
-                        notify(r.status, r.message);
-                    }
-                });
+                self.setAttribute('org-id', org.id);
             });
         });
     }
