33eefb6caac6e64f1191bb441e8197b48d3bee83
ferdinand <beowulf1416@gmail.com>
2020-07-07 10:11:32 +0800

    adding support for vendor_id in purchase order

--- data/sql/postgres/purchasing/procs/purchase_order_save.sql
+++ data/sql/postgres/purchasing/procs/purchase_order_save.sql
@@ -3,6 +3,7 @@ create or replace function purchase_order_save (
     p_po_id purchasing.purchase_orders.id%type,
     p_description purchasing.purchase_orders.description%type,
     p_warehouse_id purchasing.purchase_orders.warehouse_id%type,
+    p_vendor_id purchasing.purchase_orders.vendor_id%type,
     p_instructions purchasing.purchase_orders.instructions%type
 )
 returns void
@@ -13,17 +14,20 @@ begin
         client_id,
         description,
         warehouse_id,
+        vendor_id,
         instructions
     ) values  (
         p_po_id,
         p_client_id,
         p_description,
         p_warehouse_id,
+        p_vendor_id,
         p_instructions
     )
     on conflict (id) do update set
         description = p_description,
         warehouse_id = p_warehouse_id,
+        vendor_id = p_vendor_id,
         instructions = p_instructions;
 end
 $$
--- data/sql/postgres/purchasing/tables/purchase_orders.sql
+++ data/sql/postgres/purchasing/tables/purchase_orders.sql
@@ -5,8 +5,11 @@ create table if not exists purchase_orders (
     client_id uuid not null,
     description text not null,
     warehouse_id uuid not null,
+    vendor_id uuid not null,
     instructions text,
     constraint pk_purchase_orders primary key (id),
     constraint fk_purchase_orders_1 foreign key (client_id)
-        references clients.clients (id) on delete restrict on update restrict 
+        references clients.clients (id) on delete restrict on update restrict,
+    constraint fk_purchase_orders_2 foreign key (vendor_id)
+        references inventory.vendors (id) on delete restrict on update restrict
 );
