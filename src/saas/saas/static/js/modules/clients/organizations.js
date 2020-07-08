bcfe3964b0925d310bedf1d70d540a17f6d4fe63
ferdinand <beowulf1416@gmail.com>
2020-07-08 20:12:22 +0800

    use util to fetch

--- src/saas/saas/static/js/modules/clients/organizations.js
+++ src/saas/saas/static/js/modules/clients/organizations.js
@@ -1,72 +1,37 @@
 'use strict';
-
+import { Util } from '/static/js/util.js';
 class ClientOrganizations {
 
     static assignParentOrganization(clientId = '', organizationId = '', parentOrganizationId = '') {
-        return fetch('/api/clients/organizations/parent/assign', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                clientId: clientId,
-                organizationId: organizationId,
-                parentOrganizationId: parentOrganizationId
-            })
-        })
-        .then((r) => r.json());
+        return Util.fetch('/api/clients/organizations/parent/assign', {
+            clientId: clientId,
+            organizationId: organizationId,
+            parentOrganizationId: parentOrganizationId
+        });
     }
 
     static tree(clientId = '') {
-        return fetch('/api/clients/organizations/tree', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                clientId: clientId
-            })
-        })
-        .then((r) => r.json());
+        return Util.fetch('/api/clients/organizations/tree', {
+            clientId: clientId
+        });
     }
 
-    static add(clientId = '', name = '', description = '') {
-        return fetch('/api/clients/organizations/add', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                clientId: clientId,
-                name: name,
-                description: description
-            })
-        })
-        .then((r) => r.json());
+    static add(clientId = '', organizationId = '', name = '', description = '') {
+        return Util.fetch('/api/clients/organizations/add', {
+            clientId: clientId,
+            organizationId: organizationId,
+            name: name,
+            description: description
+        });
     }
 
     static update(clientId = '', organizationId = '', name = '', description = '') {
-        return fetch('/api/clients/organizations/update', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                clientId: clientId,
-                organizationId: organizationId,
-                name: name,
-                description: description
-            })
-        })
-        .then((r) => r.json());
+        return Util.fetch('/api/clients/organizations/update', {
+            clientId: clientId,
+            organizationId: organizationId,
+            name: name,
+            description: description
+        });
     }
 }
 export { ClientOrganizations };
