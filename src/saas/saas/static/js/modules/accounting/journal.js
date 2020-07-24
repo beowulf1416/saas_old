7b22fad3da81239989dffc4bd2667d6535b4b041
ferdinand <beowulf1416@gmail.com>
2020-07-24 15:58:24 +0800

    remove journals endpoint and store

--- src/saas/saas/static/js/modules/accounting/journal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import { Util } from '/static/js/util.js';
-class Journal {
-
-    static add(journal = {}) {
-        return Util.fetch('/api/accounting/journals/add', journal);
-    }
-}
-export { Journal };
