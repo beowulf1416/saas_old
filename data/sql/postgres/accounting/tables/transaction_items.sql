903c31a97bdbbdc4928925b5ac2342729a8e2b25
ferdinand <beowulf1416@gmail.com>
2020-07-24 10:53:28 +0800

    modified check constraints on accounting transaction items table

--- data/sql/postgres/accounting/tables/transaction_items.sql
+++ data/sql/postgres/accounting/tables/transaction_items.sql
@@ -13,8 +13,10 @@ create table if not exists transaction_items (
     constraint fk_transaction_items_2 foreign key (created_year, client_id, transaction_id)
         references accounting.transactions (created_year, client_id, id) on delete restrict on update restrict,
     constraint chk_transaction_items_1 check (debit <> credit),
-    constraint chk_transaction_items_2 check (debit > 0 and (credit is null or credit = 0)),
-    constraint chk_transaction_items_3 check (credit > 0 and (debit is null or debit = 0))
+    constraint chk_transaction_items_2 check (
+        (debit > 0 and (credit is null or credit = 0))
+        or (credit > 0 and (debit is null or debit = 0))
+    )
 )
 partition by range (created_year);
 
