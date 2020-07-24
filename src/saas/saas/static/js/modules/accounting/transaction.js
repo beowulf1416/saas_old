f9b51fa6dcde2a762c8a27ff599e447b27d30e61
ferdinand <beowulf1416@gmail.com>
2020-07-24 15:14:43 +0800

    adding helper functions to retrieve accounting transactions

--- src/saas/saas/static/js/modules/accounting/transaction.js
+++ src/saas/saas/static/js/modules/accounting/transaction.js
@@ -9,5 +9,19 @@ class Transaction {
     static update(transaction = {}) {
         return Util.fetch('/api/accounting/transactions/update', transaction);
     }
+
+    static get(clientId = '', transactionId = '') {
+        return Util.fetch('api.accounting.transactions.get', {
+            clientId: clientId,
+            transactionId: transactionId
+        })
+    }
+
+    static filter(clientId = '', filter = '') {
+        return Util.fetch('/api/accounting/transactions/filter', {
+            clientId: clientId,
+            filter: filter
+        })
+    }
 }
 export { Transaction }
