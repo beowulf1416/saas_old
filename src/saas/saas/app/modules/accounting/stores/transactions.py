6dff521342e7fcf1f6228f945a5d5f4bf6af92f5
ferdinand <beowulf1416@gmail.com>
2020-07-24 10:04:23 +0800

    modified transaction store. modified test.

--- src/saas/saas/app/modules/accounting/stores/transactions.py
+++ src/saas/saas/app/modules/accounting/stores/transactions.py
@@ -12,17 +12,85 @@ class TransactionStore(BaseStore):
     def __init__(self, manager: ConnectionManager, name: str):
         super(TransactionStore, self).__init__(manager, name)
 
-    def add(self, transation = {}):
+    def add(self, transaction = {}):
         try:
+            client_id = transaction['clientId']
+            transaction_id = transaction['transactionId']
+
             cn = super(TransactionStore, self).begin()
 
             c = cn.cursor()
             c.callproc('accounting.transaction_add', [
-                transaction['clientId'],
-                transaction['transactionId'],
+                client_id,
+                transaction_id,
                 transaction['currencyId'],
                 transaction['description']
             ])
+
+            # process transaction items
+            entries = transaction['entries']
+            for e in entries:
+                c.callproc('accounting.transaction_item_add', [
+                    client_id,
+                    transaction_id,
+                    e['itemId'],
+                    e['accountId'],
+                    e['debit'],
+                    e['credit']
+                ])
+
+            super(TransactionStore, self).commit(cn)
+        except Exception as e:
+            log.error(e)
+            super(TransactionStore, self).rollback(cn)
+            raise StoreException(e)
+
+
+    def update(self, transaction = {}):
+        try:
+            client_id = transaction['clientId']
+            transaction_id = transaction['transactionId']
+
+            cn = super(TransactionStore, self).begin()
+
+            c = cn.cursor()
+            c.callproc('accounting.transaction_update', [
+                client_id,
+                transaction_id,
+                transaction['currencyId'],
+                transaction['description']
+            ])
+
+            # process transaction items
+            entries = transaction['entries']
+            for e in entries:
+                status = e['status'] if 'status' in e else None
+
+                if status:
+                    if status == 'update':
+                        c.callproc('accounting.transaction_item_update', [
+                            client_id,
+                            transaction_id,
+                            e['itemId'],
+                            e['accountId'],
+                            e['debit'],
+                            e['credit']
+                        ])
+                    elif status == 'remove':
+                        c.callproc('accounting.transaction_item_remove', [
+                            client_id,
+                            transaction_id,
+                            e['itemId']
+                        ])
+                else:
+                    c.callproc('accounting.transaction_item_add', [
+                        client_id,
+                        transaction_id,
+                        e['itemId'],
+                        e['accountId'],
+                        e['debit'],
+                        e['credit']
+                    ])
 
             super(TransactionStore, self).commit(cn)
         except Exception as e:
--- src/saas/tests/1_stores/accounting/test_accounting_transaction_store.py
+++ src/saas/tests/1_stores/accounting/test_accounting_transaction_store.py
@@ -6,6 +6,8 @@ import string
 import random
 import uuid
 
+from saas.app.modules.accounting.models.account_types import AccountTypes
+
 
 class TestTransactionStore(unittest.TestCase):
 
@@ -33,9 +35,40 @@ class TestTransactionStore(unittest.TestCase):
     def test_transaction_add(self):
         client_id = self.defaultClient[0]
         transaction_id = str(uuid.uuid4())
+        currency_id = self.defaultClient[5]
+        description = self.generate_random_str(10)
+
+        account_id_1 = str(uuid.uuid4())
+        account_id_2 = str(uuid.uuid4())
+
+        account_name_1 = self.generate_random_str(10)
+        account_name_2 = self.generate_random_str(10)
+
+        self.accountsStore.add(client_id, account_id_1, AccountTypes.ASSETS, account_name_1, account_name_1)
+        self.accountsStore.add(client_id, account_id_2, AccountTypes.ASSETS, account_name_2, account_name_2)
 
         transaction = {
             'clientId': client_id,
             'transactionId': transaction_id,
-            
+            'description': description,
+            'currencyId': currency_id,
+            'entries': [
+                {
+                    'itemId': str(uuid.uuid4()),
+                    'accountId': account_id_1,
+                    'debit': 100,
+                    'credit': 0
+                },
+                {
+                    'itemId': str(uuid.uuid4()),
+                    'accountId': account_id_2,
+                    'debit': 0,
+                    'credit': 100
+                }
+            ]
         }
+
+        try:
+            self.transactionStore.add(transaction)
+        except Exception as e:
+            self.fail(e)
