3280495ce941b0d3ac7549e063bb285b466443a1
ferdinand <beowulf1416@gmail.com>
2020-07-25 22:44:18 +0800

    clean up

--- src/saas/saas/static/js/modules/common/common.js
+++ src/saas/saas/static/js/modules/common/common.js
@@ -3,21 +3,18 @@ import { Util } from '/static/js/util.js';
 class Common {
     static countries() {
         return Util.fetch('/api/common/countries', {});
-        // return fetch('/api/common/countries', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
-        // .then((r) => r.json());
     }
 
     static currencies(filter = '') {
         return Util.fetch('/api/common/currencies', {
             filter: filter
         });
+    }
+
+    static currency_get(currencyId = '') {
+        return Util.fetch('/api/common/currencies/get', {
+            currencyId: currencyId
+        })
     }
 }
 export { Common };
