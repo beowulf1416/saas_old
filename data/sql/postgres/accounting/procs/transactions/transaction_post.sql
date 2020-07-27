9f7939712db44c2e722c6f5b869aadc9cbf781e7
ferdinand <beowulf1416@gmail.com>
2020-07-27 15:11:09 +0800

    process transaction items on post

--- data/sql/postgres/accounting/procs/transactions/transaction_post.sql
+++ data/sql/postgres/accounting/procs/transactions/transaction_post.sql
@@ -4,13 +4,23 @@ create or replace function transaction_post (
 )
 returns void
 as $$
+declare
+    t_items record;
 begin
     update accounting.transactions set 
         posted_ts = now()
     where client_id = p_client_id
-        and id = p_transaction_id;
+        and id = p_transaction_id
+        and posted_ts is null;
 
     -- process transaction items
+    update accounting.account_balances ab
+        set value = value + debit - credit
+    from accounting.transaction_items ti
+    where ti.account_id = ab.acct_id
+        and ti.client_id = ab.client_id
+        and ti.transaction_id = p_transaction_id
+        and ti.client_id = p_client_id;
 end
 $$
 language plpgsql;
