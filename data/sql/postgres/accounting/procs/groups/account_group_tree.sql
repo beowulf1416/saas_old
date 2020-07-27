3f75ee614bf847d9ff5ae5cbe49dbea4a3eaea43
ferdinand <beowulf1416@gmail.com>
2020-07-27 21:10:06 +0800

    exclude root account group from chart of accounts

--- data/sql/postgres/accounting/procs/groups/account_group_tree.sql
+++ data/sql/postgres/accounting/procs/groups/account_group_tree.sql
@@ -7,7 +7,15 @@ returns table (
     level int
 )
 as $$
+declare
+    t_root_account_group_id accounting.account_groups.id%type;
 begin
+    select
+        a.id into t_root_account_group_id
+    from accounting.account_groups a
+    where a.client_id = p_client_id
+        and a.name = 'root';
+
     return query
     select
         a.group_id,
@@ -17,6 +25,7 @@ begin
         inner join accounting.account_groups b on a.group_id = b.id
     where a.client_id = p_client_id
         and a.path <@ 'root'
+        and a.group_id <> t_root_account_group_id
     order by a.path;
 end
 $$
