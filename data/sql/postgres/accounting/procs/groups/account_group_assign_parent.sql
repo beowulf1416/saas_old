26b64195f5d5bf110d01200df1f13389885de28e
ferdinand <beowulf1416@gmail.com>
2020-07-28 21:56:21 +0800

    modify account group assignment

--- data/sql/postgres/accounting/procs/groups/account_group_assign_parent.sql
+++ data/sql/postgres/accounting/procs/groups/account_group_assign_parent.sql
@@ -8,12 +8,21 @@ as $$
 declare
     t_parent_path public.ltree;
     t_new_path public.ltree;
+    t_old_path public.ltree;
 begin
+    -- get parent path
     select
         a.path into t_parent_path
     from accounting.account_group_tree a
     where a.client_id = p_client_id
         and a.group_id = p_parent_group_id;
+
+    -- get current path
+    select
+        a.path into t_old_path
+    from accounting.account_group_tree a
+    where a.client_id = p_client_id
+        and a.group_id = p_group_id;
 
     t_new_path := text2ltree(ltree2text(t_parent_path) || '.' || replace(p_group_id::text, '-', '_'));
 
@@ -22,6 +31,16 @@ begin
         path = t_new_path
     where client_id = p_client_id
         and group_id = p_group_id;
+
+    -- update descendants
+    update accounting.account_group_tree a set
+        path = t_new_path || subpath(b.path, nlevel(t_old_path))
+    from accounting.account_group_tree b
+    where a.client_id = p_client_id
+        and b.client_id = p_client_id
+        and b.path <@ t_old_path
+        and nlevel(b.path) > nlevel(t_old_path)
+        and a.group_id = b.group_id;
 end
 $$
 language plpgsql;
