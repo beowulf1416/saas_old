4d6596c5fa3ee3c941007c1f41073533e20684e2
ferdinand <beowulf1416@gmail.com>
2020-07-28 22:26:07 +0800

    add type id field to account groups. add account group update helper function.

--- src/saas/saas/app/modules/accounting/api/groups.py
+++ src/saas/saas/app/modules/accounting/api/groups.py
@@ -15,19 +15,58 @@ def api_accounting_groups_add(request):
     params = request.json_body
     client_id = params['clientId'] if 'clientId' in params else None
     group_id = params['groupId'] if 'groupId' in params else None
+    type_id = params['typeId'] if 'typeId' in params else None
     name = params['name'] if 'name' in params else None
     description = params['description'] if 'description' in params else None
 
-    if client_id is None or group_id is None or name is None:
+    if client_id is None or group_id is None or type_id is None or name is None:
         raise exception.HTTPBadRequest(
             detail='Missing required parameters',
-            explanation='Client Id, Group Id and Name is required'
+            explanation='Client Id, Group Id, Type Id and Name is required'
         )
 
     services = request.services()
     try:
         store = services['store.accounting.groups']
-        store.add(client_id, group_id, name, description)
+        store.add(client_id, group_id, type_id, name, description)
+    except Exception as e:
+        raise exception.HTTPInternalServerError(
+            detail=str(e),
+            explanation=str(e)
+        )
+
+    raise exception.HTTPOk(
+        detail='Account Group added',
+        body={
+            'message': 'Account Group added'
+        }
+    )
+
+
+@view_config(
+    route_name='api.accounting.groups.update',
+    request_method='POST',
+    renderer='json',
+    permission='user.authenticated'
+)
+def api_accounting_groups_update(request):
+    params = request.json_body
+    client_id = params['clientId'] if 'clientId' in params else None
+    group_id = params['groupId'] if 'groupId' in params else None
+    type_id = params['typeId'] if 'typeId' in params else None
+    name = params['name'] if 'name' in params else None
+    description = params['description'] if 'description' in params else None
+
+    if client_id is None or group_id is None or type_id is None or name is None:
+        raise exception.HTTPBadRequest(
+            detail='Missing required parameters',
+            explanation='Client Id, Group Id, Type Id and Name is required'
+        )
+
+    services = request.services()
+    try:
+        store = services['store.accounting.groups']
+        store.update(client_id, group_id, type_id, name, description)
     except Exception as e:
         raise exception.HTTPInternalServerError(
             detail=str(e),
--- src/saas/saas/static/js/modules/accounting/groups.js
+++ src/saas/saas/static/js/modules/accounting/groups.js
@@ -2,13 +2,24 @@
 import { Util } from '/static/js/util.js';
 class Groups {
 
-    static add(clientId = '', groupId = '', name = '', description = '') {
+    static add(clientId = '', groupId = '', typeId = '', name = '', description = '') {
         return Util.fetch('/api/accounting/groups/add', {
             clientId: clientId,
             groupId: groupId,
+            typeId: typeId,
             name: name,
             description: description
         })
+    }
+
+    static update(clientId = '', groupId = '', typeId = '', name = '', description = '') {
+        return Util.fetch('/api/accounting/groups/update', {
+            clientId: clientId,
+            groupId: groupId,
+            typeId: typeId,
+            name: name,
+            description: description
+        });
     }
 
     static tree(clientId = '') {
