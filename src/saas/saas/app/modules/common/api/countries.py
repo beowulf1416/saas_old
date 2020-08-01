6706651e9ceab180fb1e92523ea2f0e9e9f7227c
ferdinand <beowulf1416@gmail.com>
2020-08-01 23:13:36 +0800

    add endpoint for retrieving country info

--- src/saas/saas/app/modules/common/api/countries.py
+++ src/saas/saas/app/modules/common/api/countries.py
@@ -54,3 +54,45 @@ def api_common_countries(request):
             'countries': countries
         }
     )
+
+
+@view_config(
+    route_name='api.common.countries.get',
+    request_method='POST',
+    renderer='json',
+    permission='user.authenticated'
+)
+def api_common_countries_get(request):
+    params = request.json_body
+    country_id = params['countryId'] if 'countryId' in params else None
+
+    if country_id is None:
+        raise exception.HTTPBadRequest(
+            detail='Missing required parameters',
+            explanation='Country Id is required'
+        )
+
+    services = request.services()
+    store = services['stores.common.countries']
+    country = {}
+    try:
+        r = store.get(country_id)
+        country = {
+            'name': r[0],
+            'alpha_2': r[1],
+            'alpha_3': r[2],
+            'id': r[3]
+        }
+    except Exception as e:
+        log.error(e)
+        raise exception.HTTPInternalServerError(
+            detail=str(e),
+            explanation=str(e)
+        )
+
+    raise exception.HTTPOk(
+        detail='country found',
+        body={
+            'country': country
+        }
+    )
