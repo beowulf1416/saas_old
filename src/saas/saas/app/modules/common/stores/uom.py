7a836941fa485abf99b8f1dba5dfeca9ec202484
ferdinand <beowulf1416@gmail.com>
2020-08-02 21:13:10 +0800

    add store function to retrieve uom

--- src/saas/saas/app/modules/common/stores/uom.py
+++ src/saas/saas/app/modules/common/stores/uom.py
@@ -2,7 +2,9 @@ import logging
 log = logging.getLogger(__name__)
 
 from saas.app.core.services.connection import ConnectionManager
-from saas.app.core.stores.base import BaseStore
+from saas.app.core.stores.base import BaseStore, StoreException
+
+from uuid import UUID
 
 
 class UOMStore(BaseStore):
@@ -52,3 +54,29 @@ class UOMStore(BaseStore):
         except Exception as e:
             log.error(e)
             raise Exception('Unable to retrieve all units of measure')
+
+    def filter(self, client_id: UUID, dimension: str, filter: str) -> []:
+        try:
+            return super(UOMStore, self).runProc('common.uom_filter', [
+                client_id,
+                dimension,
+                f'%{filter}%'
+            ])
+        except Exception as e:
+            log.error(e)
+            raise StoreException('Unable to retrieve filter list of units of measure')
+
+    def get(self, client_id: UUID, dimension: str, uom_id: int) -> {}:
+        try:
+            result = super(UOMStore, self).runProc('common.uom_get', [
+                client_id,
+                dimension,
+                uom_id
+            ])
+            if len(result) > 0:
+                return result[0]
+            else:
+                raise StoreException('Unable to find unit of measure')
+        except Exception as e:
+            log.error(e)
+            raise StoreException('Unable to retrieve unit of measure')
--- src/saas/tests/1_stores/common/test_common_uom_store.py
+++ src/saas/tests/1_stores/common/test_common_uom_store.py
@@ -10,11 +10,15 @@ class TestUOMStore(unittest.TestCase):
 
         from saas.app.core.services.connection import ConnectionManager
         from saas.app.modules.common.stores.uom import UOMStore
+        from saas.app.core.stores.client import ClientStore
 
         self.mgr = ConnectionManager({
             'app.config': '../../etc'
         })
         self.uomStore = UOMStore(self.mgr, 'default')
+        self.clientStore = ClientStore(self.mgr, 'default')
+
+        self.defaultClient = self.clientStore.getDefaultClient()
 
     def test_uom_length(self):
         result = self.uomStore.length()
@@ -39,3 +43,14 @@ class TestUOMStore(unittest.TestCase):
     def test_uom_all(self):
         result = self.uomStore.all()
         self.assertGreater(len(result), 0)
+
+    def test_uom_filter(self):
+        client_id = self.defaultClient[0]
+        result = self.uomStore.filter(client_id, 'length', '')
+        self.assertGreater(len(result), 0, '{0}'.format(result))
+
+    def test_uom_get(self):
+        client_id = self.defaultClient[0]
+        result = self.uomStore.filter(client_id, 'length', '')
+        result = self.uomStore.get(client_id, 'length', result[0][0])
+        self.assertGreater(len(result), 0, '{0}'.format(result))
