886c3235d2833be77633849a9bdf2ffd5ab70383
ferdinand <beowulf1416@gmail.com>
2020-08-02 21:20:11 +0800

    add simple validation. add function to retrieve uom info.

--- src/saas/saas/app/modules/common/api/uoms.py
+++ src/saas/saas/app/modules/common/api/uoms.py
@@ -17,6 +17,12 @@ def api_common_uom_filter(request):
     dimension = params['dimension'] if 'dimension' in params else None
     filter = params['filter'] if 'filter' in params else None
 
+    if client_id is None or dimension is None or filter is None:
+        raise exception.HTTPBadRequest(
+            detail='Missing required parameter',
+            explanation='Client Id, dimension and filter is required'
+        )
+
     services = request.services()
     store = services['stores.common.uoms']
     uoms = []
@@ -31,12 +37,11 @@ def api_common_uom_filter(request):
                 filter
             )
 
-        currencies = [
+        uoms = [
             {
                 'id': r[0],
                 'name': r[1],
-                'currency': r[2],
-                'symbol': r[3]
+                'symbol': r[2]
             }
             for r in result
         ]
@@ -48,8 +53,50 @@ def api_common_uom_filter(request):
         )
 
     raise exception.HTTPOk(
-        detail=f'{len(currencies)} countries found',
+        detail=f'{len(uoms)} units found',
         body={
-            'currencies': currencies
+            'uoms': uoms
+        }
+    )
+
+@view_config(
+    route_name='api.common.uom.get',
+    request_method='POST',
+    renderer='json',
+    permission='user.authenticated'
+)
+def api_common_uom_filter(request):
+    params = request.json_body
+    client_id = params['clientId'] if 'clientId' in params else None
+    dimension = params['dimension'] if 'dimension' in params else None
+    uom_id = params['uomId'] if 'uomId' in params else None
+
+    if client_id is None or dimension is None or uom_id is None:
+        raise exception.HTTPBadRequest(
+            detail='Missing required parameter',
+            explanation='Client Id, dimension and UOM Id is required'
+        )
+
+    services = request.services()
+    store = services['stores.common.uoms']
+    uom = {}
+    try:
+        r = store.get(client_id, dimension, uom_id)
+        uom = {
+            'id': r[0],
+            'name': r[1],
+            'symbol': r[2]
+        }
+    except Exception as e:
+        log.error(e)
+        raise exception.HTTPInternalServerError(
+            detail=str(e),
+            explanation=str(e)
+        )
+
+    raise exception.HTTPOk(
+        detail='unit found',
+        body={
+            'uom': uom
         }
     )
