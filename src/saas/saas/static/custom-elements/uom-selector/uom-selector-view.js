14f79e01ed752bd1bc1ee04b0ad6b248633967d5
ferdinand <beowulf1416@gmail.com>
2020-08-02 21:31:01 +0800

    modify uom selector element

---
+++ src/saas/saas/static/custom-elements/uom-selector/uom-selector-view.js
@@ -0,0 +1,115 @@
+'use strict';
+import { Common } from '/static/js/modules/common/common.js';
+import { Util } from '/static/js/util.js';
+class UomSelectorView extends HTMLElement {
+
+    constructor() {
+        const self = super();
+
+        const style = document.createElement("link");
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', '/static/custom-elements/uom-selector/uom-selector.css');
+
+        const default_style = document.createElement("link");
+        default_style.setAttribute('rel', 'stylesheet');
+        default_style.setAttribute('href', '/static/css/default.css');
+
+        const div = document.createElement('div');
+        div.classList.add('component-wrapper');
+
+        this._init(div);
+
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(style);
+        shadow.appendChild(default_style);
+        shadow.appendChild(div);
+
+        this._attachEventHandlers = this._attachEventHandlers.bind(this);
+
+        this._attachEventHandlers();
+    }
+
+    _init(container) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <form-search id="search"></form-search>
+            <div class="table-wrapper">
+                <table id="tbl-uoms">
+                    <caption>Units if Measure</caption>
+                    <colgroup>
+                        <col class="select">
+                        <col class="name">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <td scope="col"></td>
+                            <th scope="col">Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div><!-- .table-wrapper -->
+        `;
+
+        container.appendChild(div);
+    }
+
+    _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const client_id = this.getAttribute('client-id');
+        const dimension = this.getAttribute('dimension');
+
+        shadow.getElementById('search').addEventListener('search', function(e) {
+            const filter = e.detail.filter;
+
+            Common.uom_filter(client_id, dimension, filter).then((r) => {
+                if (r.status == 'success') {
+                    self.setUoms(r.json.uoms, filter);
+                } else {
+                    notify(r.status, r.message, 3000);
+                }
+            });
+        });
+    }
+
+    setUoms(uoms = [], filter = '') {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const tbody = shadow.querySelector('table#tbl-uoms tbody');
+        while(tbody.firstChild) {
+            tbody.removeChild(tbody.lastChild);
+        }
+
+        uoms.forEach((u) => {
+            const id = 'id' + Util.generateId();
+            const uom_name = u.name.replace(filter, `<strong>${filter}</strong>`);
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td><input type="radio" id="${id}" name="selected" class="form-input-selected" title="Selected" value="${u.id}" data-name="${u.name}" /></td>
+                <td><label for="${id}">${uom_name}</label></td>
+            `;
+
+            tbody.appendChild(tr);
+
+            // event handlers
+            const input_selected = tr.querySelector('.form-input-selected');
+            input_selected.addEventListener('change', function() {
+                self.dispatchEvent(new CustomEvent('change', {
+                    bubbles: true,
+                    cancelable: true,
+                    detail: {
+                        uom: {
+                            id: input_selected.value,
+                            name: input_selected.dataset.name
+                        }
+                    }
+                }));
+            });
+        });
+    }
+}
+customElements.define('uom-selector-view', UomSelectorView);
--- src/saas/saas/static/custom-elements/uom-selector/uom-selector.js
+++ src/saas/saas/static/custom-elements/uom-selector/uom-selector.js
@@ -21,6 +21,27 @@ class UomSelector extends HTMLElement {
         shadow.appendChild(style);
         shadow.appendChild(default_style);
         shadow.appendChild(div);
+
+        this._attachEventHandlers = this._attachEventHandlers.bind(this);
+        this._fetch = this._fetch.bind(this);
+        this.value = this.value.bind(this);
+ 
+        this._attachEventHandlers();
+        this._fetch();
+    }
+
+    get value() {
+        return this.getAttribute('uom-id');
+    }
+
+    static get observedAttributes() { 
+        return ['uom-id']; 
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == 'uom-id') {
+            this._fetch();
+        }
     }
 
     _init(container) {
@@ -35,4 +56,44 @@ class UomSelector extends HTMLElement {
 
         container.appendChild(div);
     }
-} 
+
+    _attachEventHandlers() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const client_id = this.getAttribute('client-id');
+        const dimention = this.hasAttribute('dimension') ? this.getAttribute('dimension') : 'quantity';
+
+        shadow.getElementById('btn-select').addEventListener('click', function() {
+            const selector = showInView('Select Unit of Measure', `<uom-selector-view client-id="${client_id}" dimension="${dimension}"></uom-selector-view>`);
+            selector.addEventListener('change', function(e) {
+                const uom = e.detail.uom;
+
+                self.setAttribute('uom-id', uom.id);
+            });
+        });
+    }
+
+    _fetch() {
+        const self = this;
+        const shadow = this.shadowRoot;
+
+        const client_id = this.getAttribute('client-id');
+        const dimension = this.getAttribute('dimension');
+        const uom_id = this.getAttribute('uom-id');
+
+        if (dimension && uom_id) {
+            Common.uom_get(client_id, dimension, uom_id).then((r) => {
+                if (r.status == 'success') {
+                    const uom = r.json.uom
+                    
+                    const input_display = shadow.getElementById('display');
+                    input_display.value = uom.name;
+                } else {
+                    notify(r.status, r.message, 3000);
+                }
+            });
+        }
+    }
+}
+customElements.define('uom-selector', UomSelector);
