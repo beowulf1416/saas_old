750da3d2950936f589515e3e47706c8c84eb3731
ferdinand <beowulf1416@gmail.com>
2020-08-04 18:19:29 +0800

    modify sp project add

--- data/sql/postgres/work/procs/projects/project_add.sql
+++ data/sql/postgres/work/procs/projects/project_add.sql
@@ -4,9 +4,7 @@ create or replace function project_add (
     p_name work.projects.name%type,
     p_description work.projects.description%type,
     p_planned_start work.projects.planned_start_ts%type,
-    p_planned_end work.projects.planned_end_ts%type,
-    p_actual_start work.projects.actual_start_ts%type,
-    p_actual_end work.projects.actual_end_ts%type
+    p_planned_end work.projects.planned_end_ts%type
 )
 returns void
 as $$
@@ -17,18 +15,14 @@ begin
         name,
         description,
         planned_start_ts,
-        planned_end_ts,
-        actual_start_ts,
-        actual_end_ts
+        planned_end_ts
     ) values (
         p_client_id,
         p_project_id,
         p_name,
         p_description,
         p_planned_start,
-        p_planned_end,
-        p_actual_start,
-        p_actual_end
+        p_planned_end
     );
 end
 $$
