571a51859a7efce2133f1510af3e74b6d55147f2
ferdinand <beowulf1416@gmail.com>
2020-08-07 16:33:52 +0800

    modify gantt chart - using d3 and momentjs

--- src/saas/saas/static/custom-elements/gantt-chart/gantt-chart.js
+++ src/saas/saas/static/custom-elements/gantt-chart/gantt-chart.js
@@ -12,9 +12,6 @@ class GanttChart extends HTMLElement {
         default_style.setAttribute('rel', 'stylesheet');
         default_style.setAttribute('href', '/static/css/default.css');
 
-        // const js_d3 = document.createElement('script');
-        // js_d3.setAttribute('src', 'https://d3js.org/d3.v5.min.js');
-
         const div = document.createElement('div');
         div.classList.add('component-wrapper');
 
@@ -25,10 +22,10 @@ class GanttChart extends HTMLElement {
         shadow.appendChild(default_style);
         shadow.appendChild(div);
 
-        // shadow.appendChild(js_d3);
-
         this._attachEventHandlers = this._attachEventHandlers.bind(this);
-        this._attachEventHandlers();
+        this.setProject = this.setProject.bind(this);
+
+        // this._attachEventHandlers();
     }
 
     _init(container) {
@@ -45,18 +42,61 @@ class GanttChart extends HTMLElement {
     }
 
     _attachEventHandlers() {
-        const self = this;
-        const shadow = this.shadowRoot;
+        // const self = this;
+        // const shadow = this.shadowRoot;
+
+        console.log('attach');
     }
 
     connectedCallback() {
         if (this.isConnected) {
             const shadow = this.shadowRoot;
-            this.render();
+            // this.render();
         }
     }
 
-    render() {
+    // render() {
+    //     const self = this;
+    //     const shadow = this.shadowRoot;
+
+    //     const parent = this.parentElement;
+    //     const chart = shadow.getElementById('chart');
+
+    //     const width = parent.clientWidth;
+    //     const height = parent.clientHeight;
+    //     // chart.setAttribute('viewBox', `0 0 ${width - 2} ${height}`);
+
+    //     const data = [
+    //         { x: 1, y: 2 },
+    //         { x: 2, y: 4},
+    //         { x: 3, y: 5 }
+    //     ];
+        
+    //     const svg = d3.select(shadow)
+    //         .select('#chart')
+    //         .attr('viewBox', [0, 0, width, height]);
+
+    //     const start_x = d3.scaleLinear()
+    //         .domain([0, d3.max(data, d => d.x )])
+    //         .range([0, 10]);
+
+    //     const y = d3.scaleBand()
+    //         .domain(d3.range(data.length))
+    //         .rangeRound([0, height])
+    //         .padding(1);
+        
+    //     svg.append('g')
+    //         .attr('fill', 'steelblue')
+    //         .selectAll('rect')
+    //         .data(data)
+    //         .join('rect')
+    //             .attr('x', d=> start_x(d.x))
+    //             .attr('y', (d, i) => y(i))
+    //             .attr('width', 10)
+    //             .attr('height', 10);
+    // }
+
+    setProject(project = {}) {
         const self = this;
         const shadow = this.shadowRoot;
 
@@ -65,27 +105,44 @@ class GanttChart extends HTMLElement {
 
         const width = parent.clientWidth;
         const height = parent.clientHeight;
-        // chart.setAttribute('viewBox', `0 0 ${width - 2} ${height}`);
+        const row_height = 16;
 
-        const data = [
-            { x: 1, y: 2 },
-            { x: 2, y: 4},
-            { x: 3, y: 5 }
-        ];
-        
+        console.log(project);
+
+        const margin = {
+            top: 20,
+            right: 0,
+            bottom: 20,
+            left: 20
+        };
+
+        const x = d3.scaleTime()
+            .domain([Date.now(), Date.now() + 24*60*60*1000])
+            .rangeRound([0, width]);
+
+        const y = d3.scaleBand()
+            .domain([0, project.tasks.length]);
+
         const svg = d3.select(shadow)
             .select('#chart')
-            .attr('viewBox', [0, 0, width, height]);
-        
+            .attr('viewBox', [ 0, 0, width, height]);
+
+        // x-axis
         svg.append('g')
-            .attr('fill', 'steelblue')
+            .attr('fill', 'black')
+            .attr('transform', `translate(0, 0)`)
+            .call(d3.axisBottom(x).ticks());
+
+        svg.append('g')
+            .attr('fill', 'red')
+            .attr('transform', `translate(0, ${margin.top})`)
             .selectAll('rect')
-            .data(data)
+            .data(project.tasks)
             .join('rect')
-                .attr('x', d => d.x)
-                .attr('y', d => d.y)
-                .attr('width', 10)
-                .attr('height', 10);
+                .attr('x', d => x(d.start))
+                .attr('y', (d, i) => row_height * i)
+                .attr('width', d => x(d.end))
+                .attr('height', row_height);
     }
 }
 customElements.define('gantt-chart', GanttChart);
