f2f1385723693f75b27e868fb2e83c80d5c1f1de
ferdinand <beowulf1416@gmail.com>
2020-08-17 19:08:59 +0800

    clean up admin client helper module

--- src/saas/saas/static/js/modules/admin/clients.js
+++ src/saas/saas/static/js/modules/admin/clients.js
@@ -3,39 +3,17 @@ import { Util } from '/static/js/util.js';
 class Clients {
     static getAll() {
         return Util.fetch('/api/clients/all', {});
-        // return fetch('/api/clients/all', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        // })
-        // .then((r) => r.json());
     }
 
     // TODO include client id in params
-    static add(name = '', address = '', countryId = '', currencyId = '') {
+    static add(clientId = '', name = '', address = '', countryId = '', currencyId = '') {
         return Util.fetch('/api/clients/add', {
+            clientId: clientId,
             name: name,
             address: address,
             countryId: countryId,
             currencyId: currencyId
         });
-        // return fetch('/api/clients/add', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         name: name,
-        //         address: address,
-        //         countryId: country
-        //     })
-        // })
-        // .then((r) => r.json());
     }
 
     static update(clientId = '', name = '', address = '', countryId = '', currencyId = '') {
@@ -43,43 +21,16 @@ class Clients {
             clientId: clientId,
             name: name,
             address: address,
-            countryId: country,
+            countryId: countryId,
             currencyId: currencyId
 
         });
-        // return fetch('/api/clients/update', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         clientId: clientId,
-        //         name: name,
-        //         address: address,
-        //         countryId: country
-        //     })
-        // })
-        // .then((r) => r.json());
     }
 
     static get(clientId) {
         return Util.fetch('/api/clients/get', {
             clientId: clientId
         });
-        // return fetch('/api/clients/get', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         clientId: clientId
-        //     })
-        // })
-        // .then((r) => r.json());
     }
 
     static setActive(clientId = '', active = '') {
@@ -87,37 +38,12 @@ class Clients {
             clientId: clientId,
             active: active
         });
-        // return fetch('/api/clients/active', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         clientId: clientId,
-        //         active: active
-        //     })
-        // })
-        // .then((r) => r.json());
     }
 
     static filter(filter = '') {
         return Util.fetch('/api/clients/filter', {
             filter: filter
         });
-        // return fetch('/api/clients/filter', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         filter: filter
-        //     })
-        // })
-        // .then((r) => r.json());
     }
 }
 export { Clients };
