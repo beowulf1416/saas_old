8e9b12b0b26a6ce40ec329c85c5165c192bb332d
ferdinand <beowulf1416@gmail.com>
2020-08-18 15:36:08 +0800

    remove autogenerated user id

--- data/sql/postgres/iam/tables/users.sql
+++ data/sql/postgres/iam/tables/users.sql
@@ -2,7 +2,7 @@
  * user accounts
  */
 create table if not exists users (
-    id uuid not null default public.gen_random_uuid(),
+    id uuid not null,
     active boolean not null default false,
     created_ts timestamp without time zone not null default(now() at time zone 'utc'),
     email public.email_address not null,
