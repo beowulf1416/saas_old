2fcd1be5322363906769f846177670a7b1874f6e
ferdinand <beowulf1416@gmail.com>
2020-08-21 16:55:34 +0800

    use global default styles in custom element

--- src/saas/saas/static/custom-elements/admin/role-selector/role-selector.js
+++ src/saas/saas/static/custom-elements/admin/role-selector/role-selector.js
@@ -9,11 +9,11 @@ class RoleSelector extends HTMLElement {
 
         const style = document.createElement("link");
         style.setAttribute('rel', 'stylesheet');
-        style.setAttribute('href', '/static/custom.elements/admin/role-selector/role-selector.css');
+        style.setAttribute('href', '/static/custom-elements/admin/role-selector/role-selector.css');
 
-        const google_web_fonts = document.createElement("link");
-        google_web_fonts.setAttribute('rel', 'stylesheet');
-        google_web_fonts.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+        const default_style = document.createElement("link");
+        default_style.setAttribute('rel', 'stylesheet');
+        default_style.setAttribute('href', '/static/css/default.css');
 
         const div = document.createElement('div');
         div.classList.add('component-wrapper');
@@ -22,7 +22,7 @@ class RoleSelector extends HTMLElement {
 
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.appendChild(style);
-        shadow.appendChild(google_web_fonts);
+        shadow.appendChild(default_style);
         shadow.appendChild(div);
 
         this.setRoles = this.setRoles.bind(this);
