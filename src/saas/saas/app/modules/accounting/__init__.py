7d11bef3efedb958dae3029495af0b1bf3bdb36e
ferdinand <beowulf1416@gmail.com>
2020-09-29 20:08:52 +0800

    switch to dynamic imports

--- src/saas/saas/app/modules/accounting/__init__.py
+++ src/saas/saas/app/modules/accounting/__init__.py
@@ -29,45 +29,79 @@ def includeme(config):
                 'script': '/static/js/modules/accounting/actions.js',
                 'async': True
             },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/account-tree/account-tree.js',
-                'async': True
-            },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/account-editor/account-editor.js',
-                'async': True
-            },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/accounting-journal/accounting-journal.js',
-                'async': True
-            },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/accounting-journals/accounting-journals.js',
-                'async': True
-            },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/account-selector/account-selector.js',
-                'async': True
-            },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/account-selector/account-selector-view.js',
-                'async': True
-            },
-            {
-                'type': 'module',
-                'script': '/static/custom-elements/accounting/account-group/account-group.js',
-                'async': True
-            },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/account-tree/account-tree.js',
+            #     'async': True
+            # },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/account-editor/account-editor.js',
+            #     'async': True
+            # },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/accounting-journal/accounting-journal.js',
+            #     'async': True
+            # },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/accounting-journals/accounting-journals.js',
+            #     'async': True
+            # },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/account-selector/account-selector.js',
+            #     'async': True
+            # },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/account-selector/account-selector-view.js',
+            #     'async': True
+            # },
+            # {
+            #     'type': 'module',
+            #     'script': '/static/custom-elements/accounting/account-group/account-group.js',
+            #     'async': True
+            # },
             {
                 'type': 'module',
                 'script': '/static/custom-elements/accounting/invoices/invoice-dashboard/invoice-dashboard.js',
                 'async': True
+            }
+        ],
+        'elements': [
+            {
+                'tag': 'account-editor',
+                'script': '/static/custom-elements/accounting/account-editor/account-editor.js'
+            },
+            {
+                'tag': 'account-selector',
+                'script': '/static/custom-elements/accounting/account-selector/account-selector.js'
+            },
+            {
+                'tag': 'account-selector-view',
+                'script': '/static/custom-elements/accounting/account-selector/account-selector-view.js'
+            },
+            {
+                'tag': 'account-group',
+                'script': '/static/custom-elements/accounting/account-group/account-group.js'
+            }
+            {
+                'tag': 'account-tree',
+                'script': '/static/custom-elements/accounting/account-tree/account-tree.js'
+            },
+            {
+                'tag': 'accounting-journal',
+                'script': '/static/custom-elements/accounting/accounting-journal/accounting-journal.js'
+            },
+            {
+                'tag': 'accounting-journals',
+                'script': '/static/custom-elements/accounting/accounting-journals/accounting-journals.js'
+            },
+            {
+                'tag': 'invoice-dashboard',
+                'script': '/static/custom-elements/accounting/invoices/invoice-dashboard/invoice-dashboard.js'
             }
         ]
     }
