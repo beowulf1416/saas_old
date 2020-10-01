8d633944b672dd2b3bad9d9faa35733c3329d24c
ferdinand <beowulf1416@gmail.com>
2020-10-01 19:58:48 +0800

    add showInView function

--- src/saas/saas/static/js/app.js
+++ src/saas/saas/static/js/app.js
@@ -70,6 +70,40 @@ const App = {
         if (tab) {
             tab.addTab(id, label, content);
         }
+    },
+
+    showInView(label = '', content = '', element = '') {
+        this.loadElement(element);
+        
+        const views = document.getElementById('views');
+        if (views.firstChild) {
+            views.removeChild(views.lastChild);
+        }
+    
+        views.innerHTML = `
+            <div class="view-wrapper">
+                <div class="view-header">
+                    <h4>${label}</h4>
+                    <a class="link-close" title="Close View" href="#">
+                        <span class="material-icons">close</span>
+                    </a>
+                </div><!-- .view-header -->
+                <div class="view-content">
+                    ${content}
+                </div><!-- .view-content -->
+            </div><!-- .view-wrapper -->
+        `;
+    
+        // event handler
+        const close = views.querySelector('.link-close');
+        close.addEventListener('click', function(e) {
+            while(views.firstChild) {
+                views.removeChild(views.lastChild);
+            }
+            e.preventDefault();
+        });
+    
+        return views.querySelector('.view-content');
     }
 };
 
