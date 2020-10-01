7d5f05c0bd6cd835ea9e8acbe0f3b5eb71b9e6a9
ferdinand <beowulf1416@gmail.com>
2020-10-01 19:45:53 +0800

    switch global app object

--- src/saas/saas/static/js/modules/clients/actions.js
+++ src/saas/saas/static/js/modules/clients/actions.js
@@ -1,7 +1,21 @@
 'use strict';
 import { showInTab } from '/static/js/ui/ui.js';
-(function(){
+import { App } from '/static/js/app.js';
+(function(app){
     console.log('clients actions');
+    app.registerAction([
+        {
+            name: 'client.info',
+            func: function() {
+                const client_id = window.clientId;
+                app.showInTab('client-editor',
+                    'Client',
+                    `<client-editor client-id="${client_id}"></client-editor>`,
+                    'client-editor'
+                );
+            }
+        }
+    ]);
     
     window.actions = window.actions ? window.actions : {};
     window.actions['client.users'] = function() {
@@ -12,8 +26,8 @@ import { showInTab } from '/static/js/ui/ui.js';
         const client_id = window.clientId;
         showInTab('organization-tree', 'Organizations', `<organization-tree client-id="${client_id}"></organization-tree>`);
     };
-    window.actions['client.info'] = function() {
-        const client_id = window.clientId;
-        showInTab('client-editor', 'Client', `<client-editor client-id="${client_id}"></client-editor>`);
-    };
-})();
+    // window.actions['client.info'] = function() {
+    //     const client_id = window.clientId;
+    //     showInTab('client-editor', 'Client', `<client-editor client-id="${client_id}"></client-editor>`);
+    // };
+})(window.app = window.app ? window.app : App);
